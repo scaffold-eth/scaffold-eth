477186edc3c20ba92c9ac1e810b78e1044c20ae3
Austin Griffith <austin@ethereum.org>
2020-04-21 12:19:57 -0600

    testing initial structure

---
+++ src/Poller.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useRef } from 'react';
+export default function Poller(callback, delay) {
+  const savedCallback = useRef();
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+  //run at start too
+  useEffect(() => {
+    callback()
+  }, []);
+}
