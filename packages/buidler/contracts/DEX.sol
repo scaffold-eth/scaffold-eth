dc1a6a9134f6f0a3dd8843d697331f307b35d7e3
Austin Griffith <austin@ethereum.org>
2020-06-04 09:31:17 -0600

    starting point for dex build

---
+++ packages/buidler/contracts/DEX.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.6.6;
+
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract DEX {
+
+  using SafeMath for uint256;
+  IERC20 token;
+
+  constructor(address token_addr) public {
+    token = IERC20(token_addr);
+  }
+
+}
