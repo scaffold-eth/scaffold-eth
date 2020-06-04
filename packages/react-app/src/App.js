cb9bff2e119a1a93f0fe8336d88a616d0fe1c8f6
Austin Griffith <austin@ethereum.org>
2020-06-04 09:35:51 -0600

    carve out scw

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -8,8 +8,6 @@ import { Row, Col } from 'antd';
 import { useExchangePrice, useGasPrice } from "./hooks"
 import { Header, Account, Provider, Faucet, Ramp } from "./components"
 
-import SmartContractWallet from './SmartContractWallet.js'
-
 const mainnetProvider = new ethers.providers.InfuraProvider("mainnet","2717afb6bf164045b5d5468031b93f87")
 const localProvider = new ethers.providers.JsonRpcProvider(process.env.REACT_APP_PROVIDER?process.env.REACT_APP_PROVIDER:"http://localhost:8545")
 
@@ -34,15 +32,8 @@ function App() {
           price={price}
         />
       </div>
-      <div style={{padding:40,textAlign: "left"}}>
-        <SmartContractWallet
-          address={address}
-          injectedProvider={injectedProvider}
-          localProvider={localProvider}
-          price={price}
-          gasPrice={gasPrice}
-        />
-      </div>
+
+
       <div style={{position:'fixed',textAlign:'right',right:0,bottom:20,padding:10}}>
         <Row align="middle" gutter={4}>
           <Col span={10}>
