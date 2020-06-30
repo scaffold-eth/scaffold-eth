189ee306654521348964d01eee548bd3604ed090
Tom French <tom@tomfren.ch>
2020-06-30 15:55:31 +0100

    add eslint and prettier

---
+++ packages/react-app/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+  },
+  extends: ["airbnb", 'plugin:prettier/recommended', 'prettier/react'],
+  plugins: ["babel"],
+  rules: {
+    "prettier/prettier": ["error"],
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        "js": "never",
+        "jsx": "never",
+        "ts": "never",
+        "tsx": "never"
+      }
+   ],
+    "import/prefer-default-export": "off",
+    "prefer-destructuring": "off",
+    "prefer-template": "off",
+    "react/prop-types": "off",
+    "react/destructuring-assignment": "off",
+    "no-console": "off"
+  },
+};
