a435364defd9e1d9d7eb48259b3e2e96c4ac0929
Tom French <tom@tomfren.ch>
2020-07-01 13:37:27 +0100

    fix: add missing exports of hooks

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 export { default as useBalance } from "./Balance";
 export { default as useBlockNumber } from "./BlockNumber";
 export { default as useContractReader } from "./ContractReader";
+export { useLookupAddress, useResolveEnsName } from "./ENS";
 export { default as useEventListener } from "./EventListener";
+export { default as useEventReader } from "./EventReader";
 export { default as useNonce } from "./Nonce";
 export { default as usePoller } from "./Poller";
 export { default as useTimestamp } from "./Timestamp";
 export { default as useTokenBalance } from "./TokenBalance";
+export { default as useUserAddress } from "./UserAddress";
