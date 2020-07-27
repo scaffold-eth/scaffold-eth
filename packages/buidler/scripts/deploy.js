418d047496cd09fdcabc44c6d0dc9d68ad0402e8
Tom French <tom@tomfren.ch>
2020-07-27 18:33:22 +0100

    refactor: neaten up deployment and publishing code

--- packages/buidler/scripts/deploy.js
+++ packages/buidler/scripts/deploy.js
@@ -1,6 +1,51 @@
 const fs = require("fs");
 const chalk = require("chalk");
 
+async function deploy(name, _args) {
+  const args = _args || []
+
+  console.log(`📄 ${name}`);
+  const contractArtifacts = artifacts.require(name);
+  const contract = await contractArtifacts.new(...args);
+  console.log(
+    chalk.cyan(name),
+    "deployed to:",
+    chalk.magenta(contract.address)
+  );
+  fs.writeFileSync(`artifacts/${name}.address`, contract.address);
+  console.log("\n");
+  return contract;
+}
+
+const isSolidity = fileName => fileName.indexOf(".sol") >= 0 && fileName.indexOf(".swp.") < 0
+
+function readArgumentsFile (contractName) {
+  let args = [];
+  try {
+    const argsFile = `./contracts/${contractName}.args`;
+    if (fs.existsSync(argsFile)) {
+      args = JSON.parse(fs.readFileSync(argsFile));
+    }
+  } catch (e) {
+    console.log(e);
+  }
+  
+  return args
+}
+
+async function autoDeploy() {
+  const contractList = fs.readdirSync("./contracts");
+  contractList
+    .filter(fileName => isSolidity(fileName))
+    .forEach(async fileName => {
+      const contractName = fileName.replace(".sol", "");
+      const args = readArgumentsFile(contractName)
+      await deploy(contractName, args);
+    }
+  )
+}
+
+
 async function main() {
   console.log("📡 Deploy \n");
   // auto deploy to read contract directory and deploy them all (add ".args" files for arguments)
@@ -11,49 +56,10 @@ async function main() {
   // const examplePriceOracle = await deploy("ExamplePriceOracle")
   // const smartContractWallet = await deploy("SmartContractWallet",[exampleToken.address,examplePriceOracle.address])
 }
+
 main()
   .then(() => process.exit(0))
   .catch((error) => {
     console.error(error);
     process.exit(1);
   });
-
-async function deploy(name, _args) {
-  let args = [];
-  if (_args) {
-    args = _args;
-  }
-  console.log("📄 " + name);
-  const contractArtifacts = artifacts.require(name);
-  const contract = await contractArtifacts.new(...args);
-  console.log(
-    chalk.cyan(name),
-    "deployed to:",
-    chalk.magenta(contract.address)
-  );
-  fs.writeFileSync("artifacts/" + name + ".address", contract.address);
-  console.log("\n");
-  return contract;
-}
-
-async function autoDeploy() {
-  const contractList = fs.readdirSync("./contracts");
-  for (const c in contractList) {
-    if (
-      contractList[c].indexOf(".sol") >= 0 &&
-      contractList[c].indexOf(".swp.") < 0
-    ) {
-      const name = contractList[c].replace(".sol", "");
-      let args = [];
-      try {
-        const argsFile = "./contracts/" + name + ".args";
-        if (fs.existsSync(argsFile)) {
-          args = JSON.parse(fs.readFileSync(argsFile));
-        }
-      } catch (e) {
-        console.log(e);
-      }
-      await deploy(name, args);
-    }
-  }
-}
--- packages/buidler/scripts/publish.js
+++ packages/buidler/scripts/publish.js
@@ -3,9 +3,32 @@ const chalk = require("chalk");
 const bre = require("@nomiclabs/buidler");
 
 const contractDir = "./contracts";
+const publishDir = "../react-app/src/contracts";
+
+
+function publishContract(contractName) {
+  console.log(
+    "Publishing",
+    chalk.cyan(contractName),
+    "to",
+    chalk.yellow(publishDir)
+  );
+  try {
+    let contract = fs.readFileSync(`${bre.config.paths.artifacts}/${contractName}.json`).toString();
+    const address = fs.readFileSync(`${bre.config.paths.artifacts}/${contractName}.address`).toString();
+    contract = JSON.parse(contract);
+    fs.writeFileSync(`${publishDir}/${contractName}.address.js`, `module.exports = "${address}";`);
+    fs.writeFileSync(`${publishDir}/${contractName}.abi.js`, `module.exports = "${JSON.stringify(contract.abi, null, 2)}";`);
+    fs.writeFileSync(`${publishDir}/${contractName}.bytecode.js`, `module.exports = "${contract.bytecode}";`);
+
+    return true
+  } catch (e) {
+    console.log(e);
+    return false
+  }
+}
 
 async function main() {
-  const publishDir = "../react-app/src/contracts";
   if (!fs.existsSync(publishDir)) {
     fs.mkdirSync(publishDir);
   }
@@ -13,46 +36,13 @@ async function main() {
   fs.readdirSync(contractDir).forEach((file) => {
     if (file.indexOf(".sol") >= 0) {
       const contractName = file.replace(".sol", "");
-      console.log(
-        "Publishing",
-        chalk.cyan(contractName),
-        "to",
-        chalk.yellow(publishDir)
-      );
-      try {
-        let contract = fs
-          .readFileSync(
-            bre.config.paths.artifacts + "/" + contractName + ".json"
-          )
-          .toString();
-        const address = fs
-          .readFileSync(
-            bre.config.paths.artifacts + "/" + contractName + ".address"
-          )
-          .toString();
-        contract = JSON.parse(contract);
-        fs.writeFileSync(
-          publishDir + "/" + contractName + ".address.js",
-          'module.exports = "' + address + '"' + ";"
-        );
-        fs.writeFileSync(
-          publishDir + "/" + contractName + ".abi.js",
-          "module.exports = " + JSON.stringify(contract.abi) + ";"
-        );
-        fs.writeFileSync(
-          publishDir + "/" + contractName + ".bytecode.js",
-          'module.exports = "' + contract.bytecode + '"' + ";"
-        );
+      // Add contract to list if publishing is successful
+      if (publishContract(contractName)) {
         finalContractList.push(contractName);
-      } catch (e) {
-        console.log(e);
       }
     }
   });
-  fs.writeFileSync(
-    publishDir + "/contracts.js",
-    "module.exports = " + JSON.stringify(finalContractList) + ";"
-  );
+  fs.writeFileSync(`${publishDir}/contracts.js`,`module.exports = ${JSON.stringify(finalContractList)};`);
 }
 main()
   .then(() => process.exit(0))
