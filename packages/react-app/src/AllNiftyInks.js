9d69fa79d1bd2d4ca82c0b41f0eeb1281a4bcb8e
Austin Griffith <austin@concurrence.io>
2020-08-17 15:21:09 -0600

    setup a somewhat reasonable way to blacklist bad or broken IPFS imnages

--- packages/react-app/src/AllNiftyInks.js
+++ packages/react-app/src/AllNiftyInks.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import { Row, Col, Avatar, Empty, Space, Spin, Badge } from 'antd';
 import { useEventListener } from "./hooks"
-import { getFromIPFS } from "./helpers"
+import { getFromIPFS, isBlacklisted } from "./helpers"
 import { Loader } from "./components"
 import StackGrid from "react-stack-grid";
 
-const MAX_FRONT_PAGE_DISPLAY = 24
-const BATCH_DOWNLOAD = 6
+const MAX_FRONT_PAGE_DISPLAY = 64
+const LOADERS_TO_SHOW = 16
+const BATCH_DOWNLOAD = 3
 
 export default function NftyWallet(props) {
   //const [allInks, setAllInks] = useState()
@@ -24,7 +25,7 @@ export default function NftyWallet(props) {
         props.totalInks.toString() !== lastStreamCount
       ) {
         let inksToShow = Math.min(MAX_FRONT_PAGE_DISPLAY, props.totalInks.toString())
-        let allInks = new Array(inksToShow).fill({})
+        let allInks = new Array(Math.min(LOADERS_TO_SHOW, props.totalInks.toString())).fill({})
         setLastStreamCount(props.totalInks.toString())
 
         const getInkImages = async (e) => {
@@ -38,25 +39,36 @@ export default function NftyWallet(props) {
 
         const loadStream = async () => {
           if(inkCreations) {
-            let mostRecentInks = inkCreations.slice(-inksToShow).reverse()
+            let mostRecentInks = inkCreations.reverse()
             let promises = []
+            let hashesForDebugging = []
+            let skips = 0
+            let newIndex = 0
             for(var i = 0; i < inksToShow; i++){
-              try {
-                promises.push(getInkImages(mostRecentInks[i]))
-              } catch (e) {console.log(e)}
+              if(!isBlacklisted(mostRecentInks[i]['jsonUrl'])){
+                try {
+                  promises.push(getInkImages(mostRecentInks[i]))
+                  hashesForDebugging.push(mostRecentInks[i]['jsonUrl'])
+                } catch (e) {console.log("EEEERRRRR",e)}
+              }else{
+                skips++
+              }
               if(promises.length>=BATCH_DOWNLOAD){
-                for(var p = 0; p <= promises.length; p++){
+                for(var p = 0; p <= (promises.length-skips); p++){
                   let result
                   try {
                     result = await promises[p]
                     if(result){
-                      let thisIndex = i-(promises.length-1)+p
-                      allInks[thisIndex] = result
+                      //let thisIndex = i-(promises.length-1)+p-skips
+                      //console.log("thisIndex",thisIndex)
+                      allInks[newIndex++] = result
                       setAllInksArray(allInks)
                     }
-                  } catch (e) {console.log(e)}
+                  } catch (e) {console.log("FAILED TO LOAD FROM IPFS =====>",hashesForDebugging[p])}
                 }
                 promises = []
+                hashesForDebugging = []
+                skips = 0
               }
             }
           }
@@ -75,8 +87,9 @@ export default function NftyWallet(props) {
            gutterWidth={32}
          >
           {allInksArray.map(item =>{
+            //console.log("item",item)
             return (
-              <div key={item['id']}>
+              <div key={item['id']} ipfsHash={item['jsonUrl']}>
                 {item['image']?/*<Badge style={{ backgroundColor: '#2db7f5' }} count={item['likes']}>*/<img src={item['image']} alt={item['name']} onClick={() => props.showInk(item['url'])} width='120' height='120'/>/*</Badge>*/:<Avatar size={120} style={{ backgroundColor: '#FFFFFF' }} icon={<Spin style={{opacity:0.125}} size="large" />} />}
               </div>
             )
