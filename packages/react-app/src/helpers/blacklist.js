b03b71b7b9423805ae8e8c0cff045291333d273b
Austin Griffith <austin@concurrence.io>
2020-09-01 13:14:33 -0600

    first swasty blacklist and pen width!

--- packages/react-app/src/helpers/blacklist.js
+++ packages/react-app/src/helpers/blacklist.js
@@ -7,7 +7,8 @@ const blacklist = [
   "QmY8h1Cgt5d7TueU1QPTdd4ZLMTCZ2FdfhiBJYwYorWKK1",
   "QmTmdMygNFZ3Ukzr7KDPmXsj6rj59AjenLLiBuU8mVxp2g",
   "Qmbpm61GFdoU13nCu6wP1mejFDK9BQ9gKVUmmTQiY77m6x",
-  "QmNnoHKiWHnfqQ4CjLgbJEeFvFqC1h5Be4Dwaa25t6TJXs"
+  "QmNnoHKiWHnfqQ4CjLgbJEeFvFqC1h5Be4Dwaa25t6TJXs",
+  "QmQZmDkMiv8EsrMwmb8Wt5REEp3SmoMFJJ6UAL8YNsKFnd"
 ]
 
 export default function isBlacklisted(hash) {
