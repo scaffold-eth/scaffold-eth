7619beeb78d1826176a4414077dcf73a36d99798
PatrickAlphaC <collinspatrickkevin@gmail.com>
2020-09-02 12:12:07 -0400

    CL contracts

---
+++ packages/buidler/contracts/APICall.sol
@@ -0,0 +1,64 @@
+pragma solidity >=0.6.0 <0.7.0;
+
+import "@nomiclabs/buidler/console.sol";
+import "@chainlink/contracts/src/v0.6/ChainlinkClient.sol";
+
+contract APICall is ChainlinkClient {
+    uint256 public ethereumPrice;
+
+    address private oracle;
+    bytes32 private jobId;
+    uint256 private fee;
+
+    /**
+     * Network: Rinkeby
+     * Oracle: Chainlink - 0x7AFe1118Ea78C1eae84ca8feE5C65Bc76CcF879e
+     * Job ID: Chainlink - 6d1bfe27e7034b1d87b5270556b17277
+     * Fee: 0.1 LINK
+     */
+    constructor() public {
+        setPublicChainlinkToken();
+        oracle = 0x7AFe1118Ea78C1eae84ca8feE5C65Bc76CcF879e;
+        jobId = "6d1bfe27e7034b1d87b5270556b17277";
+        fee = 0.1 * 10**18; // 0.1 LINK
+    }
+
+    /**
+     * Create a Chainlink request to retrieve API response, find the target price
+     * data, then multiply by 100 (to remove decimal places from price).
+     */
+    function requestEthereumPrice() public returns (bytes32 requestId) {
+        Chainlink.Request memory request = buildChainlinkRequest(
+            jobId,
+            address(this),
+            this.fulfill.selector
+        );
+
+        // Set the URL to perform the GET request on
+        request.add(
+            "get",
+            "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD"
+        );
+
+        // Set the path to find the desired data in the API response, where the response format is:
+        // {"USD":243.33}
+        request.add("path", "USD");
+
+        // Multiply the result by 100 to remove decimals
+        request.addInt("times", 100);
+
+        // Sends the request
+        return sendChainlinkRequestTo(oracle, request, fee);
+    }
+
+    /**
+     * Receive the response in the form of uint256
+     */
+
+    function fulfill(bytes32 _requestId, uint256 _price)
+        public
+        recordChainlinkFulfillment(_requestId)
+    {
+        ethereumPrice = _price;
+    }
+}
---
+++ packages/buidler/contracts/PriceFeed.sol
@@ -0,0 +1,33 @@
+pragma solidity >=0.6.0 <0.7.0;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+
+contract PriceFeed {
+    AggregatorV3Interface internal priceFeed;
+
+    /**
+     * Network: Rinkeby
+     * Aggregator: ETH/USD
+     * Address: 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+     * Find all addresses at https://docs.chain.link/docs/reference-contracts#rinkeby
+     */
+    constructor() public {
+        priceFeed = AggregatorV3Interface(
+            0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+        );
+    }
+
+    /**
+     * Returns the latest price
+     */
+    function getLatestPrice() public view returns (int256) {
+        (
+            uint80 roundID,
+            int256 price,
+            uint256 startedAt,
+            uint256 timeStamp,
+            uint80 answeredInRound
+        ) = priceFeed.latestRoundData();
+        return price;
+    }
+}
