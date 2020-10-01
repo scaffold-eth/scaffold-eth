4947868845fdffeead810b33217567d13abfae19
Austin Griffith <austin@concurrence.io>
2020-10-01 11:08:03 -0600

    fix load glitch with ens

--- packages/react-app/src/components/Address.jsx
+++ packages/react-app/src/components/Address.jsx
@@ -33,7 +33,7 @@ export default function Address(props) {
 
   let displayAddress = props.value.substr(0, 6);
 
-  if (ens) {
+  if (ens && ens.indexOf("0x")<0) {
     displayAddress = ens;
   } else if (props.size === "short") {
     displayAddress += "..." + props.value.substr(-4);
