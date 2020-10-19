ca17a635fb3301866282f5980d720d010703177c
Austin Griffith <austin@concurrence.io>
2020-10-19 12:18:18 -0600

    clean up smart contract

--- packages/buidler/contracts/YourContract.sol
+++ packages/buidler/contracts/YourContract.sol
@@ -4,18 +4,13 @@ import "@nomiclabs/buidler/console.sol";
 
 contract YourContract {
 
+  event SetPurpose(address sender, string purpose);
+
+  string public purpose;
+
   constructor() public {
     setPurpose("🛠 Programming Unstoppable Money");
   }
-
-  event SetPurpose(address sender, string purpose);
-
-  uint8 public count = 1;
-  function dec() public {
-    count -= 1;
-  }
-
-  string public purpose;
 
   function setPurpose(string memory newPurpose) public {
     purpose = newPurpose;
