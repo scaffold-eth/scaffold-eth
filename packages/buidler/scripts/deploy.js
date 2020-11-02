9b201798178ae8155cbe0d520833068933bd9cd0
nazariyv <nazariy@inbox.ru>
2020-11-02 21:00:15 +0000

    [+] refactored the deploy script

--- packages/buidler/scripts/deploy.js
+++ packages/buidler/scripts/deploy.js
@@ -1,81 +1,112 @@
+/* eslint no-use-before-define: "warn" */
 const fs = require("fs");
 const chalk = require("chalk");
 const { config, ethers } = require("@nomiclabs/buidler");
 const { utils } = require("ethers");
+const R = require("ramda");
 
-async function main() {
-  console.log("📡 Deploy \n");
+const main = async () => {
+  // ? Tip: if on VSCode, install "Better Comments" extension
+  console.log("\n\n 📡 Deploying...\n");
 
-  // auto deploy to read contract directory and deploy them all (add ".args" files for arguments)
+  // ! AUTO DEPLOY
+  // * -----------
+  // to read contract directory and deploy them all (add ".args" files for arguments)
   await autoDeploy();
-  // OR
+
+  // ! OR CUSTOM DEPLOY
+  // * ----------------
   // custom deploy (to use deployed addresses dynamically for example:)
   // const exampleToken = await deploy("ExampleToken")
   // const examplePriceOracle = await deploy("ExamplePriceOracle")
   // const smartContractWallet = await deploy("SmartContractWallet",[exampleToken.address,examplePriceOracle.address])
-}
 
-
-
-
-
-async function deploy(name, _args) {
-  const args = _args || [];
-
-  console.log(` 🛰  Deploying ${name}`);
-  const contractArtifacts = await ethers.getContractFactory(name);
-  const contract = await contractArtifacts.deploy(...args);
-  console.log(" 📄",
-    chalk.cyan(name),
-    "deployed to:",
-    chalk.magenta(contract.address),
-    "\n"
+  console.log(
+    " 💾  Artifacts (address, abi, and args) saved to: ",
+    chalk.blue("packages/buidler/artifacts/"),
+    "\n\n"
   );
-  fs.writeFileSync(`artifacts/${name}.address`, contract.address);
-  console.log("💾  Artifacts (address, abi, and args) saved to: ",chalk.blue("packages/buidler/artifacts/"),"\n")
-  return contract;
-}
+};
 
+const autoDeploy = async () => {
+  const allDeployed = [];
+  const contractList = fs
+    .readdirSync(config.paths.sources)
+    .filter((fileName) => isSolidity(fileName));
+
+  // loop through each solidity file from config.path.sources and deploy it
+  // abi encode any args, if found
+  // ! do not use .forEach in place of this loop
+  for (let i=0; i < contractList.length; i++) {
+    const file = contractList[i];
+    const contractName = file.replace(".sol", "");
+    const contractArgs = readArgsFile(contractName);
+    const deployed = await deploy(contractName, contractArgs);
+
+    allDeployed.push(deployed);
+  }
+
+  return allDeployed;
+};
+
+const deploy = async (contractName, _args) => {
+  console.log(` 🛰  Deploying: ${contractName}`);
+
+  const contractArgs = _args || [];
+  const contractArtifacts = await ethers.getContractFactory(contractName);
+  const deployed = await contractArtifacts.deploy(...contractArgs);
+  const encoded = abiEncodeArgs(deployed, contractArgs);
+  fs.writeFileSync(`artifacts/${contractName}.address`, deployed.address);
+
+  console.log(
+    " 📄",
+    chalk.cyan(contractName),
+    "deployed to:",
+    chalk.magenta(deployed.address),
+  );
+
+  if (!encoded || encoded.length <= 2) return deployed;
+  fs.writeFileSync(`artifacts/${contractName}.args`, encoded.slice(2));
+
+  return deployed;
+};
+
+// ------ utils -------
+
+// abi encodes contract arguments
+// useful when you want to manually verify the contracts
+// for example, on Etherscan
+const abiEncodeArgs = (deployed, contractArgs) => {
+  // not writing abi encoded args if this does not pass
+  if (
+    !contractArgs ||
+    !deployed ||
+    !R.hasPath(["interface", "deploy"], deployed)
+  ) {
+    return "";
+  }
+  const encoded = utils.defaultAbiCoder.encode(
+    deployed.interface.deploy.inputs,
+    contractArgs
+  );
+  return encoded;
+};
+
+// checks if it is a Solidity file
 const isSolidity = (fileName) =>
   fileName.indexOf(".sol") >= 0 && fileName.indexOf(".swp.") < 0;
 
-function readArgumentsFile(contractName) {
+const readArgsFile = (contractName) => {
   let args = [];
   try {
     const argsFile = `./contracts/${contractName}.args`;
-    if (fs.existsSync(argsFile)) {
-      args = JSON.parse(fs.readFileSync(argsFile));
-    }
+    if (!fs.existsSync(argsFile)) return args;
+    args = JSON.parse(fs.readFileSync(argsFile));
   } catch (e) {
     console.log(e);
   }
-
   return args;
-}
-
-async function autoDeploy() {
-  const contractList = fs.readdirSync(config.paths.sources);
-  return contractList
-    .filter((fileName) => isSolidity(fileName))
-    .reduce((lastDeployment, fileName) => {
-      const contractName = fileName.replace(".sol", "");
-      const args = readArgumentsFile(contractName);
-
-      // Wait for last deployment to complete before starting the next
-      return lastDeployment.then((resultArrSoFar) =>
-        deploy(contractName, args).then((result,b,c) => {
-
-          if(args&&result&&result.interface&&result.interface.deploy){
-            let encoded = utils.defaultAbiCoder.encode(result.interface.deploy.inputs,args)
-            fs.writeFileSync(`artifacts/${contractName}.args`, encoded);
-          }
-
-          return [...resultArrSoFar, result]
-        })
-      );
-    }, Promise.resolve([]));
-}
-
+};
 
 main()
   .then(() => process.exit(0))
