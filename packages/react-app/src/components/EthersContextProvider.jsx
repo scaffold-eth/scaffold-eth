1a63715e6e0190f833921eaa940b8ae0316fba0d
steven <steven.nevins.js@gmail.com>
2020-12-15 11:51:33 -0500

    using gasPrice in example UI view

--- packages/react-app/src/components/EthersContextProvider.jsx
+++ packages/react-app/src/components/EthersContextProvider.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 import { EthersContext } from "../context";
+import { useGasPrice } from "../hooks";
 
-export default function EthersContextProvider({ value = { provider: 1 }, children }) {
+export default function EthersContextProvider({ children }) {
+
+  const gasPrice = useGasPrice("fast");
+  const provider = 1
+  const value = {
+    gasPrice,
+    provider
+  }
   return <EthersContext.Provider value={value}>{children}</EthersContext.Provider>;
 }
