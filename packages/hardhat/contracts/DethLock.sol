3ea93fffe5609f86a6c83ff6dd26d84fdfeadda1
DrHongos <lucasziegenfuhs@gmail.com>
2021-01-29 05:17:41 -0300

    Components for inputs, new contracts

---
+++ packages/hardhat/contracts/DethLock.sol
@@ -0,0 +1,22 @@
+pragma solidity 0.8.0;
+import './inherited/Accountable.sol';
+
+// SPDX-License-Identifier: UNLICENSED
+
+contract DethLock is Accountable {
+
+    // Dethlock core: ------------------------------------
+    struct will{
+        address payable owner;
+        address payable beneficiary;
+        uint ethBalance;
+        address payable tokenAddress;
+        uint tokenBalance;
+        uint deadline;
+    }
+
+    will[] _masterWillList;
+    mapping (address => uint[]) _owners;
+    mapping (address => uint[]) _beneficiaries;
+
+}
---
+++ packages/hardhat/contracts/Noun.sol
@@ -0,0 +1,42 @@
+pragma solidity 0.8.0;
+import "hardhat/console.sol";
+import './DethLock.sol';
+
+// SPDX-License-Identifier: UNLICENSED
+
+contract Noun is DethLock {
+
+    constructor(address _verbAddress) {
+        _address['verbAddress'] = _verbAddress;
+        _uint['willsCreated'] = 0;
+        _bool['initialized'] = true;
+        console.log(msg.sender,"Noun constructed.");
+    }
+
+    function upgrade(address _newAddress) public{
+        _address['verbAddress'] = _newAddress;
+        console.log(msg.sender,"Verb address updated to:",_newAddress);
+    }
+
+    // Redirect everything to functional contract.
+    fallback() payable external {
+        address implementation = _address['verbAddress'];
+        require(_address['verbAddress'] != address(0));
+        bytes memory data = msg.data;
+
+        console.log(msg.sender,"Forwarding.");
+
+        assembly{
+            let result := delegatecall(gas(), implementation, add(data, 0x20), mload(data),0,0)
+            let size := returndatasize()
+            let ptr := mload(0x40)
+            returndatacopy(ptr,0,size)
+            switch result
+            case 0 {revert(ptr,size)}
+            default {return(ptr,size)}
+        }
+
+    }
+
+
+}
