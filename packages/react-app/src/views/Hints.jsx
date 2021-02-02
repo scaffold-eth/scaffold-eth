f46ed1f96cb16244f5e5a0ac300e411980088b5c
Orbit Mechanic <56613757+orbitmechanic@users.noreply.github.com>
2021-02-01 16:02:27 -0800

    React Working.  React-Bootstrap installed.

--- packages/react-app/src/views/Hints.jsx
+++ packages/react-app/src/views/Hints.jsx
@@ -1,21 +1,21 @@
-/* eslint-disable jsx-a11y/accessible-emoji */
-
 import React from "react";
-import { formatEther } from "@ethersproject/units";
-import { Address, AddressInput } from "../components";
+// import { formatEther } from "@ethersproject/units";
+// import { Address, AddressInput, TokenList} from "../components";
+// import { fetchTokenList } from '../helpers/TokenList';
 
-
-export default function Hints({yourLocalBalance, mainnetProvider, price, address }) {
+export default function Hints() {
 
   return (
     <div>
+      <br />      <br />
+
       <div style={{ margin: 32 }}>
         <span style={{ marginRight: 8 }}>👷</span>
         Open 4 terminals in root folder<br />
         yarn run chain --- GANACHE local network<br />
         yarn run deploy / Copy 'packages/react-app/CopyToNoun.abi.js' content into '../react-app/src/contracts/Noun.abi.js' (if there have been changes in contract, must recopy ABI from Verb) - Copy also 'the-graph-NounABI' content into 'subgraph/abis/Nouns.json' --- Artifacts and ABIs generation<br />
-        rm -rf docker/graph-node/data/ - yarn graph-run-node (linux users: sudo it and had changed host.internal.docker in root/docker/docker-compose.yml) --- Docker node <br />
-         - (if first time (or rm'ed data): yarn graph-create-local) - yarn graph-ship-local --- Create/Launch Docker graph <br />
+        rm -rf docker/graph-node/data/ - yarn graph-run-node (mac/windows users: change host.internal.docker in root/docker/docker-compose.yml (refer to original line in scaffold-eth)) --- Docker node <br />
+         - (if first time (or rm'ed data): yarn graph-create-local) - yarn graph-ship-local (every update of the-graph code) --- Create/Launch Docker graph <br />
         yarn start --- Server<br />
         <br />
         You should now have in /YourContract (frontpage) 4 contracts with its functions <br />
@@ -28,55 +28,21 @@ export default function Hints({yourLocalBalance, mainnetProvider, price, address
         In Manage you can see a simple query'er with simple interaction, filters to come!<br />
         Index in /Manage start from 1, if you need to interact with the will remember to index-1<br />
         --------------------------------------------------------------------------------------<br/>
-        PROBLEMS<br />
+        <h3>PROBLEMS</h3><br />
         Any reload on /Create will have a problem reading Stoodges coins addresses (this wont happen in the wild) go to any other and come back<br />
-        TODO<br/>
+        <h3>TODO</h3><br/>
         Stoodges tokens not working (compiled ERC20 with pragma 8 required to modify transfer and approval functions(error is in those fn))<br />
-        ERC721??<br />
+        ERC721?? create function for token handling and receiver<br />
         Change /Create, assign wills data in props for updates, if null, then create<br/>
-        Finish thegraph events mappings<br />
+        Finish thegraph events mappings (setDeadline and setBeneficiaries should emit index)<br />
         -------------------------------------<br/>
         Then.. <br/>
         Multi pragma compiler for contracts with hardhat
          <br />
 
-
-      <div style={{ marginTop: 32 }}>
-        <span style={{ marginRight: 8 }}>⚙️</span>
-        build your app with
-        <span style={{ marginLeft: 4, backgroundColor: "#f1f1f1", padding: 4, borderRadius: 4, fontWeight: "bolder" }}>
-          yarn run build
-        </span>
       </div>
 
-      <div style={{ marginTop: 32 }}>
-        <span style={{ marginRight: 8 }}>🚢</span>
-        ship it!
-        <span style={{ marginLeft: 4, backgroundColor: "#f1f1f1", padding: 4, borderRadius: 4, fontWeight: "bolder" }}>
-          yarn run surge
-        </span>
-        or
-        <span style={{ marginLeft: 4, backgroundColor: "#f1f1f1", padding: 4, borderRadius: 4, fontWeight: "bolder" }}>
-          yarn run s3
-        </span>
-        or
-        <span style={{ marginLeft: 4, backgroundColor: "#f1f1f1", padding: 4, borderRadius: 4, fontWeight: "bolder" }}>
-          yarn run ipfs
-        </span>
-      </div>
 
-      <div style={{ marginTop: 32 }}>
-        <span style={{ marginRight: 8 }}>💬</span>
-        for support, join this
-        <span style={{ marginLeft: 4, backgroundColor: "#f9f9f9", padding: 4, borderRadius: 4, fontWeight: "bolder" }}>
-          <a target="_blank" rel="noopener noreferrer" href="https://t.me/joinchat/KByvmRe5wkR-8F_zz6AjpA">
-            Telegram Chat
-          </a>
-        </span>
-      </div>
-      <div style={{ padding: 128 }}>
-        🛠 Check out your browser's developer console for more... (inpect -> console) 🚀
-      </div>
     </div>
   );
 }
