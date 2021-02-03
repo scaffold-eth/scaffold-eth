29a7f406c219e13e6df51bcd66c3374ef73a99aa
DrHongos <lucasziegenfuhs@gmail.com>
2021-02-02 22:22:25 -0300

    the-graph
    Schema, mapping and handlers adapted to wills

--- packages/subgraph/src/mapping.ts
+++ packages/subgraph/src/mapping.ts
@@ -1,13 +1,19 @@
 import { BigInt, Address } from "@graphprotocol/graph-ts"
 import {
   Noun,
+  NewWillCreated,
   WillCreated,
   WillFunded,
-  WillDeFunded
+  WillDeFunded,
+  TokensDepositedToWill,
+  TokensWithdrawnFromWill,
+  BenifitedTokens,
+  BeneficiarySet,
+  DeadlineUpdated
 } from "../generated/Noun/Noun"
 import { Will } from "../generated/schema"
 
-export function handleWillCreated(event: WillCreated): void {
+export function handleNewWillCreated(event: NewWillCreated): void {
   let id = event.params.index.minus(BigInt.fromI32(1)).toHexString()
   let will = new Will(id)
 
@@ -15,6 +21,8 @@ export function handleWillCreated(event: WillCreated): void {
   will.beneficiary = event.params.beneficiary
   will.deadline = event.params.deadline
   will.index = event.params.index
+  will.token = event.params.tokenAddress
+  // will.tokenBalance = 0
   will.value = event.params.value
   will.transactionHash = event.transaction.hash.toHex()
   will.save()
@@ -31,5 +39,46 @@ export function handleWillFunded(event: WillFunded): void {
   let id = event.params.index.toHexString()
   let will = Will.load(id)
   will.value = will.value.plus(event.params.amount)
+  will.save()
+}
+
+
+export function handleTokensDepositedToWill(event: TokensDepositedToWill): void {
+  let id = event.params.willIndex.toHexString()
+  let will = Will.load(id)
+  if(will.token == null){
+    will.token = event.params.tokenAddress
+  }
+  will.tokenBalance = will.tokenBalance.plus(event.params.value)
+  will.save()
+}
+
+export function handleTokensWithdrawnFromWill(event: TokensWithdrawnFromWill): void {
+  let id = event.params.willIndex.toHexString()
+  let will = Will.load(id)
+  will.tokenBalance = will.tokenBalance.minus(event.params.value)
+  will.save()
+}
+
+export function handleBenifitedTokens(event: BenifitedTokens): void {
+  let id = event.params.willIndex.toHexString()
+  let will = Will.load(id)
+  will.tokenBalance = will.tokenBalance.minus(event.params.value)
+  will.save()
+}
+
+
+
+export function handleBeneficiarySet(event: BeneficiarySet): void {
+  let id = event.params.index.toHexString()
+  let will = Will.load(id)
+  will.beneficiary = event.params.beneficiary
+  will.save()
+}
+
+export function handleDeadlineUpdated(event: DeadlineUpdated): void {
+  let id = event.params.index.toHexString()
+  let will = Will.load(id)
+  will.deadline = event.params.new_value
   will.save()
 }
