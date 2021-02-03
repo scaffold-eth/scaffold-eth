e8c9ec8ed24cc155109948cbde6340a661b53d90
DrHongos <lucasziegenfuhs@gmail.com>
2021-02-02 23:04:05 -0300

    Update header
    Just 'cause

--- packages/react-app/src/components/Header.jsx
+++ packages/react-app/src/components/Header.jsx
@@ -3,10 +3,10 @@ import { PageHeader } from "antd";
 
 export default function Header() {
   return (
-    <a href="https://github.com/austintgriffith/scaffold-eth" target="_blank" rel="noopener noreferrer">
+    <a href="https://github.com/orbitmechanic/scaffold-eth/tree/TimeLock" target="_blank" rel="noopener noreferrer">
       <PageHeader
-        title="🏗 scaffold-eth"
-        subTitle="forkable Ethereum dev stack focused on fast product iteration"
+        title=" 🏹 TimeLock/dETHlock ☠️"
+        subTitle="Manage the future of your assets"
         style={{ cursor: "pointer" }}
       />
     </a>
