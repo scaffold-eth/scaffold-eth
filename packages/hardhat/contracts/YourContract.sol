e1a0023a659312a9cea87ac9be6f672aaab6d520
Amogh <amogh-jrules@github.com>
2021-02-11 22:21:58 +0400

    Player turn mechanics

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -7,17 +7,34 @@ import "hardhat/console.sol";
 contract YourContract {
 
   event SetPurpose(address sender, string purpose);
-
+  bool gameIsStarted = false;
   string public purpose = "🛠 Programming Unstoppable Money";
+  mapping(address => uint256) steakedValues;
+  mapping(address => bool) doesPlayerExist;
+  uint256 playerCount = 0;
+  // address[] public players;
+  uint256 currentIndex = 0;
 
-  constructor() public {
-    // what should we do on deploy?
+  constructor(uint256 numberOfPlayers) public {
+    playerCount = numberOfPlayers;
+  }
+  event NewPlayerJoined(address steaker, uint256 value);
+  function steakAndParticipate(address steaker, uint256 value) public {  
+    require(gameIsStarted == false, "Game has already begun");
+    require(doesPlayerExist[steaker] == false, "Player has already staked");
+    require(playerCount>0, "Player Limit is reached, Join us in the next round!");
+
+      playerCount -=1;
+      // players.push(steaker);
+      doesPlayerExist[steaker] == true;
+      emit NewPlayerJoined(steaker, value);
   }
 
-  function setPurpose(string memory newPurpose) public {
-    purpose = newPurpose;
-    console.log(msg.sender,"set purpose to",purpose);
-    emit SetPurpose(msg.sender, purpose);
-  }
+  function generateRandomNumber(address player) public returns (uint256) {
+    // require(player == players[currentIndex], "Its not your turn!");
+    
+    currentIndex+=1;
+    currentIndex%=playerCount;
 
+  }
 }
