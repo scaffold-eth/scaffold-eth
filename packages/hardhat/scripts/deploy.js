98c969df6644c0d29cf93ab0752f4c7c55b8de5f
Austin Griffith <you@example.com>
2021-02-19 11:15:41 -0700

    comment out the deploy lines

--- packages/hardhat/scripts/deploy.js
+++ packages/hardhat/scripts/deploy.js
@@ -11,19 +11,18 @@ const main = async () => {
 
   const yourToken = await deploy("YourToken")
 
-  const vendor = await deploy("Vendor",[ yourToken.address ])
+  //Todo: deploy the vendor
+  //const vendor = await deploy("Vendor",[ yourToken.address ])
 
   //console.log("\n 🏵  Sending all 1000 tokens to the vendor...\n");
-
   //Todo: transfer the tokens to the vendor
-  const result = await yourToken.transfer( vendor.address, utils.parseEther("1000") );
+  //const result = await yourToken.transfer( vendor.address, utils.parseEther("1000") );
 
   //const stakerContract = await deploy("Staker",[ exampleExternalContract.address ]) // <-- add in constructor args like line 14 ^^^
 
-  console.log("\n 🤹  Sending ownership to frontend address...\n")
-
+  //console.log("\n 🤹  Sending ownership to frontend address...\n")
   //ToDo: change address with your burner wallet address vvvv
-  await vendor.transferOwnership( "0xD75b0609ed51307E13bae0F9394b5f63A7f8b6A1" );
+  //await vendor.transferOwnership( "0xD75b0609ed51307E13bae0F9394b5f63A7f8b6A1" );
 
   //const secondContract = await deploy("SecondContract")
 
