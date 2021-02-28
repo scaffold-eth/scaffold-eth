830be108194480b7607607b6774fae77d26ec09c
azf20 <azf20@users.noreply.github.com>
2021-02-28 19:24:03 +0000

    remove unlimited contract

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -227,13 +227,6 @@ function App(props) {
               address={address}
               blockExplorer={blockExplorer}
             />
-            <Contract
-              name="Unlimited"
-              signer={userProvider.getSigner()}
-              provider={localProvider}
-              address={address}
-              blockExplorer={blockExplorer}
-            />
 
 
             { /* uncomment for a second contract:
