77603e19c1c911827145274d044987167b9b897a
Austin Griffith <you@example.com>
2021-03-17 09:18:09 -0600

    add that extra compiler to fix issues when you bring in OZ

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -105,6 +105,15 @@ module.exports = {
             runs: 200
           }
         }
+      },
+      {
+        version: "0.6.7",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
       }
     ],
 
