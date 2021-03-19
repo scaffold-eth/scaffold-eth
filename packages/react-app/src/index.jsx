b9731289d9853bb15c4784acbd5cb5f52a3b2e08
Elliot Blanchard <git@elliotblanchard.com>
2021-03-18 21:41:39 -0400

    Updated index.jsx

--- packages/react-app/src/index.jsx
+++ packages/react-app/src/index.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import "./index.css";
+
+import {createStore, applyMiddleware, compose} from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import rootReducer from "./reducers/manageSendersRecievers";
+
 import App from "./App";
+
 
 import { ThemeSwitcherProvider } from "react-css-theme-switcher";
 
@@ -10,6 +18,10 @@ const themes = {
   dark: `${process.env.PUBLIC_URL}/dark-theme.css`,
   light: `${process.env.PUBLIC_URL}/light-theme.css`,
 };
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+let store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 const prevTheme = window.localStorage.getItem("theme");
 
@@ -21,10 +33,12 @@ const client = new ApolloClient({
 });
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <ThemeSwitcherProvider themeMap={themes} defaultTheme={prevTheme ? prevTheme : "light"}>
-      <App subgraphUri={subgraphUri}/>
-    </ThemeSwitcherProvider>
-  </ApolloProvider>,
+  <Provider store={store}>
+    <ApolloProvider client={client}>
+      <ThemeSwitcherProvider themeMap={themes} defaultTheme={prevTheme ? prevTheme : "light"}>
+        <App subgraphUri={subgraphUri}/>
+      </ThemeSwitcherProvider>
+    </ApolloProvider>
+  </Provider>,
   document.getElementById("root"),
 );
