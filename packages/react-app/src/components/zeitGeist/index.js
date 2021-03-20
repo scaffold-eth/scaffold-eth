251183f6285f506b3a1a5bdc4b08797367b5d8bd
djudjuu <julfaber@gmail.com>
2021-03-20 10:45:16 +0100

    empty component stubs

---
+++ packages/react-app/src/components/zeitGeist/index.js
@@ -0,0 +1,4 @@
+export { default as Main } from "./Main";
+export { default as Timeline } from "./MemCard";
+export { default as GasGauge } from "./MemCardList";
+export { default as BytesStringInput } from "./MemDetails";
