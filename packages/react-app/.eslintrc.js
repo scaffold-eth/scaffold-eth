09404eaa152703ede3d18bf5fa9dae8f0d0f1827
Sören Steiger <soren1@gmx.net>
2021-04-02 12:26:49 +0200

    Update eslint config

--- packages/react-app/.eslintrc.js
+++ packages/react-app/.eslintrc.js
@@ -1,27 +1,37 @@
 module.exports = {
   env: {
-    browser: true,
+    browser: true
   },
-  extends: ["airbnb", 'plugin:prettier/recommended', 'prettier/react'],
-  plugins: ["babel"],
+  extends: ['airbnb', 'plugin:prettier/recommended', 'prettier/react'],
+  plugins: ['babel'],
   rules: {
-    "prettier/prettier": ["error"],
-    "import/extensions": [
-      "error",
-      "ignorePackages",
+    'prettier/prettier': [
+      'error',
       {
-        "js": "never",
-        "jsx": "never",
-        "ts": "never",
-        "tsx": "never"
+        semi: false,
+        singleQuote: true,
+        jsxSingleQuote: true,
+        'space-before-function-paren': [2, 'never'],
+        'object-curly-newline': false
       }
-   ],
-    "import/prefer-default-export": "off",
-    "prefer-destructuring": "off",
-    "prefer-template": "off",
-    "react/prop-types": "off",
-    "react/destructuring-assignment": "off",
-    "no-console": "off",
-    "jsx-a11y/accessible-emoji": ["off"]
-  },
-};
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never'
+      }
+    ],
+    'import/prefer-default-export': 'off',
+    'prefer-destructuring': 'off',
+    'prefer-template': 'off',
+    'react/prop-types': 'off',
+    'react/destructuring-assignment': 'off',
+    'no-unused-vars': 1,
+    'no-console': 'off',
+    'jsx-a11y/accessible-emoji': ['off']
+  }
+}
