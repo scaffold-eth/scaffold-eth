dde8e39c4c3b326683dcab88f05957bdf531fefc
Sören Steiger <soren1@gmx.net>
2021-04-02 19:41:09 +0200

    Fix eslint errors in hardhat package

--- packages/hardhat/scripts/publish.js
+++ packages/hardhat/scripts/publish.js
@@ -1,102 +1,107 @@
-const fs = require("fs");
-const chalk = require("chalk");
-const bre = require("hardhat");
+const fs = require('fs')
+const chalk = require('chalk')
+const bre = require('hardhat')
 
-const publishDir = "../react-app/src/contracts";
-const graphDir = "../subgraph"
+const publishDir = '../react-app/src/contracts'
+const graphDir = '../subgraph'
 
 function publishContract(contractName) {
   console.log(
-    " 💽 Publishing",
+    ' 💽 Publishing',
     chalk.cyan(contractName),
-    "to",
+    'to',
     chalk.gray(publishDir)
-  );
+  )
   try {
     let contract = fs
-      .readFileSync(`${bre.config.paths.artifacts}/contracts/${contractName}.sol/${contractName}.json`)
-      .toString();
+      .readFileSync(
+        `${bre.config.paths.artifacts}/contracts/${contractName}.sol/${contractName}.json`
+      )
+      .toString()
     const address = fs
       .readFileSync(`${bre.config.paths.artifacts}/${contractName}.address`)
-      .toString();
-    contract = JSON.parse(contract);
-    let graphConfigPath = `${graphDir}/config/config.json`
+      .toString()
+    contract = JSON.parse(contract)
+    const graphConfigPath = `${graphDir}/config/config.json`
     let graphConfig
     try {
       if (fs.existsSync(graphConfigPath)) {
-        graphConfig = fs
-          .readFileSync(graphConfigPath)
-          .toString();
+        graphConfig = fs.readFileSync(graphConfigPath).toString()
       } else {
         graphConfig = '{}'
       }
-      } catch (e) {
-        console.log(e)
-      }
+    } catch (e) {
+      console.log(e)
+    }
 
     graphConfig = JSON.parse(graphConfig)
-    graphConfig[contractName + "Address"] = address
+    graphConfig[contractName + 'Address'] = address
     fs.writeFileSync(
       `${publishDir}/${contractName}.address.js`,
       `module.exports = "${address}";`
-    );
+    )
     fs.writeFileSync(
       `${publishDir}/${contractName}.abi.js`,
       `module.exports = ${JSON.stringify(contract.abi, null, 2)};`
-    );
+    )
     fs.writeFileSync(
       `${publishDir}/${contractName}.bytecode.js`,
       `module.exports = "${contract.bytecode}";`
-    );
+    )
 
-    const folderPath = graphConfigPath.replace("/config.json","")
-    if (!fs.existsSync(folderPath)){
-      fs.mkdirSync(folderPath);
+    const folderPath = graphConfigPath.replace('/config.json', '')
+    if (!fs.existsSync(folderPath)) {
+      fs.mkdirSync(folderPath)
     }
-    fs.writeFileSync(
-      graphConfigPath,
-      JSON.stringify(graphConfig, null, 2)
-    );
+    fs.writeFileSync(graphConfigPath, JSON.stringify(graphConfig, null, 2))
     fs.writeFileSync(
       `${graphDir}/abis/${contractName}.json`,
       JSON.stringify(contract.abi, null, 2)
-    );
+    )
 
-    console.log(" 📠 Published "+chalk.green(contractName)+" to the frontend.")
+    console.log(
+      ' 📠 Published ' + chalk.green(contractName) + ' to the frontend.'
+    )
 
-    return true;
+    return true
   } catch (e) {
-    if(e.toString().indexOf("no such file or directory")>=0){
-      console.log(chalk.yellow(" ⚠️  Can't publish "+contractName+" yet (make sure it getting deployed)."))
-    }else{
-      console.log(e);
-      return false;
+    if (e.toString().indexOf('no such file or directory') >= 0) {
+      console.log(
+        chalk.yellow(
+          " ⚠️  Can't publish " +
+            contractName +
+            ' yet (make sure it getting deployed).'
+        )
+      )
+    } else {
+      console.log(e)
+      return false
     }
   }
 }
 
 async function main() {
   if (!fs.existsSync(publishDir)) {
-    fs.mkdirSync(publishDir);
+    fs.mkdirSync(publishDir)
   }
-  const finalContractList = [];
+  const finalContractList = []
   fs.readdirSync(bre.config.paths.sources).forEach((file) => {
-    if (file.indexOf(".sol") >= 0) {
-      const contractName = file.replace(".sol", "");
+    if (file.indexOf('.sol') >= 0) {
+      const contractName = file.replace('.sol', '')
       // Add contract to list if publishing is successful
       if (publishContract(contractName)) {
-        finalContractList.push(contractName);
+        finalContractList.push(contractName)
       }
     }
-  });
+  })
   fs.writeFileSync(
     `${publishDir}/contracts.js`,
     `module.exports = ${JSON.stringify(finalContractList)};`
-  );
+  )
 }
 main()
   .then(() => process.exit(0))
   .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    console.error(error)
+    process.exit(1)
+  })
