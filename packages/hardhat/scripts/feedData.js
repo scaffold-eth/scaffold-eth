14f2fa1615b99f00f4f0d0002701db1d56b72e97
Barak Chamo <barakchamo@gmail.com>
2021-04-06 02:23:17 -0400

    funds view, feed default values and event handler

--- packages/hardhat/scripts/feedData.js
+++ packages/hardhat/scripts/feedData.js
@@ -6,6 +6,7 @@ const feedData = [
     apiUrl: "https://sdmx.data.unicef.org/ws/public/sdmxapi/rest/data/UNESCO,UIS,1.0/USA.ROFST._T.._T._T....PCNT?format=sdmx-json",
     apiValueParseMap: "data.dataSets.0.series.0:0:0:0:0:0:0:0:0:0.observations.0.0",
     yearOffset: 1,
+    value: 1200
    
   },  
   {
@@ -14,6 +15,7 @@ const feedData = [
       apiUrl: "http://migration",
       apiValueParseMap: "0.data.value",
       yearOffset: 1,
+      value: 1000
     },
     {
       name: "Carbon Data Feed",
@@ -21,6 +23,7 @@ const feedData = [
       apiUrl: "http://migration",
       apiValueParseMap: "0.data.value",
       yearOffset: 1,
+      value: 1500
     },
     {
       name: "Education Data Feed",
@@ -28,6 +31,7 @@ const feedData = [
       apiUrl: "http://migration",
       apiValueParseMap: "0.data.value",
       yearOffset: 1,
+      value: 1000
     },
 ];
 
@@ -39,7 +43,7 @@ const fundData = [
     description: 'The Education Impact Fund features variable pricing based on the percentage of primary school age children out of school. All donations to the Education Impact Fund are transferred to UNICEF.',
     image: 'https://upload.wikimedia.org/wikipedia/commons/7/78/Unicef.png',
     rangeMin: 1000,
-    rangeMax: 1000,
+    rangeMax: 2000,
     attributes: [
       {
         trait_type: "Beneficiary Address",
@@ -54,7 +58,7 @@ const fundData = [
     description: 'The Environmental Awareness Fund features variable pricing based on the carbon emissions of countries in the European Union. All donations to the Enbironmental Awareness Fund are transferred to the WWF.',
     image: 'https://1000logos.net/wp-content/uploads/2017/05/WWF-logo.png',
     rangeMin: 1000,
-    rangeMax: 1000,
+    rangeMax: 2000,
     attributes: [
       {
         trait_type: "Beneficiary Address",
