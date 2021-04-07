6ef41995537dcd69cf645db6552055fb525bbe6f
Barak Chamo <barakchamo@gmail.com>
2021-04-07 00:21:03 -0400

    feed view

--- packages/react-app/src/views/index.js
+++ packages/react-app/src/views/index.js
@@ -3,5 +3,6 @@ export { default as Artworks } from "./Artworks";
 export { default as Vault } from "./Vault";
 export { default as Artwork } from "./Artwork";
 export { default as Funds } from "./Funds";
+export { default as Feeds } from "./Feeds";
 export { default as MintArtwork } from "./MintArtwork";
 export { default as Subgraph } from "./Subgraph";
