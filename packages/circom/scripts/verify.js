868b8b96c38e80241feebc2852ea4ddbb8c8d4e4
Calvin Quin Boehr <calv.bore@gmail.com>
2021-04-11 16:02:51 -0600

    verify all circuits with -A or --all flags

--- packages/circom/scripts/verify.js
+++ packages/circom/scripts/verify.js
@@ -1,7 +1,7 @@
 const snarkjs = require("snarkjs");
 const fs = require("fs");
 
-const circuitsList = process.argv[2];
+let circuitsList = process.argv[2];
 const inputJson = process.argv[3];
 
 if (process.argv.length < 3 || process.argv.length > 4) {
@@ -10,6 +10,23 @@ if (process.argv.length < 3 || process.argv.length > 4) {
     'verify comma,seperated,list,of,circuits optionalinput.json'
   );
   process.exit(1);
+}
+
+const cwd = process.cwd();
+console.log(cwd);
+
+if (circuitsList === "-A" || circuitsList === "--all") {
+  try {
+    circuitsList = fs.readdirSync(cwd + "/circuits", { withFileTypes: true })
+      .filter(dirent => dirent.isDirectory())
+      .map(dirent => dirent.name).join();
+
+      console.log("Compiling all circuits...");
+      console.log(circuitsList);
+  } catch(error) {
+    console.log(error);
+    process.exit(1);
+  }
 }
 
 const proofLog = [];
@@ -51,35 +68,3 @@ run().then(() => {
   }
   process.exit(0);
 });
-
-
-// const snarkjs = require("snarkjs");
-// const fs = require("fs");
-//
-// async function run() {
-//   const { proof, publicSignals } = await snarkjs.groth16.fullProve(
-//       {
-//         "x": "1764",
-//         "hash": "15893827533473716138720882070731822975159228540693753428689375377280130954696"
-//       },
-//         "./circuits/hash/compiled/circuit.wasm",
-//         "./circuits/hash/keys/circuit_final.zkey"
-//       );
-//     console.log("Proof: ");
-//     console.log(JSON.stringify(proof, null, 1));
-//
-//     const vKey = JSON.parse(fs.readFileSync("./circuits/hash/keys/verification_key.json"));
-//
-//     const res = await snarkjs.groth16.verify(vKey, publicSignals, proof);
-//
-//     if (res === true) {
-//         console.log("Verification OK");
-//     } else {
-//         console.log("Invalid proof");
-//     }
-//
-// }
-//
-// run().then(() => {
-//     process.exit(0);
-// });
