98833fd2fe778bb5e6d351f345e85b88193174fa
Barak Chamo <barakchamo@gmail.com>
2021-04-10 23:14:21 -0400

    image header

--- packages/react-app/src/components/Header.jsx
+++ packages/react-app/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { PageHeader } from "antd";
+import { PageHeader, Typography } from "antd";
 
 // displays a page header
 
@@ -7,9 +7,20 @@ export default function Header() {
   return (
     <a href="/">
       <PageHeader
-        title="🤝 Good Tokens"
-        subTitle="not bad tokens"
-        style={{ cursor: "pointer" }}
+        title={
+          <Typography.Title style={{ color: "white", marginTop: 50 }}>
+            🤝 Good <br />
+            Tokens
+          </Typography.Title>
+        }
+        style={{
+          cursor: "pointer",
+          textAlign: "left",
+          height: 250,
+          backgroundPosition: "100% 50%",
+          backgroundSize: "100%",
+          backgroundImage: `url(https://www.unicef.org/ethiopia/sites/unicef.org.ethiopia/files/styles/media_large_image/public/IMG_9057.jpg?itok=aLSI3TTV)`,
+        }}
       />
     </a>
   );
