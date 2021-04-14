391ea2a5f765f084af053913d27a8bbd320db452
viraj124 <virajm72@gmail.com>
2021-04-14 11:12:10 +0530

    refactor: updated auction contract logic

--- packages/hardhat/contracts/Auction.sol
+++ packages/hardhat/contracts/Auction.sol
@@ -3,50 +3,96 @@ pragma solidity 0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Auction {
-  ERC721 public nonFungibleContract;
+    struct tokenDetails {
+        address seller;
+        uint128 price;
+        uint256 duration;
+        uint256 maxBid;
+        address maxBidUser;
+        bool isActive;
+        uint256[] bidAmounts;
+        address[] users;
+    }
 
-  struct Auction {
-    address seller;
-    uint128 price;
-  }
+    mapping(address => mapping(uint256 => tokenDetails)) public tokenToAuction;
 
-  mapping (uint256 => Auction) public tokenIdToAuction;
+    mapping(address => mapping(uint256 => mapping(address => uint256)))
+        public bids;
+    
+    /**
+       Seller puts the item on auction
+    */
+    function createTokenAuction(
+        address _nft,
+        uint256 _tokenId,
+        uint128 _price,
+        uint256 _duration
+    ) external {
+        require(msg.sender != address(0));
+        require(_nft != address(0));
+        require(_price > 0);
+        require(_duration > 0);
+        tokenDetails memory _auction = tokenDetails({
+            seller: msg.sender,
+            price: uint128(_price),
+            duration: _duration,
+            maxBid: 0,
+            maxBidUser: address(0),
+            isActive: true,
+            bidAmounts: new uint256[](0),
+            users: new address[](0)
+        });
+        address owner = msg.sender;
+        ERC721(_nft).safeTransferFrom(owner, address(this), _tokenId);
+        tokenToAuction[_nft][_tokenId] = _auction;
+    }
+    /**
+       Users bid for a particular nft, the max bid is compared and set if the current bid id highest
+    */
+    function bid(address _nft, uint256 _tokenId) external payable {
+        tokenDetails storage auction = tokenToAuction[_nft][_tokenId];
+        require(msg.value >= auction.price);
+        require(auction.isActive);
+        require(auction.duration > block.timestamp);
+        bids[_nft][_tokenId][msg.sender] = msg.value;
+        if (auction.bidAmounts.length == 0) {
+            auction.maxBid = msg.value;
+            auction.maxBidUser = msg.sender;
+        } else {
+            uint256 lastIndex = auction.bidAmounts.length - 1;
+            if (auction.bidAmounts[lastIndex] < msg.value) {
+                auction.maxBid = msg.value;
+                auction.maxBidUser = msg.sender;
+            }
+        }
+        auction.users.push(msg.sender);
+        auction.bidAmounts.push(msg.value);
+    }
+    /**
+       Called by the seller when the auction duration is over the hightest biid user get's the nft and other bidders get eth back
+    */
+    function executeSale(address _nft, uint256 _tokenId) external {
+        tokenDetails storage auction = tokenToAuction[_nft][_tokenId];
+        require(auction.duration <= block.timestamp);
+        require(auction.seller == msg.sender);
+        require(auction.isActive);
+        auction.isActive = false;
+        (bool success, ) = auction.seller.call{value: auction.maxBid}("");
+        require(success);
+        ERC721(_nft).safeTransferFrom(
+            address(this),
+            auction.maxBidUser,
+            _tokenId
+        );
+        for (uint256 i = 0; i < auction.users.length; i++) {
+            if (auction.users[i] != auction.maxBidUser) {
+                (success, ) = auction.users[i].call{
+                    value: bids[_nft][_tokenId][auction.users[i]]
+                }("");
+                require(success);
+            }
+        }
+    }
 
-  constructor(address _nftAddress) {
-    nonFungibleContract = ERC721(_nftAddress);
-  }
-
-  function createAuction(uint256 _tokenId, uint128 _price) public {
-    Auction memory _auction = Auction({
-       seller: msg.sender,
-       price: uint128(_price)
-    });
-    address owner = msg.sender;
-    nonFungibleContract.safeTransferFrom(owner, address(this), _tokenId);
-    tokenIdToAuction[_tokenId] = _auction;
-  }
-
-  function bid(uint256 _tokenId) public payable {
-    Auction memory auction = tokenIdToAuction[_tokenId];
-    require(auction.seller != address(0));
-    require(msg.value >= auction.price);
-
-    address seller = auction.seller;
-    uint128 price = auction.price;
-
-    delete tokenIdToAuction[_tokenId];
-
-    (bool success, ) = seller.call{value: price}("");
-    require(success);
-    address user = msg.sender;
-    nonFungibleContract.safeTransferFrom(address(this), user, _tokenId);
-  }
-
-  function cancel(uint256 _tokenId) public {
-    Auction memory auction = tokenIdToAuction[_tokenId];
-    require(auction.seller == msg.sender);
-
-    delete tokenIdToAuction[_tokenId];
-    nonFungibleContract.safeTransferFrom(address(this), msg.sender, _tokenId);
-  }
+    receive() external payable {}
 }
