ce756fb4b15acbccd0e48c81dfde74dc52973ef8
Sören Steiger <soren1@gmx.net>
2021-04-18 14:27:06 +0200

    Fix import

--- packages/react-app/src/contracts/contracts.js
+++ packages/react-app/src/contracts/contracts.js
@@ -1,1 +1,1 @@
-module.exports = ["Clicker"];
+module.exports = ["EthereumCityERC20TokenMinter"];
--- packages/react-app/src/redux/rootReducer.js
+++ packages/react-app/src/redux/rootReducer.js
@@ -2,14 +2,14 @@ import { combineReducers } from 'redux'
 
 import { reducer as terminal } from '../views/App/views/Terminal'
 import { reducer as wallet } from '../views/App/views/Wallet'
-import { reducer as dialogModal } from '../views/App/views/DialogModal'
+import { reducer as dialogContainer } from '../views/App/views/DialogContainer'
 import { reducer as toolbelt } from '../views/App/views/Toolbelt'
 import { reducer as dish } from '../views/App/views/Dish'
 
 const rootReducer = combineReducers({
   terminal,
   wallet,
-  dialogModal,
+  dialogContainer,
   toolbelt,
   dish
 })
