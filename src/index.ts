e5749388f17e67ceccf03f05f68756e89c5a9480
Calvin Quin Boehr <calv.bore@gmail.com>
2021-04-28 02:08:05 -0600

    add useExternalContractReader

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,4 @@ export { default as useTokenBalance } from "./TokenBalance";
 export { default as useUserAddress } from "./UserAddress";
 export { default as useUserProvider } from "./UserProvider";
 export { default as useContractLoader } from "./ContractLoader";
+export { default as useExternalContractLoader } from "./ExternalContractLoader";
