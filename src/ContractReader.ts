86c5d8e3242db9f29e3c6df3736bf3075399ecb9
Calvin Quin Boehr <calv.bore@gmail.com>
2021-05-02 16:40:14 -0600

    update useContarcReader to use useOnBlock

--- src/ContractReader.ts
+++ src/ContractReader.ts
@@ -2,20 +2,21 @@
 import { useState, useEffect } from "react";
 import { Contract } from "@ethersproject/contracts";
 import usePoller from "./Poller";
+import useOnBlock from "./OnBlock";
 import { BLOCK_TIME } from "./constants";
 
 const DEBUG = false;
 
 export default function useContractReader(
-  contract: Contract,
+  contracts: any,
+  contractName: string,
   functionName: string,
   args?: any[],
   pollTime?: number,
   formatter?: (...args: any[]) => any,
   onChange?: (newValue: any) => void,
 ): any {
-  let adjustPollTime = BLOCK_TIME;
-
+  let adjustPollTime = 0;
   if (pollTime) {
     adjustPollTime = pollTime;
   } else if (!pollTime && typeof args === "number") {
@@ -23,41 +24,52 @@ export default function useContractReader(
     adjustPollTime = args;
   }
 
-  const [value, setValue] = useState<any>();
+  const [value, setValue] = useState();
   useEffect(() => {
     if (typeof onChange === "function") {
-      setTimeout(() => onChange(value), 1);
+      setTimeout(onChange.bind(this, value), 1);
     }
   }, [value, onChange]);
 
-  usePoller(() => {
-    const readContractValue = async () => {
-      try {
-        let newValue;
-        if (DEBUG) console.log("CALLING ", functionName, "with args", args);
-        if (args && args.length > 0) {
-          newValue = await contract[functionName](...args);
-          if (DEBUG) {
-            console.log("functionName", functionName, "args", args, "RESULT:", newValue);
-          }
-        } else {
-          newValue = await contract[functionName]();
-        }
-        // perform any processing on the raw response
-        if (formatter && typeof formatter === "function") {
-          newValue = formatter(newValue);
-        }
-
-        if (newValue !== value) {
-          setValue(newValue);
-        }
-      } catch (e) {
-        console.log(e);
+  const updateValue = async () => {
+    try {
+      let newValue;
+      if (DEBUG) console.log("CALLING ", contractName, functionName, "with args", args);
+      if (args && args.length > 0) {
+        newValue = await contracts[contractName][functionName](...args);
+        if (DEBUG)
+          console.log("contractName", contractName, "functionName", functionName, "args", args, "RESULT:", newValue);
+      } else {
+        newValue = await contracts[contractName][functionName]();
       }
-    };
+      if (formatter && typeof formatter === "function") {
+        newValue = formatter(newValue);
+      }
+      // console.log("GOT VALUE",newValue)
+      if (newValue !== value) {
+        setValue(newValue);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
 
-    if (contract && typeof contract[functionName] !== "undefined") readContractValue();
-  }, adjustPollTime);
+// Only pass a provider to watch on a block if we have a contract and no PollTime
+  useOnBlock(
+    (contracts && contracts[contractName] && adjustPollTime === 0)&&contracts[contractName].provider,
+    () => {
+    if (contracts && contracts[contractName] && adjustPollTime === 0) {
+      updateValue()
+  }
+  })
+
+// Use a poller if a pollTime is provided
+usePoller(async () => {
+  if (contracts && contracts[contractName] && adjustPollTime > 0) {
+    if (DEBUG) console.log('polling!', contractName, functionName)
+    updateValue()
+  }
+}, adjustPollTime, contracts && contracts[contractName])
 
   return value;
 }
