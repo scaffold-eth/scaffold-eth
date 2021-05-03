c0ddbaffc9cb621591f00486bbce276aac386157
Calvin Quin Boehr <calv.bore@gmail.com>
2021-05-03 00:43:09 -0600

    added useLocalStorage hook

--- src/index.ts
+++ src/index.ts
@@ -11,4 +11,5 @@ export { default as useUserAddress } from "./UserAddress";
 export { default as useUserProvider } from "./UserProvider";
 export { default as useContractLoader } from "./ContractLoader";
 export { default as useExternalContractLoader } from "./ExternalContractLoader";
+export { default as useLocalStorage } from "./LocalStorage";
 export { default as useOnBlock } from "./OnBlock";
