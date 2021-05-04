ecee802f85c89281e8ef0be6baa19f50d5b017fe
Calvin Quin Boehr <calv.bore@gmail.com>
2021-05-04 01:22:39 -0600

    update useLookupAddress to be current with scaffold-eth

--- src/ENS/LookupAddress.ts
+++ src/ENS/LookupAddress.ts
@@ -1,30 +1,77 @@
 import { useState, useEffect } from "react";
 import { Provider } from "@ethersproject/providers";
-import { getAddress } from "@ethersproject/address";
+import { getAddress, isAddress } from "@ethersproject/address";
+import useLocalStorage from "../LocalStorage";
 
-const lookupAddress = async (provider: Provider, address: string): Promise<string> => {
-  try {
-    // Accuracy of reverse resolution is not enforced.
-    // We then manually ensure that the reported ens name resolves to address
-    const reportedName = await provider.lookupAddress(address);
-    const resolvedAddress = await provider.resolveName(reportedName);
-    if (getAddress(address) === getAddress(resolvedAddress)) {
-      return reportedName;
+// resolved if(name){} to not save "" into cache
+
+/*
+  ~ What it does? ~
+
+  Gets ENS name from given address and provider
+
+  ~ How can I use? ~
+
+  const ensName = useLookupAddress(mainnetProvider, address);
+
+  ~ Features ~
+
+  - Provide address and get ENS name corresponding to given address
+*/
+
+const lookupAddress = async (
+  provider: Provider,
+  address: string
+) => {
+  if(isAddress(address)) {
+    //console.log(`looking up ${address}`)
+    try {
+      // Accuracy of reverse resolution is not enforced.
+      // We then manually ensure that the reported ens name resolves to address
+      const reportedName = await provider.lookupAddress(address);
+
+      const resolvedAddress = await provider.resolveName(reportedName);
+
+      if (getAddress(address) === getAddress(resolvedAddress)) {
+        return reportedName;
+      } else {
+        return getAddress(address)
+      }
+    } catch (e) {
+      return getAddress(address)
     }
-  } catch (e) {
-    // Do nothing
   }
-  return "";
+  return 0;
 };
 
-const useLookupAddress = (provider: Provider, address: string): string => {
-  const [ensName, setEnsName] = useState<string>(address);
+const useLookupAddress = (
+  provider: Provider,
+  address: string
+) => {
+  const [ensName, setEnsName] = useState(address);
+  //const [ensCache, setEnsCache] = useLocalStorage('ensCache_'+address); Writing directly due to sync issues
 
   useEffect(() => {
-    if (provider) {
-      lookupAddress(provider, address).then((name: string) => setEnsName(name));
+
+    let cache: any = window.localStorage.getItem('ensCache_'+address);
+    cache = cache && JSON.parse(cache)
+
+    if( cache && cache.timestamp>Date.now()){
+      setEnsName(cache.name)
+    }else{
+      if (provider) {
+        lookupAddress(provider, address).then((name) => {
+          if (name) {
+            setEnsName(name);
+            window.localStorage.setItem('ensCache_'+address, JSON.stringify({
+              timestamp:Date.now()+360000,
+              name:name
+            }))
+          }
+        });
+      }
     }
-  }, [provider, address]);
+  }, [provider, address, setEnsName]);
 
   return ensName;
 };
