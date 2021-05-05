50c99b9557fa41026ae629b0abc0dc9bbdff3269
Adam Fuller <azf20@users.noreply.github.com>
2021-05-05 12:29:09 +0100

    optional ttl on useLocalStorage (#171)
    - ttl is optional, in milliseconds
    - lazy removal (only if a value is checked)
    - expiry date is not returned
    - potential breaking case where a non-TTL useLocalStorage stores a value that is an object with "expiry" and "value" as keys (will only return the value, not the expiry) but that is unlikely

--- packages/react-app/src/hooks/LocalStorage.js
+++ packages/react-app/src/hooks/LocalStorage.js
@@ -1,14 +1,28 @@
 import { useState } from 'react';
 // Hook from useHooks! (https://usehooks.com/useLocalStorage/)
-export default function useLocalStorage(key, initialValue) {
+export default function useLocalStorage(key, initialValue, ttl) {
   // State to store our value
   // Pass initial state function to useState so logic is only executed once
   const [storedValue, setStoredValue] = useState(() => {
     try {
       // Get from local storage by key
       const item = window.localStorage.getItem(key);
+      let parsedItem = item ? JSON.parse(item) : initialValue;
+
+      if(typeof parsedItem === 'object' && parsedItem !== null
+        && 'expiry' in parsedItem && 'value' in parsedItem) {
+          const now = new Date()
+          if (ttl && now.getTime() > parsedItem.expiry) {
+        		// If the item is expired, delete the item from storage
+        		// and return null
+        		window.localStorage.removeItem(key)
+        		return initialValue
+        	} else {
+            return parsedItem.value
+          }
+      }
       // Parse stored json or if none return initialValue
-      return item ? JSON.parse(item) : initialValue;
+      return parsedItem
     } catch (error) {
       // If error also return initialValue
       console.log(error);
@@ -26,7 +40,19 @@ export default function useLocalStorage(key, initialValue) {
       // Save state
       setStoredValue(valueToStore);
       // Save to local storage
-      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      if(ttl) {
+        const now = new Date()
+
+        // `item` is an object which contains the original value
+        // as well as the time when it's supposed to expire
+        const item = {
+          value: valueToStore,
+          expiry: now.getTime() + ttl,
+        }
+        window.localStorage.setItem(key, JSON.stringify(item))
+      } else {
+        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      }
     } catch (error) {
       // A more advanced implementation would handle the error case
       console.log(error);
