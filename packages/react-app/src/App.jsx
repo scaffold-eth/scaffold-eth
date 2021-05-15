8e01fbfe10fd83c4d92be1ea9b7ffcd1cf789bf7
Hunter Chang <changodaman@gmail.com>
2021-05-15 14:57:34 -0600

    warn specifically about chain id mis match with 31337 and 1337 readd

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -204,41 +204,66 @@ function App(props) {
   if (localChainId && selectedChainId && localChainId !== selectedChainId) {
     const networkSelected = NETWORK(selectedChainId);
     const networkLocal = NETWORK(localChainId);
+    if (selectedChainId == 1337 && localChainId == 31337) {
+      networkDisplay = (
+        <div style={{ zIndex: 2, position: "absolute", right: 0, top: 60, padding: 16 }}>
+          <Alert
+            message="⚠️ Wrong Network ID"
+            description={
+              <div>
+                You have <b>chain id 1337</b> for localhost and you need to change it to <b>31337</b> to work with
+                HardHat.
+                <div>(MetaMask -> Settings -> Networks -> Chain ID -> 31337)</div>
+              </div>
+            }
+            type="error"
+            closable={false}
+          />
+        </div>
+      );
+    } else {
+      networkDisplay = (
+        <div style={{ zIndex: 2, position: "absolute", right: 0, top: 60, padding: 16 }}>
+          <Alert
+            message="⚠️ Wrong Network"
+            description={
+              <div>
+                You have <b>{networkSelected && networkSelected.name}</b> selected and you need to be on{" "}
+                <b>{networkLocal && networkLocal.name}</b>.
+                <Button
+                  onClick={async () => {
+                    const ethereum = window.ethereum;
+                    const data = [
+                      {
+                        chainId: "0x" + targetNetwork.chainId.toString(16),
+                        chainName: targetNetwork.name,
+                        nativeCurrency: targetNetwork.nativeCurrency,
+                        rpcUrls: [targetNetwork.rpcUrl],
+                        blockExplorerUrls: [targetNetwork.blockExplorer],
+                      },
+                    ];
+                    console.log("data", data);
+                    const tx = await ethereum.request({ method: "wallet_addEthereumChain", params: data }).catch();
+                    if (tx) {
+                      console.log(tx);
+                    }
+                  }}
+                >
+                  {NETWORK(localChainId).name}
+                </Button>
+                .
+              </div>
+            }
+            type="error"
+            closable={false}
+          />
+        </div>
+      );
+    }
+  } else {
     networkDisplay = (
-      <div style={{ zIndex: 2, position: "absolute", right: 0, top: 16, padding: 8 }}>
-        <Alert
-          message="⚠️ Wrong Network"
-          description={
-            <div>
-              You have <b>{networkSelected && networkSelected.name}</b> selected and you need to be on{" "}
-              <b>{networkLocal && networkLocal.name}</b>.
-              <Button
-                onClick={async () => {
-                  const ethereum = window.ethereum;
-                  const data = [
-                    {
-                      chainId: "0x" + targetNetwork.chainId.toString(16),
-                      chainName: targetNetwork.name,
-                      nativeCurrency: targetNetwork.nativeCurrency,
-                      rpcUrls: [targetNetwork.rpcUrl],
-                      blockExplorerUrls: [targetNetwork.blockExplorer],
-                    },
-                  ];
-                  console.log("data", data);
-                  const tx = await ethereum.request({ method: "wallet_addEthereumChain", params: data }).catch();
-                  if (tx) {
-                    console.log(tx);
-                  }
-                }}
-              >
-                {NETWORK(localChainId).name}
-              </Button>
-              .
-            </div>
-          }
-          type="error"
-          closable={false}
-        />
+      <div style={{ zIndex: -1, position: "absolute", right: 154, top: 28, padding: 16, color: targetNetwork.color }}>
+        {targetNetwork.name}
       </div>
     );
   }
