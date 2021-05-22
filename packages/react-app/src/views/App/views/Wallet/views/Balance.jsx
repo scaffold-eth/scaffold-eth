cae29a418183d8924020f32a4463201e2d0a7b04
Hunter Chang <changodaman@gmail.com>
2021-05-22 16:58:56 -0600

    re-add eslint disable in balance

--- packages/react-app/src/views/App/views/Wallet/views/Balance.jsx
+++ packages/react-app/src/views/App/views/Wallet/views/Balance.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import { formatEther } from '@ethersproject/units'
 import React, { useState } from 'react'
 import { useBalance } from '../../../../../hooks'
