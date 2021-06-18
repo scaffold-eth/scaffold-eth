e42cfb3b7e3546b33711775350d2fb1193cc7326
Pablo Ruiz <pabloruiz55@gmail.com>
2021-06-18 07:56:04 -0300

    Update constants.js

--- packages/react-app/src/constants.js
+++ packages/react-app/src/constants.js
@@ -1,5 +1,5 @@
 // MY INFURA_ID, SWAP IN YOURS FROM https://infura.io/dashboard/ethereum
-export const INFURA_ID = "ac748b58b39244d2873bbccf84313350";
+export const INFURA_ID = "460f40a260564ac4a4f4b3fffb032dad";
 
 // MY ETHERSCAN_ID, SWAP IN YOURS FROM https://etherscan.io/myapikey
 export const ETHERSCAN_KEY = "PSW8C433Q667DVEX5BCRMGNAH9FSGFZ7Q8";
