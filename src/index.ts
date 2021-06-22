3a8844a4a4c8ad4297e075380b72ccfe1115d628
Calvin Quin Boehr <calv.bore@gmail.com>
2021-06-22 01:42:28 -0600

    remove redundant hooks

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 export { default as useBalance } from "./Balance";
 export { default as useBlockNumber } from "./BlockNumber";
 export { default as useContractReader } from "./ContractReader";
-export { default as useLocalContractsReader } from "./LocalContractsReader";
 export { useLookupAddress, useResolveEnsName } from "./ENS";
 export { useEventListener, useEventReader, useLocalEventListener } from "./Events";
 export { default as useNonce } from "./Nonce";
@@ -9,7 +8,6 @@ export { default as usePoller } from "./Poller";
 export { default as useTimestamp } from "./Timestamp";
 export { default as useTokenBalance } from "./TokenBalance";
 export { default as useUserAddress } from "./UserAddress";
-export { default as useUserProvider } from "./UserProvider";
 export { default as useUserSigner } from "./UserSigner";
 export { default as useContractLoader } from "./ContractLoader";
 export { default as useLocalStorage } from "./LocalStorage";
