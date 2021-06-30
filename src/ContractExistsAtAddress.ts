b8553b6d7bf855f1b2325b91a2a53caf7cfc8a33
Shravan Sunder <shravan.retail@gmail.com>
2021-06-30 14:39:14 -0400

    feat: #16 ♻ modified useContractExistedAtAddress

--- src/ContractExistsAtAddress.ts
+++ src/ContractExistsAtAddress.ts
@@ -1,46 +1,40 @@
-import { useEffect, useState } from "react";
-import { Provider } from "@ethersproject/providers";
-import { utils } from "ethers";
+import { utils } from 'ethers';
+import { useEffect, useState } from 'react';
 
-/*
-  ~ What it does? ~
+import { TEthHooksProvider } from '~~/models/providerTypes';
 
-  Checks whether a contract exists on the blockchain, returns true if it exists, otherwise false
-
-  ~ How can I use? ~
-
-  const contractIsDeployed = useContractExistsAtAddress(localProvider, contractAddress);
-
+/**
+ * Checks whether a contract exists on the blockchain, returns true if it exists, otherwise false
+ * 
   ~ Features ~
 
   - Provide contractAddress to check if the contract is deployed
   - Change provider to check contract address on different chains (ex. mainnetProvider)
-*/
+ * @param provider 
+ * @param contractAddress 
+ * @returns 
+ */
+export const useContractExistsAtAddress = (provider: TEthHooksProvider, contractAddress: string): boolean => {
+  const [contractIsDeployed, setContractIsDeployed] = useState(false);
 
-const useContractExistsAtAddress = (
-  provider: Provider,
-  contractAddress: string
-): boolean => {
-  const [contractIsDeployed, setContractIsDeployed] = useState<boolean>(false);
-
-  // We can look at the blockchain and see what's stored at `contractAddress`
-  // If we find code then we know that a contract exists there.
-  // If we find nothing (0x0) then there is no contract deployed to that address
   useEffect(() => {
-    // eslint-disable-next-line consistent-return
+    /**
+     * We can look at the blockchain and see what's stored at `contractAddress`
+     * If we find code then we know that a contract exists there.
+     * If we find nothing (0x0) then there is no contract deployed to that address
+     * @returns
+     */
     const checkDeployment = async () => {
       if (!utils.isAddress(contractAddress)) {
         return false;
       }
       const bytecode = await provider.getCode(contractAddress);
-      setContractIsDeployed(bytecode !== "0x");
+      setContractIsDeployed(bytecode !== '0x');
     };
     if (provider) {
-      checkDeployment();
+      void checkDeployment();
     }
   }, [provider, contractAddress]);
 
   return contractIsDeployed;
 };
-
-export default useContractExistsAtAddress;
