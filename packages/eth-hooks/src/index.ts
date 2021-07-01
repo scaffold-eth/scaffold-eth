8425a0d506d6634e5b333794917776d1231ba3e8
Calvin Quin Boehr <calv.bore@gmail.com>
2021-07-01 16:37:09 -0600

    remove useEventReader hook

--- packages/eth-hooks/src/index.ts
+++ packages/eth-hooks/src/index.ts
@@ -2,7 +2,7 @@ export { default as useBalance } from "./Balance";
 export { default as useBlockNumber } from "./BlockNumber";
 export { default as useContractReader } from "./ContractReader";
 export { useLookupAddress, useResolveEnsName } from "./ENS";
-export { useEventListener, useEventReader } from "./Events";
+export { useEventListener } from "./Events";
 export { default as useNonce } from "./Nonce";
 export { default as usePoller } from "./Poller";
 export { default as useTimestamp } from "./Timestamp";
