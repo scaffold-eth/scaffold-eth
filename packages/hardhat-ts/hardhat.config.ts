b12ad436146a18928fd491337c9db7849a53fac7
Shravan Sunder <shravan.retail@gmail.com>
2021-07-06 22:04:04 -0400

    changed how generated folders are ignored.

--- packages/hardhat-ts/hardhat.config.ts
+++ packages/hardhat-ts/hardhat.config.ts
@@ -56,9 +56,9 @@ const config: HardhatUserConfig = {
         if there is no mnemonic, it will just use account 0 of the hardhat node to deploy
         (you can put in a mnemonic here to set the deployer locally)
       */
-      accounts: {
-        mnemonic: mnemonic(),
-      },
+      // accounts: {
+      //   mnemonic: mnemonic(),
+      // },
     },
     rinkeby: {
       url: 'https://rinkeby.infura.io/v3/460f40a260564ac4a4f4b3fffb032dad', //<---- YOUR INFURA ID! (or it won't work)
