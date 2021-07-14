7f2e9d3aca865122501f50f74498e97aaa4d3b8d
Sören Steiger <soren1@gmx.net>
2021-07-14 19:11:22 +0200

    Set initial level in Levels component

--- packages/eth-dev/src/components/App/levels/index.jsx
+++ packages/eth-dev/src/components/App/levels/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { connectController as wrapGlobalGameData } from '../gameItems'
 import { Background } from '../gameItems/components'
@@ -7,7 +7,15 @@ import TemplateLevel from './TemplateLevel'
 import SetupLocalNetwork from './SetupLocalNetwork'
 import CreateWallet from './CreateWallet'
 
-const Levels = ({ levelContainer: { currentLevel } }) => {
+const Levels = ({ levelContainer: { currentLevel }, actions }) => {
+  const setInitialLevel = levelId => {
+    actions.level.setCurrentLevel({ levelId })
+  }
+
+  useEffect(() => {
+    setInitialLevel('setup-local-network')
+  }, [])
+
   return (
     <>
       <Background />
