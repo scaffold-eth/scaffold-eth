acb534fb9f721ca84588ada222dd7b63a4a843f3
mercury-protocol <84585728+mercury-protocol@users.noreply.github.com>
2021-07-16 05:33:31 -0400

    uint to string function now works and appends the token value to the tokenURI

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -11,11 +11,41 @@ mapping (address => mapping(address => bool)) public operatorList;
 mapping (uint => address) public approved; 
 mapping (address => uint) public balances;
 mapping (uint => bool) hasMinted;
+
 /*
 constructor(address _ogNFT) {
   ogNFT = _ogNFT;
 }
 */
+
+function uint2str(
+uint256 _i
+)
+internal
+pure
+returns (string memory str)
+{
+if (_i == 0)
+{
+  return "0";
+}
+uint256 j = _i;
+uint256 length;
+while (j != 0)
+{
+  length++;
+  j /= 10;
+}
+bytes memory bstr = new bytes(length);
+uint256 k = length;
+j = _i;
+while (j != 0)
+{
+  bstr[--k] = bytes1(uint8(48 + j % 10));
+  j /= 10;
+}
+str = string(bstr);
+}
 
 function hasMintedNFT(uint _tokenId) public view returns(bool){
   return hasMinted[_tokenId];
@@ -32,7 +62,7 @@ hasMinted[_tokenId] = true;
 }
 
 function tokenURI(uint _tokenId) public pure returns(string memory) {
-return string(abi.encodePacked("https://samplehostingservice/ConditonalNFT/"));
+return string(abi.encodePacked("https://samplehostingservice/ConditonalNFT/", uint2str(_tokenId)));
 }
 function isContract(address addr) public view returns(bool) {
 uint size;
