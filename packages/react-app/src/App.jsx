0891dbb97f09b8192a2aa2d98dcdfd40854a5ea4
Austin Griffith <you@example.com>
2021-07-19 16:33:45 +0200

    add a disconnect from wallet connect after talking with pedro in paris

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -1,4 +1,5 @@
 import WalletConnectProvider from "@walletconnect/web3-provider";
+//import Torus from "@toruslabs/torus-embed"
 import WalletLink from "walletlink";
 import { Alert, Button, Col, Menu, Row } from "antd";
 import "antd/dist/antd.css";
@@ -92,6 +93,7 @@ const web3Modal = new Web3Modal({
     walletconnect: {
       package: WalletConnectProvider, // required
       options: {
+        bridge: "https://polygon.bridge.walletconnect.org",
         infuraId: INFURA_ID,
         rpc: {
           1:'https://mainnet.infura.io/v3/${INFURA_ID}', // mainnet // For more WalletConnect providers: https://docs.walletconnect.org/quick-start/dapps/web3-provider#required
@@ -99,6 +101,9 @@ const web3Modal = new Web3Modal({
         },
       },
     },
+    /*torus: {
+      package: Torus,
+    },*/
     'custom-walletlink': {
       display: {
         logo: 'https://play-lh.googleusercontent.com/PjoJoG27miSglVBXoXrxBSLveV6e3EeBPpNY55aiUUBM9Q1RCETKCOqdOkX2ZydqVf0',
@@ -114,18 +119,24 @@ const web3Modal = new Web3Modal({
   },
 });
 
-const logoutOfWeb3Modal = async () => {
-  await web3Modal.clearCachedProvider();
-  setTimeout(() => {
-    window.location.reload();
-  }, 1);
-};
+
 
 function App(props) {
   const mainnetProvider = scaffoldEthProvider && scaffoldEthProvider._network ? scaffoldEthProvider : mainnetInfura;
 
   const [injectedProvider, setInjectedProvider] = useState();
   const [address, setAddress] = useState();
+
+  const logoutOfWeb3Modal = async () => {
+    await web3Modal.clearCachedProvider();
+    if(injectedProvider && injectedProvider.provider && typeof injectedProvider.provider.disconnect == "function"){
+      await injectedProvider.provider.disconnect();
+    }
+    setTimeout(() => {
+      window.location.reload();
+    }, 1);
+  };
+
   /* 💵 This hook will get the price of ETH from 🦄 Uniswap: */
   const price = useExchangePrice(targetNetwork, mainnetProvider);
 
@@ -299,6 +310,7 @@ function App(props) {
       </div>
     );
   }
+
 
   const loadWeb3Modal = useCallback(async () => {
     const provider = await web3Modal.connect();
