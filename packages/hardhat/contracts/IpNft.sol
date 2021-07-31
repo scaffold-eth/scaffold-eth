0c36aa3104610390a1236a9caf63f29fd593c98b
zhenyangg <chrenelias@outlook.com>
2021-07-31 22:54:56 +0800

    Added docs

--- packages/hardhat/contracts/IpNft.sol
+++ packages/hardhat/contracts/IpNft.sol
@@ -10,35 +10,37 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
+contract IpNft is ERC721, ERC721URIStorage, Ownable {
+    address licensor = owner();
+    uint256 licenseCost = 10000000000000000;
+    string[] IP;
 
-contract IpNft is ERC721,ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
-  address licensor = owner();
-  uint256 licenseCost = 10000000000000000;
-  string [] IP;
+    constructor(
+        string memory IpBrandName,
+        string memory IpBrandSymbol,
+        string memory IpURI
+    ) public ERC721(IpBrandName, IpBrandSymbol) {
+        _baseURI();
+        IP.push(IpURI);
+    }
 
-  using Counters for Counters.Counter;
-  Counters.Counter private _tokenIds;
-
-  constructor(string memory IpBrandName, string memory IpBrandSymbol, string memory IpURI )
-  
-
-    
-    public ERC721(IpBrandName, IpBrandSymbol) {
-      _baseURI();
-    IP.push(IpURI);
-  }
-  s
-  //@dev Override base uri
- function _baseURI() internal pure override returns (string memory) {
+    /**
+     * @dev Override _baseURI() in ERC721
+     * Base URI for computing {tokenURI}. If set, the resulting URI for each
+     * token will be the concatenation of the `baseURI` and the `tokenId`.
+     */
+    function _baseURI() internal pure override returns (string memory) {
         return "ifps://";
     }
 
-  /**
-  * @dev Override tokenUri
-  * @param _tokenIds TokenId
-  **/
-  function tokenURI(uint256 tokenId)
+    /**
+     * @dev Override tokenUri() in ERC721
+     * @param tokenId Token Id
+     **/
+    function tokenURI(uint256 tokenId)
         public
         view
         override(ERC721, ERC721URIStorage)
@@ -47,33 +49,38 @@ contract IpNft is ERC721,ERC721URIStorage, Ownable {
         return super.tokenURI(tokenId);
     }
 
-  ///@dev Mint Licensee a License
-  function licenseIP()
-      public payable
-      returns (uint256)
-  {
-      require(msg.value == licenseCost);
-      _tokenIds.increment();
-      uint256 id = _tokenIds.current();
-      _mint(msg.sender, id);
-      _setTokenURI(id, 
-      IP[0]);
+    /**
+     * @dev Mint Licensee a License
+     * @returns token id of license
+     **/
+    function licenseIP() public payable returns (uint256) {
+        require(msg.value == licenseCost);
+        _tokenIds.increment();
+        uint256 id = _tokenIds.current();
+        _mint(msg.sender, id);
+        _setTokenURI(id, IP[0]);
 
-      return id;
-  }
+        return id;
+    }
+
     /**
      * @dev Change the licensor and owner of the contract
-     * @param newLicensor address of the new licensor 
+     * @param newLicensor address of the new licensor
      **/
-  function changeLicensor(address newLicensor) public onlyOwner {
-      transferOwnership(newLicensor);
-  }
-  /**  
-    * @dev Change cost of License
-    * @param newLicenseCost New price for license
-  **/
-  function changeLicenseCost(uint256 newLicenseCost) public onlyOwner returns (uint256) {
-      licenseCost = newLicenseCost;
-      return licenseCost;
-  }
+    function changeLicensor(address newLicensor) public onlyOwner {
+        transferOwnership(newLicensor);
+    }
+
+    /**
+     * @dev Change cost of License
+     * @param newLicenseCost New price for license
+     **/
+    function changeLicenseCost(uint256 newLicenseCost)
+        public
+        onlyOwner
+        returns (uint256)
+    {
+        licenseCost = newLicenseCost;
+        return licenseCost;
+    }
 }
