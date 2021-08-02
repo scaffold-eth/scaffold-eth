f5c84c1d59b1a17f166d20c0493473bb326d445e
Austin Griffith <you@example.com>
2021-08-02 14:37:21 -0600

    better network check and switch button

--- packages/hardhat/scripts/mint.js
+++ packages/hardhat/scripts/mint.js
@@ -12,7 +12,7 @@ const delayMS = 1000 //sometimes xDAI needs a 6000ms break lol 😅
 const main = async () => {
 
   // ADDRESS TO MINT TO:
-  const toAddress = "0xb2ac59ae04d0f7310dc3519573bf70387b3b6e3a"
+  const toAddress = "0x34aA3F359A9D614239015126635CE7732c18fDF3"
 
   console.log("\n\n 🎫 Minting to "+toAddress+"...\n");
 
@@ -201,7 +201,7 @@ const main = async () => {
 
 
 
-  await sleep(delayMS)
+  //await sleep(delayMS)
 
   // console.log("Transferring Ownership of YourCollectible to "+toAddress+"...")
 
