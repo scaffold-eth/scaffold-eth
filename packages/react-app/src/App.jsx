2d71cf2c0f71a0e3b304d7bedf62a02b70e590b6
codenamejason <codenamejason@outlook.com>
2021-08-10 14:49:12 -0400

    Add 3 new wallets to web3modal walletconnect.

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -22,6 +22,9 @@ import {
 } from "./hooks";
 // import Hints from "./Hints";
 import { ExampleUI, Hints, Subgraph } from "./views";
+import Portis from "@portis/web3";
+import Fortmatic from "fortmatic";
+import Authereum from "authereum";
 
 const { ethers } = require("ethers");
 /*
@@ -44,7 +47,7 @@ const { ethers } = require("ethers");
 */
 
 /// 📡 What chain are your contracts deployed to?
-const targetNetwork = NETWORKS.localhost; // <------- select your target frontend network (localhost, rinkeby, xdai, mainnet)
+const targetNetwork = NETWORKS.kovan; // <------- select your target frontend network (localhost, rinkeby, xdai, mainnet)
 
 // 😬 Sorry for all the console logging
 const DEBUG = true;
@@ -83,6 +86,7 @@ const walletLinkProvider = walletLink.makeWeb3Provider(
     1,
 );
 
+// Portis ID: 6255fb2b-58c8-433b-a2c9-62098c05ddc9
 /*
   Web3 modal helps us "connect" external wallets:
 */
@@ -98,13 +102,42 @@ const web3Modal = new Web3Modal({
         infuraId: INFURA_ID,
         rpc: {
           1:`https://mainnet.infura.io/v3/${INFURA_ID}`, // mainnet // For more WalletConnect providers: https://docs.walletconnect.org/quick-start/dapps/web3-provider#required
+          42: `https://kovan.infura.io/v3/${INFURA_ID}`,
           100:"https://dai.poa.network", // xDai
         },
       },
+      
     },
-    /*torus: {
-      package: Torus,
-    },*/
+    portis: {
+      display: {
+        logo: "https://user-images.githubusercontent.com/9419140/128913641-d025bc0c-e059-42de-a57b-422f196867ce.png",
+        name: "Portis",
+        description: "Connect to Portis App",
+      },
+      package: Portis,
+      options: {
+        id: "6255fb2b-58c8-433b-a2c9-62098c05ddc9",
+      },
+    },
+    fortmatic: {
+      package: Fortmatic, // required
+      options: {
+        key: "pk_live_5A7C91B2FC585A17", // required
+      },
+    },
+    // torus: {
+    //   package: Torus,
+    //   options: {
+    //     networkParams: {
+    //       host: "https://localhost:8545", // optional
+    //       chainId: 1337, // optional
+    //       networkId: 1337 // optional
+    //     },
+    //     config: {
+    //       buildEnv: "development" // optional
+    //     },
+    //   },
+    // },
     'custom-walletlink': {
       display: {
         logo: 'https://play-lh.googleusercontent.com/PjoJoG27miSglVBXoXrxBSLveV6e3EeBPpNY55aiUUBM9Q1RCETKCOqdOkX2ZydqVf0',
@@ -117,6 +150,9 @@ const web3Modal = new Web3Modal({
         return provider;
       },
     },
+    authereum: {
+      package: Authereum, // required
+    }
   },
 });
 
