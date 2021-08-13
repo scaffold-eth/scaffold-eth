01b9e89be182766ffc1b9282f43d708cd05efc98
Bliss Da Sailor <meremezeukah@gmail.com>
2021-08-13 11:52:04 -0500

    dummy token implementation

---
+++ packages/hardhat/contracts/DummyToken.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+// dummyToken Contract to test out TokenDistribution locally
+contract DummyToken is ERC20 {
+    constructor() public ERC20("DummyToken", "DTKN") {
+        _mint(msg.sender, 1000000000000000000000);
+    }
+}
--- packages/hardhat/deploy/01_deploy_tokendistributor_contract.js
+++ packages/hardhat/deploy/01_deploy_tokendistributor_contract.js
@@ -1,11 +1,49 @@
 // deploy/00_deploy_tokendistributor_contract.js
 
+const { ethers } = require("hardhat");
+
 module.exports = async ({ getNamedAccounts, deployments }) => {
+  const frontendAddress = "0xC8a0797F73a5b03AC135b6fc7843D78a6DE5Ce32";
+
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
   await deploy("TokenDistributor", {
     from: deployer,
     log: true,
+  });
+
+  await deploy("DummyToken", {
+    from: deployer,
+    log: true,
+  });
+
+  const tokenDistributorContract = await ethers.getContract(
+    "TokenDistributor",
+    deployer
+  );
+  const dummyTokenContract = await ethers.getContract("DummyToken", deployer);
+
+  // transfer ownership to UI owner if needed
+  // tokenDistributorContract.transferOwnership(frontendAddress);
+
+  const mintedBalance = await dummyTokenContract.balanceOf(deployer);
+  const splitValue = mintedBalance.div(ethers.BigNumber.from(2));
+
+  // split tokens between frontendAddress and tokenDistributorContract for later distribution
+  await dummyTokenContract.transfer(frontendAddress, splitValue);
+  await dummyTokenContract.transfer(
+    tokenDistributorContract.address,
+    splitValue
+  );
+
+  const frontendBalance = await dummyTokenContract.balanceOf(frontendAddress);
+  const distributorBalance = await dummyTokenContract.balanceOf(
+    tokenDistributorContract.address
+  );
+
+  console.log({
+    frontend: frontendBalance.toString(),
+    distributor: distributorBalance.toString(),
   });
 };
 module.exports.tags = ["TokenDistributor"];
