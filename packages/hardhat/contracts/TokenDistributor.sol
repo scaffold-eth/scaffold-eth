237f47d610d9f4b4ad0b03a16744a434fc074b93
Bliss Da Sailor <meremezeukah@gmail.com>
2021-08-17 20:56:05 -0500

    add access control and int overflow protection

--- packages/hardhat/contracts/TokenDistributor.sol
+++ packages/hardhat/contracts/TokenDistributor.sol
@@ -2,49 +2,124 @@
 pragma solidity >=0.6.0 <0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract TokenDistributor is Ownable {
-    event shareCompleted(uint256 amount, uint256 share);
+contract TokenDistributor is Ownable, AccessControl {
+    using SafeMath for uint256;
+    bytes32 public constant DISTRIBUTOR_ROLE = keccak256("DISTRIBUTOR_ROLE");
 
-    // split contract's own token
-    function splitToken(
-        address[] memory team,
-        uint256 amount,
-        ERC20 token
-    ) public onlyOwner {
+    event tokenShareCompleted(uint256 amount, uint256 share, address from);
+    event ethShareCompleted(uint256 amount, uint256 share);
+
+    modifier isPermittedDistributor() {
         require(
-            token.balanceOf(address(this)) >= amount,
-            "You don't have enough token balance for this split"
+            hasRole(DISTRIBUTOR_ROLE, msg.sender) || owner() == msg.sender,
+            "Not an approved user"
         );
-        uint256 totalMembers = team.length;
-        uint256 share = amount / totalMembers;
-
-        for (uint256 i = 0; i < team.length; i++) {
-            token.transfer(team[i], share);
-        }
-
-        emit shareCompleted(amount, share);
+        _;
     }
 
-    // contract must be approved spender for selected user
-    function splitTokenFromUser(
-        address user,
-        address[] memory team,
+    modifier isAdminOrOwner() {
+        require(
+            hasRole(DEFAULT_ADMIN_ROLE, msg.sender) || owner() == msg.sender,
+            "You can't perform admin actions"
+        );
+        _;
+    }
+
+    modifier hasValidUsers(address[] memory users) {
+        require(users.length > 0 && users.length < 256, "Invalid users array");
+        _;
+    }
+
+    modifier hasEnoughBalance(uint256 balance, uint256 amount) {
+        require(balance >= amount, "Not enough token balance");
+        _;
+    }
+
+    constructor(address admin) public {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(DISTRIBUTOR_ROLE, msg.sender);
+        // in case of deploying with a different account
+        _setupRole(DEFAULT_ADMIN_ROLE, admin);
+        _setupRole(DISTRIBUTOR_ROLE, admin);
+    }
+
+    function splitEth(address[] memory users, uint256 amount)
+        public
+        isPermittedDistributor
+        hasValidUsers(users)
+        hasEnoughBalance(address(this).balance, amount)
+    {
+        uint256 share = _handleDistribution(
+            users,
+            amount,
+            address(this),
+            "0x"
+            false
+        );
+
+        emit ethShareCompleted(amount, share);
+    }
+
+    // split contract's own token
+    function splitTokenBalance(
+        address[] memory users,
         uint256 amount,
         ERC20 token
-    ) public onlyOwner {
-        require(
-            token.balanceOf(user) >= amount,
-            "You don't have enough token balance for this split"
-        );
-        uint256 totalMembers = team.length;
-        uint256 share = amount / totalMembers;
+    )
+        public
+        isPermittedDistributor
+        hasValidUsers(users)
+        hasEnoughBalance(token.balanceOf(address(this)), amount)
+    {
+        uint256 share = _handleDistribution(users, amount, address(this), token, true);
 
-        for (uint256 i = 0; i < team.length; i++) {
-            token.transferFrom(user, team[i], share);
+        emit tokenShareCompleted(amount, share, address(token));
+    }
+
+    function splitTokenFromUser(
+        address[] memory users,
+        uint256 amount,
+        ERC20 token
+    )
+        public
+        isPermittedDistributor
+        hasValidUsers(users)
+        hasEnoughBalance(token.balanceOf(msg.sender), amount)
+    {
+        uint256 share = _handleDistribution(users, amount, msg.sender, token, true);
+
+        emit tokenShareCompleted(amount, share, msg.sender);
+    }
+
+    function _handleDistribution(
+        address[] memory users,
+        uint256 amount,
+        address from,
+        address token
+        bool isToken,
+    ) private returns (uint256 share) {
+        uint256 totalMembers = users.length;
+        share = amount.div(totalMembers);
+
+        for (uint256 i = 0; i < users.length; i++) {
+            // if split token is requested, split from specified user
+            if (isToken) {
+                ERC20(token).transferFrom(from, users[i], share);
+            } else {
+                payable(users[i]).transfer(share); // else split eth
+            }
         }
+    }
 
-        emit shareCompleted(amount, share);
+    function addNewDistributor(address user) public isAdminOrOwner {
+        grantRole(DISTRIBUTOR_ROLE, user);
+    }
+
+    function revokeDistributor(address user) public isAdminOrOwner {
+        revokeRole(DISTRIBUTOR_ROLE, user);
     }
 }
