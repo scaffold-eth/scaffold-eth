0efaa4c36a1eee4ba2914d4b766d11da427b5870
Bliss Da Sailor <meremezeukah@gmail.com>
2021-08-23 09:46:04 -0500

    switch network to kovan

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -50,7 +50,7 @@ const { ethers } = require("ethers");
 */
 
 /// 📡 What chain are your contracts deployed to?
-const targetNetwork = NETWORKS.localhost; // <------- select your target frontend network (localhost, rinkeby, xdai, mainnet)
+const targetNetwork = NETWORKS.kovan; // <------- select your target frontend network (localhost, rinkeby, xdai, mainnet)
 
 // 😬 Sorry for all the console logging
 const DEBUG = true;
