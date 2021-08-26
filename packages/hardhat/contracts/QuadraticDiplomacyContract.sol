a8a587680995c3bd41ea03c95da020bbec0fc4b9
Bliss Da Sailor <meremezeukah@gmail.com>
2021-08-26 13:42:33 -0500

    contracts update

--- packages/hardhat/contracts/QuadraticDiplomacyContract.sol
+++ packages/hardhat/contracts/QuadraticDiplomacyContract.sol
@@ -2,15 +2,16 @@ pragma solidity >=0.6.7 <0.9.0;
 pragma experimental ABIEncoderV2;
 //SPDX-License-Identifier: MIT
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./Distributor.sol";
 
-contract QuadraticDiplomacyContract is AccessControl {
-
+contract QuadraticDiplomacyContract is Distributor, AccessControl {
     event Vote(address votingAddress, address wallet, uint256 amount);
     event AddMember(address admin, address wallet);
 
     bytes32 public constant VOTER_ROLE = keccak256("VOTER_ROLE");
 
-    mapping (address => uint256) public votes;
+    mapping(address => uint256) public votes;
 
     constructor(address startingAdmin) public {
         _setupRole(DEFAULT_ADMIN_ROLE, startingAdmin);
@@ -18,7 +19,7 @@ contract QuadraticDiplomacyContract is AccessControl {
     }
 
     modifier onlyAdmin() {
-        require( hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "NOT ADMIN");
+        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "NOT ADMIN");
         _;
     }
 
@@ -36,7 +37,10 @@ contract QuadraticDiplomacyContract is AccessControl {
         emit Vote(msg.sender, wallet, amount);
     }
 
-    function voteMultiple(address[] memory wallets, uint256[] memory amounts) public canVote {
+    function voteMultiple(address[] memory wallets, uint256[] memory amounts)
+        public
+        canVote
+    {
         require(wallets.length == amounts.length, "Wrong size");
 
         for (uint256 i = 0; i < wallets.length; i++) {
@@ -61,10 +65,54 @@ contract QuadraticDiplomacyContract is AccessControl {
         emit AddMember(msg.sender, wallet);
     }
 
-    function addMembersWithVotes(address[] memory wallets, uint256 voteAllocation) public onlyAdmin {
+    function addMembersWithVotes(
+        address[] memory wallets,
+        uint256 voteAllocation
+    ) public onlyAdmin {
         for (uint256 i = 0; i < wallets.length; i++) {
             addMember(wallets[i]);
             giveVotes(wallets[i], voteAllocation);
         }
     }
+
+    // expose distributor functions
+    function shareETH(address[] memory users, uint256[] memory shares)
+        public
+        onlyAdmin
+    {
+        _shareETH(users, shares);
+    }
+
+    function sharePayedETH(address[] memory users, uint256[] memory shares)
+        public
+        payable
+        onlyAdmin
+    {
+        // makes sure msg.value has some eth in it
+        _sharePayedETH(users, shares);
+    }
+
+    function shareToken(
+        address[] memory users,
+        uint256[] memory shares,
+        IERC20 token
+    ) public onlyAdmin {
+        _shareToken(users, shares, token);
+    }
+
+    function shareToken(
+        address[] memory users,
+        uint256[] memory shares,
+        IERC20 token,
+        address spender
+    ) public onlyAdmin {
+        _shareToken(users, shares, token, spender);
+    }
+
+    function deposit() public payable {}
+
+    // payable fallback function
+    receive() external payable {}
+
+    fallback() external payable {}
 }
