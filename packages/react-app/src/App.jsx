38fe366fab2de43ceae64b1f583fcbdbb98b17a5
carletex <oceanrdn@gmail.com>
2021-09-07 08:49:49 +0200

    Un-ignore hardhat_contracts. Use Network envvar

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -43,7 +43,7 @@ const { ethers } = require("ethers");
 */
 
 /// 📡 What chain are your contracts deployed to?
-const targetNetwork = NETWORKS.localhost; // <------- select your target frontend network (localhost, rinkeby, xdai, mainnet)
+const targetNetwork = process.env.REACT_APP_NETWORK ? NETWORKS[process.env.REACT_APP_NETWORK] : NETWORKS.localhost;
 
 // 😬 Sorry for all the console logging
 const DEBUG = true;
