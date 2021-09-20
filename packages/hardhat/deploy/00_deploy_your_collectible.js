07ec3791e6c0298c1fa8ed762044674efb4bdf03
calvbore <calv.bore@mail.com>
2021-09-20 01:23:48 -0600

    deploy contract using hardhat-deploy

--- packages/hardhat/deploy/00_deploy_your_collectible.js
+++ packages/hardhat/deploy/00_deploy_your_collectible.js
@@ -1,14 +1,29 @@
 // deploy/00_deploy_your_contract.js
 
-//const { ethers } = require("hardhat");
+const fs = require("fs");
+const { ethers } = require("hardhat");
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
-  await deploy("YourContract", {
+
+  console.log("\n\n 📡 Deploying...\n");
+
+  // read in all the assets to get their IPFS hash...
+  let uploadedAssets = JSON.parse(fs.readFileSync("./uploaded.json"))
+  let bytes32Array = []
+  for(let a in uploadedAssets){
+    console.log(" 🏷 IPFS:",a)
+    let bytes32 = ethers.utils.id(a)
+    console.log(" #️⃣ hashed:",bytes32)
+    bytes32Array.push(bytes32)
+  }
+  console.log(" \n")
+
+  await deploy("YourCollectible", {
     // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
     from: deployer,
-    //args: [ "Hello", ethers.utils.parseEther("1.5") ],
+    args: [ bytes32Array ],
     log: true,
   });
 
@@ -16,9 +31,9 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     // Getting a previously deployed contract
     const YourContract = await ethers.getContract("YourContract", deployer);
     await YourContract.setPurpose("Hello");
-  
-    To take ownership of yourContract using the ownable library uncomment next line and add the 
-    address you want to be the owner. 
+
+    To take ownership of yourContract using the ownable library uncomment next line and add the
+    address you want to be the owner.
     // yourContract.transferOwnership(YOUR_ADDRESS_HERE);
 
     //const yourContract = await ethers.getContractAt('YourContract', "0xaAC799eC2d00C013f1F11c37E654e59B0429DF6A") //<-- if you want to instantiate a version of a contract at a specific address!
@@ -48,4 +63,4 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   });
   */
 };
-module.exports.tags = ["YourContract"];
+module.exports.tags = ["YourCollectible"];
