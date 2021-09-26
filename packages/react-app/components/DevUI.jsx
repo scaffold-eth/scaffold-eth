56dba5e830bc3a21ef6aa4ef9cb2243a8381e2cb
Bliss Da Sailor <meremezeukah@gmail.com>
2021-09-26 11:15:30 -0500

    show networkDisplay

--- packages/react-app/components/DevUI.jsx
+++ packages/react-app/components/DevUI.jsx
@@ -7,6 +7,7 @@ import { Web3Consumer } from "../helpers/Web3Context";
 function DevUI({ web3 }) {
   return (
     <>
+      {web3.networkDisplay}
       {/* 👨‍💼 Your account is in the top right with a wallet at connect options */}
       <div style={{ position: "fixed", textAlign: "right", right: 0, top: 0, padding: 10 }}>
         <Account {...web3} />
