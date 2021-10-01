14f8accd80bddd2d5b8be90567ffa4be25712d45
Mridul Garg <gmridul09@gmail.com>
2021-10-01 21:18:37 +0100

    loogie tank contract

---
+++ packages/hardhat/contracts/ToColor.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+library ToColor {
+    bytes16 internal constant ALPHABET = '0123456789abcdef';
+
+    function toColor(bytes3 value) internal pure returns (string memory) {
+      bytes memory buffer = new bytes(6);
+      for (uint256 i = 0; i < 3; i++) {
+          buffer[i*2+1] = ALPHABET[uint8(value[i]) & 0xf];
+          buffer[i*2] = ALPHABET[uint8(value[i]>>4) & 0xf];
+      }
+      return string(buffer);
+    }
+}
