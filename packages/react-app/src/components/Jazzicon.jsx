979a688d57748a4afccd1cd939cad126ae358420
Dan Finlay <dan@danfinlay.com>
2021-10-09 19:48:18 -0700

    Add jazzicon component
    Uses same interface as blockie component

--- packages/react-app/src/components/Jazzicon.jsx
+++ packages/react-app/src/components/Jazzicon.jsx
@@ -1,17 +1,13 @@
 import React from "react";
-import Jazzicon from 'react-jazzicon'
-import { toChecksumAddress } from 'ethereumjs-util';
+import Jazzicon from "react-jazzicon";
+import { toChecksumAddress } from "ethereumjs-util";
 
-// provides a blockie image for the address using "react-blockies" library
-export default function Blockie(props) {
+export default function JazziconComponent (props) {
   if (!props.address || typeof props.address.toLowerCase !== "function") {
     return <span />;
   }
   // eslint-disable-next-line react/jsx-props-no-spreading
   return <Jazzicon seed={jsNumberForAddress(props.address)} diameter={25} {...props} />
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  // return <Blockies seed={props.address.toLowerCase()} {...props} />;
-
 }
 
 function jsNumberForAddress(lowCaseAddr) {
