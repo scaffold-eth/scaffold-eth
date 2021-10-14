510117d05cde19d6a5d682898665d4682f69096d
Mark Grothe <grothem@gmail.com>
2021-10-14 10:55:24 -0500

    Inherit erc721 enumerable

--- packages/hardhat/contracts/YourCollectible.sol
+++ packages/hardhat/contracts/YourCollectible.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.8.0;
 //SPDX-License-Identifier: MIT
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
@@ -13,7 +14,7 @@ import './ToColor.sol';
 
 // GET LISTED ON OPENSEA: https://testnets.opensea.io/get-listed/step-two
 
-contract YourCollectible is ERC721, Ownable {
+contract YourCollectible is ERC721Enumerable, Ownable {
 
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
