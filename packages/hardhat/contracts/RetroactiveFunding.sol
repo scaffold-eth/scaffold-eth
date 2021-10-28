f5dc5c0c24959aa58c883dbba903894dd7ea760a
viraj124 <virajm72@gmail.com>
2021-10-28 22:25:13 +0530

    refactor: initial version of the contract

---
+++ packages/hardhat/contracts/RetroactiveFunding.sol
@@ -0,0 +1,50 @@
+pragma solidity >=0.8.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract RetroactiveFunding {
+    uint128 constant MAX_FLOOR_LIMIT = 1000 ether;
+
+    IERC20 public immutable weth;
+
+    struct FloorConfig {
+        uint128 floor;
+        uint128 quantity;
+    }
+
+    mapping(address => mapping(address => FloorConfig)) public whaleFloorRequest;
+
+    constructor(IERC20 _weth) {
+        weth = _weth;
+    }
+
+    function requestToIncreaseFloor(
+        IERC721 _nft,
+        uint128 _floor,
+        uint128 _quantity
+    ) external {
+        require(_floor > 0, "RetroactiveFunding: invalid floor amount");
+        require(
+            _floor < MAX_FLOOR_LIMIT,
+            "RetroactiveFunding: floor limit reached"
+        );
+        require(
+            weth.allowance(msg.sender, address(this)) >= _floor * _quantity,
+            "RetroactiveFunding: weth allowance missing"
+        );
+        whaleFloorRequest[address(_nft)][msg.sender] = FloorConfig(
+            _floor,
+            _quantity
+        );
+    }
+
+    function executeSale(IERC721 _nft, address _whale, uint[] calldata _ids) external {
+        FloorConfig storage floorConfig = whaleFloorRequest[address(_nft)][_whale];
+        floorConfig.quantity = floorConfig.quantity - uint128(_ids.length);
+        weth.transferFrom(_whale, msg.sender, floorConfig.floor * _ids.length);
+        for(uint i = 0; i < _ids.length; i++){
+            _nft.safeTransferFrom(msg.sender, address(0), _ids[i]);
+        }
+    }
+}
