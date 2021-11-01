32c284a838c2a53f901e719cbc438e58f48d042a
PowVT <epowlowsky@gmail.com>
2021-11-01 14:18:24 -0400

    master updates

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -66,12 +66,8 @@ module.exports = {
   networks: {
     localhost: {
       url: "http://localhost:8545",
-<<<<<<< HEAD
       //gasPrice: 125000000000,//you can adjust gasPrice locally to see how much it will cost on production
       /*
-=======
-      /*      
->>>>>>> master
         notice no mnemonic here? it will just use account 0 of the hardhat node to deploy
         (you can put in a mnemonic here to set the deployer locally)
       
