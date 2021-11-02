6a8c347d918d2ede16eada92d0df63d460436da5
tomMobileDev123 <tomMobileDev123@me.me>
2021-11-02 18:37:24 -0400

    Various approaches to call constructors

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -3,6 +3,22 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "hardhat/console.sol";
 
-contract YourContract {
+contract X {
+  string public name;
+
+  constructor(string memory _name) {
+    name = _name;
+  }
+}
+
+contract Y {
+  string public text;
+
+  constructor (string memory _text) {
+    text = _text;
+  }
+}
+
+contract YourContract is X("Input to X"), Y("Input to Y") {
 
 }
