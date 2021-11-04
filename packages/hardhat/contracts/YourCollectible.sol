cd965ff39206dc886e4858fbfc47f5c26fbe3956
Bliss Da Sailor <meremezeukah@gmail.com>
2021-11-04 14:24:04 -0500

    contract updates

---
+++ packages/hardhat/contracts/YourCollectible.sol
@@ -0,0 +1,34 @@
+pragma solidity >=0.6.0 <0.7.0;
+//SPDX-License-Identifier: MIT
+
+//import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+//learn more: https://docs.openzeppelin.com/contracts/3.x/erc721
+
+// GET LISTED ON OPENSEA: https://testnets.opensea.io/get-listed/step-two
+
+contract YourCollectible is ERC721, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() public ERC721("YourCollectible", "YCB") {
+        _setBaseURI("https://ipfs.io/ipfs/");
+    }
+
+    function mintItem(address to, string memory tokenURI)
+        public
+        onlyOwner
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 id = _tokenIds.current();
+        _mint(to, id);
+        _setTokenURI(id, tokenURI);
+
+        return id;
+    }
+}
--- packages/hardhat/scripts/deploy.js
+++ packages/hardhat/scripts/deploy.js
@@ -26,9 +26,7 @@ And learn more here: https://www.npmjs.com/package/hardhat-deploy
 const main = async () => {
   console.log("\n\n 📡 Deploying...\n");
 
-  const yourContract = await deploy("YourContract"); // <-- add in constructor args like line 19 vvvv
-  // use for local token bridging
-  // const mockToken = await deploy("MockERC20") // <-- add in constructor args like line 19 vvvv
+  const yourCollectible = await deploy("YourCollectible") // <-- add in constructor args like line 19 vvvv
 
   //const yourContract = await ethers.getContractAt('YourContract', "0xaAC799eC2d00C013f1F11c37E654e59B0429DF6A") //<-- if you want to instantiate a version of a contract at a specific address!
   //const secondContract = await deploy("SecondContract")
