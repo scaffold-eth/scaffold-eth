6b1c259f9581b8430a59d311f94d9db03fdffa85
Bliss Da Sailor <meremezeukah@gmail.com>
2021-11-05 00:08:28 -0500

    vimeo player and jwt decode

--- packages/react-app/src/views/Home.jsx
+++ packages/react-app/src/views/Home.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
-import { Button, notification } from "antd";
+import { Button, notification, Modal } from "antd";
 import axios from "axios";
-import arm from '../assets/g35.png'
-import playFilmImg from '../assets/earth.png'
+import jwtDecode from "jwt-decode";
+import Vimeo from "@u-wave/react-vimeo";
+import arm from "../assets/g35.png";
+import playFilmImg from "../assets/earth.png";
+import { X } from "react-feather";
 
 /* This Home build uses the custom auth process
   - Signature, message and address is sent to the server for verification and custom JWT token
@@ -13,6 +16,10 @@ const server = "http://localhost:49832";
 function Home({ userSigner, web3Modal, loadWeb3Modal }) {
   const [isSigning, setIsSigning] = useState(false);
   const [token, setToken] = useState(null);
+  const [video, setVideo] = useState(null);
+  const [play, setPlay] = useState(false);
+
+  const closeIcon = <X size={30} />;
 
   const sendNotification = (type, data) => {
     return notification[type]({
@@ -22,7 +29,6 @@ function Home({ userSigner, web3Modal, loadWeb3Modal }) {
   };
 
   const handleSignIn = async () => {
-
     if (web3Modal.cachedProvider == "") {
       return sendNotification("error", {
         message: "Failed to Sign In!",
@@ -42,6 +48,16 @@ function Home({ userSigner, web3Modal, loadWeb3Modal }) {
       // send signature here for auth token
       const { data } = await axios.post(`${server}/signIn`, { signature, message, address });
 
+      // decode JWT token
+      const decodedToken = jwtDecode(data.authToken);
+
+      console.log(decodedToken);
+
+      if (decodedToken.address.toLowerCase() !== address.toLowerCase()) {
+        throw new Error("Invalid connected address and signer match");
+      }
+
+      setVideo(decodedToken.video);
       setToken(data.authToken);
 
       // notify user of sign-in
@@ -67,38 +83,30 @@ function Home({ userSigner, web3Modal, loadWeb3Modal }) {
             1. To watch the film, <br />
             first link your wallet.
           </h2>
-          <Button
-            key="loginbutton"
-            onClick={loadWeb3Modal}
-          >
+          <Button key="loginbutton" onClick={loadWeb3Modal}>
             connect wallet
           </Button>
           <h2 className="instruction">(You will have to present a token once your wallet is linked.)</h2>
-        </div>
-        ,
+        </div>,
       );
     } else if (web3Modal.cachedProvider && !token) {
       phases.push(
         <div>
           <h2 className="instruction">
-            2. Thanks for linking your wallet. Next, sign<br />
-            a message presenting your token.
+            2. Thanks for linking your wallet. Next, sign
+            <br />a message presenting your token.
           </h2>
-          <Button loading={isSigning}
-            onClick={handleSignIn}>
+          <Button loading={isSigning} onClick={handleSignIn}>
             sign message
           </Button>
-        </div>
-        ,
+        </div>,
       );
     } else if (web3Modal.cachedProvider && token) {
       phases.push(
-        <div>
-          <h2 className="instruction earth-small">
-            3. Click to play the film. Enjoy!
-          </h2>
-          <img className="play-film" src={playFilmImg} alt="click to play the film" />
-        </div>
+        <div style={{ marginTop: 30, marginBottom: "10rem" }}>
+          <h2 className="instruction earth-small">3. Click to play the film. Enjoy!</h2>
+          <img className="play-film" src={playFilmImg} alt="click to play the film" onClick={() => setPlay(true)} />
+        </div>,
       );
     }
   }
@@ -106,9 +114,22 @@ function Home({ userSigner, web3Modal, loadWeb3Modal }) {
   return (
     <div className="home">
       <img className="arm" src={arm} alt="arm and hand pointing to button" />
-
       {phases}
 
+      {play && video && (
+        <Modal
+          title={null}
+          centered
+          visible={play}
+          footer={null}
+          padding={0}
+          width="700"
+          closeIcon={closeIcon}
+          onCancel={() => setPlay(false)}
+        >
+          <Vimeo video={video} width="700" height="390" style={{ backgroundColor: "transparent" }} showTitle={false} />
+        </Modal>
+      )}
     </div>
   );
 }
