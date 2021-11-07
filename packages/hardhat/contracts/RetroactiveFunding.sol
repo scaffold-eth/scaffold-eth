e0e7c6a962aeb980b546c1a2ad28cb925530cdce
Temirzhan Yussupov <timka2609@gmail.com>
2021-11-07 15:26:23 +0600

    feat: update the contract logic

--- packages/hardhat/contracts/RetroactiveFunding.sol
+++ packages/hardhat/contracts/RetroactiveFunding.sol
@@ -4,19 +4,32 @@ import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract RetroactiveFunding {
+    /// @dev mapping which tracks the amount deposited for each nft
+    mapping(address => uint256) public amount;
 
-    /// @dev mapping which tracks the floor for each nft
-    mapping(address => uint) public floor;
+    /// @dev mapping which maps NFT addresses to supplies
+    mapping(address => uint256) public totalSupply;
+
+    /// @dev mapping which tracks whether NFT collection was added to the contract in the past
+    mapping(address => bool) public addedBefore;
 
     /**
      * @notice Whale increasesfloor price for a particular nft by locking in a specific amount of eth and floor is calulated based on eth locked and nft's total supply
      * @param _nft nft address
      */
     function increaseFloor(IERC721Enumerable _nft) external payable {
-       uint totalSupply = _nft.totalSupply();
-       floor[address(_nft)] = floor[address(_nft)] + (msg.value / totalSupply);
-       (bool success, ) = msg.sender.call{value: msg.value}("");
-       require(success);
+        require(msg.value > 0, "RetroactiveFunding: zero payment");
+
+        if (!addedBefore[address(_nft)]) {
+            require(
+                _nft.totalSupply() > 0,
+                "RetroactiveFunding: invalid collection passed"
+            );
+            addedBefore[address(_nft)] = true;
+            totalSupply[address(_nft)] = _nft.totalSupply();
+        }
+
+        amount[address(_nft)] += msg.value;
     }
 
     /**
@@ -24,19 +37,28 @@ contract RetroactiveFunding {
      * @param _nft nft address
      * @param _id nft id
      */
-    function executeSale(
-        IERC721Enumerable _nft,
-        uint256 _id
-    ) external {
-        uint currentFloor = floor[address(_nft)];
-        // updating the total supply to calculate the new floor
-        uint updatedTotalSupply = _nft.totalSupply() - 1;
-        // updating floor price by subtracting the current floor by the ratio of the sale amount and the new total supply
-        require(floor[address(_nft)] > currentFloor / updatedTotalSupply, "RetroactiveFunding: sale cannot be made right now!");
-        floor[address(_nft)] = floor[address(_nft)] - currentFloor / updatedTotalSupply; 
+    function executeSale(IERC721Enumerable _nft, uint256 _id) external {
+        require(
+            _nft.ownerOf(_id) == msg.sender,
+            "RetroactiveFunding: invalid ERC721 _id passed"
+        );
+        require(
+            totalSupply[address(_nft)] > 0 && amount[address(_nft)] > 0,
+            "RetroactiveFunding: sale execution is not possible"
+        );
+
+        uint256 currentFloor = amount[address(_nft)] /
+            totalSupply[address(_nft)];
+        require(currentFloor > 0, "RetroactiveFunding: zero floor price");
+
         (bool success, ) = msg.sender.call{value: currentFloor}("");
         require(success);
-        // burn the nft's approval required
-        _nft.safeTransferFrom(msg.sender, address(0), _id);
+
+        amount[address(_nft)] -= currentFloor;
+        totalSupply[address(_nft)]--;
+
+        // Approval for address 0x1 is required
+        // Address 0x0 is prohibited by ERC721
+        _nft.safeTransferFrom(msg.sender, address(1), _id);
     }
 }
