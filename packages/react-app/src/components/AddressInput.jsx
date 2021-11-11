346de13b03c39b0f94166f2e97cc5331df53effe
Sean Magin <sean@arrowburn.co.nz>
2021-11-12 00:00:27 +1300

    cleanup address input component

--- packages/react-app/src/components/AddressInput.jsx
+++ packages/react-app/src/components/AddressInput.jsx
@@ -33,27 +33,13 @@ import Blockie from "./Blockie";
 */
 
 export default function AddressInput(props) {
+  const { ensProvider, onChange } = props;
   const [value, setValue] = useState(props.value);
   const [scan, setScan] = useState(false);
 
   const currentValue = typeof props.value !== "undefined" ? props.value : value;
   const ens = useLookupAddress(props.ensProvider, currentValue);
 
-  const scannerButton = (
-    <div
-      style={{ marginTop: 4, cursor: "pointer" }}
-      onClick={() => {
-        setScan(!scan);
-      }}
-    >
-      <Badge count={<CameraOutlined style={{ fontSize: 9 }} />}>
-        <QrcodeOutlined style={{ fontSize: 18 }} />
-      </Badge>{" "}
-      Scan
-    </div>
-  );
-
-  const { ensProvider, onChange } = props;
   const updateAddress = useCallback(
     async newValue => {
       if (typeof newValue !== "undefined") {
@@ -76,48 +62,46 @@ export default function AddressInput(props) {
     [ensProvider, onChange],
   );
 
-  const scanner = scan ? (
-    <div
-      style={{
-        zIndex: 256,
-        position: "absolute",
-        left: 0,
-        top: 0,
-        width: "100%",
-      }}
-      onClick={() => {
-        setScan(false);
-      }}
-    >
-      <QrReader
-        delay={250}
-        resolution={1200}
-        onError={e => {
-          console.log("SCAN ERROR", e);
-          setScan(false);
-        }}
-        onScan={newValue => {
-          if (newValue) {
-            console.log("SCAN VALUE", newValue);
-            let possibleNewValue = newValue;
-            if (possibleNewValue.indexOf("/") >= 0) {
-              possibleNewValue = possibleNewValue.substr(possibleNewValue.lastIndexOf("0x"));
-              console.log("CLEANED VALUE", possibleNewValue);
-            }
-            setScan(false);
-            updateAddress(possibleNewValue);
-          }
-        }}
-        style={{ width: "100%" }}
-      />
-    </div>
-  ) : (
-    ""
-  );
-
   return (
     <div>
-      {scanner}
+      {scan ? (
+        <div
+          style={{
+            zIndex: 256,
+            position: "absolute",
+            left: 0,
+            top: 0,
+            width: "100%",
+          }}
+          onClick={() => {
+            setScan(false);
+          }}
+        >
+          <QrReader
+            delay={250}
+            resolution={1200}
+            onError={e => {
+              console.log("SCAN ERROR", e);
+              setScan(false);
+            }}
+            onScan={newValue => {
+              if (newValue) {
+                console.log("SCAN VALUE", newValue);
+                let possibleNewValue = newValue;
+                if (possibleNewValue.indexOf("/") >= 0) {
+                  possibleNewValue = possibleNewValue.substr(possibleNewValue.lastIndexOf("0x"));
+                  console.log("CLEANED VALUE", possibleNewValue);
+                }
+                setScan(false);
+                updateAddress(possibleNewValue);
+              }
+            }}
+            style={{ width: "100%" }}
+          />
+        </div>
+      ) : (
+        ""
+      )}
       <Input
         id="0xAddress" // name it something other than address for auto fill doxxing
         name="0xAddress" // name it something other than address for auto fill doxxing
@@ -126,7 +110,19 @@ export default function AddressInput(props) {
         placeholder={props.placeholder ? props.placeholder : "address"}
         prefix={<Blockie address={currentValue} size={8} scale={3} />}
         value={ens || currentValue}
-        addonAfter={scannerButton}
+        addonAfter={
+          <div
+            style={{ marginTop: 4, cursor: "pointer" }}
+            onClick={() => {
+              setScan(!scan);
+            }}
+          >
+            <Badge count={<CameraOutlined style={{ fontSize: 9 }} />}>
+              <QrcodeOutlined style={{ fontSize: 18 }} />
+            </Badge>{" "}
+            Scan
+          </div>
+        }
         onChange={e => {
           updateAddress(e.target.value);
         }}
