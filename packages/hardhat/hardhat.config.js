ee07cb1282074b8929ed11c3555f1b5949d71790
Adam Fuller <azf20@users.noreply.github.com>
2021-11-12 17:36:59 +0000

    Update hardhat.config.js

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -9,7 +9,6 @@ require("@tenderly/hardhat-tenderly");
 require("hardhat-deploy");
 require("hardhat-gas-reporter");
 
-require("@eth-optimism/hardhat-ovm");
 require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-etherscan");
 
@@ -205,8 +204,17 @@ module.exports = {
         l2: "localArbitrum",
       },
     },
+    optimism: {
+      url: "https://mainnet.optimism.io",
+      accounts: {
+        mnemonic: mnemonic(),
+      },
+      companionNetworks: {
+        l1: "mainnet",
+      },
+    },
     kovanOptimism: {
-      url: "https://kovan.optimism.io",
+      url: "https://mainnet.optimism.io",
       accounts: {
         mnemonic: mnemonic(),
       },
@@ -216,11 +224,9 @@ module.exports = {
     },
     localOptimism: {
       url: "http://localhost:8545",
-      gasPrice: 0,
       accounts: {
         mnemonic: mnemonic(),
       },
-      ovm: true,
       companionNetworks: {
         l1: "localOptimismL1",
       },
