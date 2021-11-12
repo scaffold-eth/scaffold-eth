06c27db1c8f3fc17db8bfcfcfb3be8f1a8b87e60
Brendan Boyle <brendanboyle87@gmail.com>
2021-11-12 08:18:12 -0500

    remove unneeded safe math

--- packages/hardhat/contracts/DEX.sol
+++ packages/hardhat/contracts/DEX.sol
@@ -2,13 +2,10 @@ pragma solidity >=0.8.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 
 import "hardhat/console.sol";
-
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract DEX {
 
-  using SafeMath for uint256;
   IERC20 token;
 
   constructor(address token_addr) {
