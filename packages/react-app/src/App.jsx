a5c7443ee9f4886a6d9832e205c1507405a59820
DeveloperMarwan <marwan@wanmar.com>
2021-11-14 12:57:31 -0500

    Enhance,ents
    - Add support for URL parameters
    - Clean up page layout

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -6,7 +6,7 @@ import WalletLink from "walletlink";
 import { Alert, Button, Col, Menu, Row, Input } from "antd";
 import "antd/dist/antd.css";
 import React, { useCallback, useEffect, useState } from "react";
-import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Link, Route, Switch, useParams } from "react-router-dom";
 import Web3Modal from "web3modal";
 import "./App.css";
 import { Account, Contract, Faucet, GasGauge, Header, Ramp, ThemeSwitch, AddressInput } from "./components";
@@ -99,75 +99,6 @@ const walletLink = new WalletLink({
 const walletLinkProvider = walletLink.makeWeb3Provider(`https://mainnet.infura.io/v3/${INFURA_ID}`, 1);
 
 // Portis ID: 6255fb2b-58c8-433b-a2c9-62098c05ddc9
-/*
-  Web3 modal helps us "connect" external wallets:
-*/
-/*
-const web3Modal = new Web3Modal({
-  network: "mainnet", // Optional. If using WalletConnect on xDai, change network to "xdai" and add RPC info below for xDai chain.
-  cacheProvider: true, // optional
-  theme: "light", // optional. Change to "dark" for a dark theme.
-  providerOptions: {
-    walletconnect: {
-      package: WalletConnectProvider, // required
-      options: {
-        bridge: "https://polygon.bridge.walletconnect.org",
-        infuraId: INFURA_ID,
-        rpc: {
-          1: `https://mainnet.infura.io/v3/${INFURA_ID}`, // mainnet // For more WalletConnect providers: https://docs.walletconnect.org/quick-start/dapps/web3-provider#required
-          42: `https://kovan.infura.io/v3/${INFURA_ID}`,
-          100: "https://dai.poa.network", // xDai
-        },
-      },
-    },
-    portis: {
-      display: {
-        logo: "https://user-images.githubusercontent.com/9419140/128913641-d025bc0c-e059-42de-a57b-422f196867ce.png",
-        name: "Portis",
-        description: "Connect to Portis App",
-      },
-      package: Portis,
-      options: {
-        id: "6255fb2b-58c8-433b-a2c9-62098c05ddc9",
-      },
-    },
-    fortmatic: {
-      package: Fortmatic, // required
-      options: {
-        key: "pk_live_5A7C91B2FC585A17", // required
-      },
-    },
-    // torus: {
-    //   package: Torus,
-    //   options: {
-    //     networkParams: {
-    //       host: "https://localhost:8545", // optional
-    //       chainId: 1337, // optional
-    //       networkId: 1337 // optional
-    //     },
-    //     config: {
-    //       buildEnv: "development" // optional
-    //     },
-    //   },
-    // },
-    "custom-walletlink": {
-      display: {
-        logo: "https://play-lh.googleusercontent.com/PjoJoG27miSglVBXoXrxBSLveV6e3EeBPpNY55aiUUBM9Q1RCETKCOqdOkX2ZydqVf0",
-        name: "Coinbase",
-        description: "Connect to Coinbase Wallet (not Coinbase App)",
-      },
-      package: walletLinkProvider,
-      connector: async (provider, _options) => {
-        await provider.enable();
-        return provider;
-      },
-    },
-    authereum: {
-      package: Authereum, // required
-    },
-  },
-});
-*/
 
 function App(props) {
   const mainnetProvider =
@@ -252,12 +183,6 @@ function App(props) {
   const myMainnetDAIBalance = useContractReader(mainnetContracts, "DAI", "balanceOf", [
     "0x34aA3F359A9D614239015126635CE7732c18fDF3",
   ]);
-
-  // keep track of a variable from the contract in the local React state:
-  const purpose = useContractReader(readContracts, "YourContract", "purpose");
-
-  // 📟 Listen for broadcast events
-  const setPurposeEvents = useEventListener(readContracts, "YourContract", "SetPurpose", localProvider, 1);
 
   /*
   const addressFromENS = useResolveName(mainnetProvider, "austingriffith.eth");
@@ -398,39 +323,6 @@ function App(props) {
     }
   }, [loadWeb3Modal]);
 
-  /*
-  const loadWeb3Modal = useCallback(async () => {
-    const provider = await web3Modal.connect();
-    setInjectedProvider(new ethers.providers.Web3Provider(provider));
-
-    provider.on("chainChanged", chainId => {
-      console.log(`chain changed to ${chainId}! updating providers`);
-      setInjectedProvider(new ethers.providers.Web3Provider(provider));
-    });
-
-    provider.on("accountsChanged", () => {
-      console.log(`account changed!`);
-      setInjectedProvider(new ethers.providers.Web3Provider(provider));
-    });
-
-    // Subscribe to session disconnection
-    provider.on("disconnect", (code, reason) => {
-      console.log(code, reason);
-      logoutOfWeb3Modal();
-    });
-  }, [setInjectedProvider]);
-
-  useEffect(() => {
-    if (web3Modal.cachedProvider) {
-      loadWeb3Modal();
-    }
-  }, [loadWeb3Modal]);
-  */
-  const [route, setRoute] = useState();
-  useEffect(() => {
-    setRoute(window.location.pathname);
-  }, [setRoute]);
-
   let faucetHint = "";
   const faucetAvailable = localProvider && localProvider.connection && targetNetwork.name.indexOf("local") !== -1;
 
@@ -467,30 +359,44 @@ function App(props) {
   let theExternalContract = useExternalContractLoader(injectedProvider, contractAddress, contractABI);
 
   let externalContractDisplay = "";
-  /*
-  useEffect(() => {
-    if (contractAddress && contractABI) {
-      externalContractDisplay = (
+  if (contractAddress && contractABI) {
+    externalContractDisplay = (
+      <div>
         <div style={{ padding: 32, backgroundColor: "#eeffef", fontWeight: "bolder" }}>
           🚀 🎖 👩‍🚀 - External contract data entered -- PROCEED -- 🎉 🍾 🎊
         </div>
-      );
-    }
-  }, [contractAddress, contractABI]);
-  */
-
-  if (contractAddress && contractABI) {
-    externalContractDisplay = (
-      <><div style={{ padding: 32, backgroundColor: "#eeffef", fontWeight: "bolder" }}>
-        🚀 🎖 👩‍🚀 - External contract data entered -- PROCEED -- 🎉 🍾 🎊
-      </div><Contract
+        <Contract
           customContract={theExternalContract}
           signer={userSigner}
           provider={localProvider}
-          chainId={selectedChainId} /></>
+          chainId={selectedChainId} 
+        />
+      </div>
     );
   } else {
     theExternalContract = null;
+  }
+
+  function AddressFromURL() {
+    let { addr, abi } = useParams();
+    let theExternalContractFromURL = useExternalContractLoader(injectedProvider, addr, abi);
+    // setContractAddress(addr);
+    // setContractABI(abi);
+
+    return (
+      <div>
+        {/*externalContractDisplay*/}
+        <div style={{ padding: 32, backgroundColor: "#eeffef", fontWeight: "bolder" }}>
+          🚀 🎖 👩‍🚀 - External contract data entered -- PROCEED -- 🎉 🍾 🎊
+        </div>
+        <Contract
+          customContract={theExternalContractFromURL}
+          signer={userSigner}
+          provider={localProvider}
+          chainId={selectedChainId} 
+        />
+      </div>
+    );
   }
 
   console.log("==-- userSigner: ", userSigner);
@@ -507,49 +413,13 @@ function App(props) {
       <Header />
       {networkDisplay}
       <BrowserRouter>
-        <Menu style={{ textAlign: "center" }} selectedKeys={[route]} mode="horizontal">
-          <Menu.Item key="/">
-            <Link
-              onClick={() => {
-                setRoute("/");
-              }}
-              to="/"
-            >
-              YourContract
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="/externalContract">
-            <Link
-              onClick={() => {
-                setRoute("/externalContract");
-              }}
-              to="/externalContract"
-            >
-              External Contract
-            </Link>
-          </Menu.Item>
-        </Menu>
-
         <Switch>
-          <Route exact path="/">
-            {/*
-                🎛 this scaffolding is full of commonly used components
-                this <Contract/> component will automatically parse your ABI
-                and give you a form to interact with it locally
-            */}
-            <Contract
-              name="YourContract"
-              signer={userSigner}
-              provider={localProvider}
-              address={address}
-              blockExplorer={blockExplorer}
-              contractConfig={contractConfig}
-              chainId={selectedChainId}
-            />
+          <Route path="/contract/:addr/:abi">
+            <AddressFromURL />
           </Route>
-          <Route path="/externalContract">
-            <div>Paste the contract's address and ABI below:</div>
-            <div style={{ margin: 8 }}>
+          <Route exact path="/">
+          <div>Paste the contract's address and ABI below:</div>
+            <div class="center" style={{ width: "50%"}}>
               <div style={{ padding: 4 }}>
                 <AddressInput
                   placeholder="Enter Contract Address"
@@ -560,6 +430,7 @@ function App(props) {
               </div>
               <div style={{ padding: 4 }}>
                 <TextArea
+                  rows={6} 
                   placeholder="Enter Contract ABI JSON"
                   value={contractABI}
                   onChange={e => {
@@ -567,12 +438,14 @@ function App(props) {
                   }}
                 />
               </div>
+              {/*externalContractDisplay*/}
+            </div>
+            <div>
               {externalContractDisplay}
             </div>
           </Route>
         </Switch>
       </BrowserRouter>
-
       <ThemeSwitch />
 
       {/* 👨‍💼 Your account is in the top right with a wallet at connect options */}
