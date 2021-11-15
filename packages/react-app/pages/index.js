63f32e9eec7ad9647afe61fcf3fa09fa823849ee
Bliss Da Sailor <meremezeukah@gmail.com>
2021-11-14 19:00:35 -0600

    move account to _app.jsx

--- packages/react-app/pages/index.js
+++ packages/react-app/pages/index.js
@@ -1,37 +1,47 @@
 import React, { useContext } from "react";
-import { Contract } from "../components";
-import { Web3Context } from "../helpers/Web3Context";
+import { Contract, Account, Header } from "../components";
+import { Web3Consumer } from "../helpers/Web3Context";
 
-function Home() {
-  const web3 = useContext(Web3Context);
-
+function Home({ web3 }) {
   console.log(`🗄 web3 context:`, web3);
 
   return (
-    <div className="flex flex-1 flex-col h-screen w-full items-center">
-      <div className="text-center" style={{ margin: 64 }}>
-        <span>This App is powered by Scaffold-eth & Next.js!</span>
-        <br />
-        <span>
-          Added{" "}
-          <a href="https://tailwindcomponents.com/cheatsheet/" target="_blank" rel="noreferrer">
-            TailwindCSS
-          </a>{" "}
-          for easier styling.
-        </span>
+    <>
+      {/* Page Header start */}
+      <div className="flex flex-1 justify-between items-center">
+        <Header />
+        <div className="mr-6">
+          <Account {...web3} />
+        </div>
       </div>
-      <div className="text-center">
-        <Contract
-          name="YourContract"
-          signer={web3.userSigner}
-          provider={web3.localProvider}
-          address={web3.address}
-          blockExplorer={web3.blockExplorer}
-          contractConfig={web3.contractConfig}
-        />
+      {/* Page Header end */}
+
+      {/* Main Page Content start */}
+      <div className="flex flex-1 flex-col h-screen w-full items-center">
+        <div className="text-center" style={{ margin: 64 }}>
+          <span>This App is powered by Scaffold-eth & Next.js!</span>
+          <br />
+          <span>
+            Added{" "}
+            <a href="https://tailwindcomponents.com/cheatsheet/" target="_blank" rel="noreferrer">
+              TailwindCSS
+            </a>{" "}
+            for easier styling.
+          </span>
+        </div>
+        <div className="text-center">
+          <Contract
+            name="YourContract"
+            signer={web3.userSigner}
+            provider={web3.localProvider}
+            address={web3.address}
+            blockExplorer={web3.blockExplorer}
+            contractConfig={web3.contractConfig}
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
-export default Home;
+export default Web3Consumer(Home);
