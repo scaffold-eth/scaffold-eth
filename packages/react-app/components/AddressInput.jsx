7d91dfe66462dcf395447616a727a418dc2887b7
Bliss Da Sailor <meremezeukah@gmail.com>
2021-11-14 19:02:19 -0600

    add backspace fix for full ENS

--- packages/react-app/components/AddressInput.jsx
+++ packages/react-app/components/AddressInput.jsx
@@ -2,9 +2,11 @@ import { CameraOutlined, QrcodeOutlined } from "@ant-design/icons";
 import { Badge, Input } from "antd";
 import { useLookupAddress } from "eth-hooks/dapps/ens";
 import React, { useCallback, useState } from "react";
-import dynamic from "next/dynamic";
-const QrReader = dynamic(() => import("react-qr-reader"), { ssr: false });
 import Blockie from "./Blockie";
+import dynamic from "next/dynamic";
+import { ethers } from "ethers";
+
+const QrReader = dynamic(() => import("react-qr-reader"), { ssr: false });
 
 // probably we need to change value={toAddress} to address={toAddress}
 
@@ -33,33 +35,21 @@ import Blockie from "./Blockie";
                           or onChange={address => { setToAddress(address);}}
 */
 
+const isENS = (address = "") => address.endsWith(".eth") || address.endsWith(".xyz");
+
 export default function AddressInput(props) {
+  const { ensProvider, onChange } = props;
   const [value, setValue] = useState(props.value);
   const [scan, setScan] = useState(false);
 
   const currentValue = typeof props.value !== "undefined" ? props.value : value;
   const ens = useLookupAddress(props.ensProvider, currentValue);
 
-  const scannerButton = (
-    <div
-      style={{ marginTop: 4, cursor: "pointer" }}
-      onClick={() => {
-        setScan(!scan);
-      }}
-    >
-      <Badge count={<CameraOutlined style={{ fontSize: 9 }} />}>
-        <QrcodeOutlined style={{ fontSize: 18 }} />
-      </Badge>{" "}
-      Scan
-    </div>
-  );
-
-  const { ensProvider, onChange } = props;
   const updateAddress = useCallback(
     async newValue => {
       if (typeof newValue !== "undefined") {
         let address = newValue;
-        if (address.indexOf(".eth") > 0 || address.indexOf(".xyz") > 0) {
+        if (isENS(address)) {
           try {
             const possibleAddress = await ensProvider.resolveName(address);
             if (possibleAddress) {
@@ -77,48 +67,46 @@ export default function AddressInput(props) {
     [ensProvider, onChange],
   );
 
-  const scanner = scan ? (
-    <div
-      style={{
-        zIndex: 256,
-        position: "absolute",
-        left: 0,
-        top: 0,
-        width: "100%",
-      }}
-      onClick={() => {
-        setScan(false);
-      }}
-    >
-      <QrReader
-        delay={250}
-        resolution={1200}
-        onError={e => {
-          console.log("SCAN ERROR", e);
-          setScan(false);
-        }}
-        onScan={newValue => {
-          if (newValue) {
-            console.log("SCAN VALUE", newValue);
-            let possibleNewValue = newValue;
-            if (possibleNewValue.indexOf("/") >= 0) {
-              possibleNewValue = possibleNewValue.substr(possibleNewValue.lastIndexOf("0x"));
-              console.log("CLEANED VALUE", possibleNewValue);
-            }
-            setScan(false);
-            updateAddress(possibleNewValue);
-          }
-        }}
-        style={{ width: "100%" }}
-      />
-    </div>
-  ) : (
-    ""
-  );
-
   return (
     <div>
-      {scanner}
+      {scan ? (
+        <div
+          style={{
+            zIndex: 256,
+            position: "absolute",
+            left: 0,
+            top: 0,
+            width: "100%",
+          }}
+          onClick={() => {
+            setScan(false);
+          }}
+        >
+          <QrReader
+            delay={250}
+            resolution={1200}
+            onError={e => {
+              console.log("SCAN ERROR", e);
+              setScan(false);
+            }}
+            onScan={newValue => {
+              if (newValue) {
+                console.log("SCAN VALUE", newValue);
+                let possibleNewValue = newValue;
+                if (possibleNewValue.indexOf("/") >= 0) {
+                  possibleNewValue = possibleNewValue.substr(possibleNewValue.lastIndexOf("0x"));
+                  console.log("CLEANED VALUE", possibleNewValue);
+                }
+                setScan(false);
+                updateAddress(possibleNewValue);
+              }
+            }}
+            style={{ width: "100%" }}
+          />
+        </div>
+      ) : (
+        ""
+      )}
       <Input
         id="0xAddress" // name it something other than address for auto fill doxxing
         name="0xAddress" // name it something other than address for auto fill doxxing
@@ -126,8 +114,20 @@ export default function AddressInput(props) {
         autoFocus={props.autoFocus}
         placeholder={props.placeholder ? props.placeholder : "address"}
         prefix={<Blockie address={currentValue} size={8} scale={3} />}
-        value={ens || currentValue}
-        addonAfter={scannerButton}
+        value={ethers.utils.isAddress(currentValue) && !isENS(currentValue) && isENS(ens) ? ens : currentValue}
+        addonAfter={
+          <div
+            style={{ marginTop: 4, cursor: "pointer" }}
+            onClick={() => {
+              setScan(!scan);
+            }}
+          >
+            <Badge count={<CameraOutlined style={{ fontSize: 9 }} />}>
+              <QrcodeOutlined style={{ fontSize: 18 }} />
+            </Badge>{" "}
+            Scan
+          </div>
+        }
         onChange={e => {
           updateAddress(e.target.value);
         }}
