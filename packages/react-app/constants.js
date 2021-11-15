f42444c8bc137ef8a2025a8d6532b6a0d9fae096
Bliss Da Sailor <meremezeukah@gmail.com>
2021-11-14 19:02:30 -0600

    alchemy key

--- packages/react-app/constants.js
+++ packages/react-app/constants.js
@@ -7,6 +7,8 @@ export const ETHERSCAN_KEY = "PSW8C433Q667DVEX5BCRMGNAH9FSGFZ7Q8";
 // BLOCKNATIVE ID FOR Notify.js:
 export const BLOCKNATIVE_DAPPID = "0b58206a-f3c0-4701-a62f-73c7243e8c77";
 
+export const ALCHEMY_KEY = "oKxs-03sij-U_N0iOlrSsZFr29-IqbuF";
+
 export const NETWORKS = {
   localhost: {
     name: "localhost",
