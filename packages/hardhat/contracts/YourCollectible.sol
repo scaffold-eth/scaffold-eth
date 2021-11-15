3c4b87ebecbb161051ce9500e3a68683907c5d54
Damian <damianmarti@gmail.com>
2021-11-15 14:53:04 -0300

    Add smile to Loogies!

--- packages/hardhat/contracts/YourCollectible.sol
+++ packages/hardhat/contracts/YourCollectible.sol
@@ -21,12 +21,13 @@ contract YourCollectible is ERC721, Ownable {
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
 
-  constructor() public ERC721("Loogies", "LOOG") {
-    // RELEASE THE LOOGIES!
+  constructor() public ERC721("OptimisticLoogies", "OPLOOG") {
+    // RELEASE THE OPTIMISTIC LOOGIES!
   }
 
   mapping (uint256 => bytes3) public color;
   mapping (uint256 => uint256) public chubbiness;
+  mapping (uint256 => uint256) public mouthLength;
 
   uint256 mintDeadline = block.timestamp + 24 hours;
 
@@ -40,9 +41,11 @@ contract YourCollectible is ERC721, Ownable {
       uint256 id = _tokenIds.current();
       _mint(msg.sender, id);
 
-      bytes32 predictableRandom = keccak256(abi.encodePacked( blockhash(block.number-1), msg.sender, address(this) ));
+      bytes32 predictableRandom = keccak256(abi.encodePacked( id, blockhash(block.number-1), msg.sender, address(this) ));
       color[id] = bytes2(predictableRandom[0]) | ( bytes2(predictableRandom[1]) >> 8 ) | ( bytes3(predictableRandom[2]) >> 16 );
       chubbiness[id] = 35+((55*uint256(uint8(predictableRandom[3])))/255);
+      // small chubiness loogies have small mouth
+      mouthLength[id] = 180+((uint256(chubbiness[id]/4)*uint256(uint8(predictableRandom[4])))/255);
 
       return id;
   }
@@ -50,7 +53,7 @@ contract YourCollectible is ERC721, Ownable {
   function tokenURI(uint256 id) public view override returns (string memory) {
       require(_exists(id), "not exist");
       string memory name = string(abi.encodePacked('Loogie #',id.toString()));
-      string memory description = string(abi.encodePacked('This Loogie is the color #',color[id].toColor(),' with a chubbiness of ',uint2str(chubbiness[id]),'!!!'));
+      string memory description = string(abi.encodePacked('This Loogie is the color #',color[id].toColor(),' with a chubbiness of ',uint2str(chubbiness[id]),' and mouth length of ',uint2str(mouthLength[id]),'!!!'));
       string memory image = Base64.encode(bytes(generateSVGofTokenById(id)));
 
       return
@@ -70,6 +73,8 @@ contract YourCollectible is ERC721, Ownable {
                               color[id].toColor(),
                               '"},{"trait_type": "chubbiness", "value": ',
                               uint2str(chubbiness[id]),
+                              '},{"trait_type": "mouthLength", "value": ',
+                              uint2str(mouthLength[id]),
                               '}], "owner":"',
                               (uint160(ownerOf(id))).toHexString(20),
                               '", "image": "',
@@ -96,23 +101,27 @@ contract YourCollectible is ERC721, Ownable {
 
   // Visibility is `public` to enable it being called by other contracts for composition.
   function renderTokenById(uint256 id) public view returns (string memory) {
+    // the translate function for the mouth is based on the curve y = 810/11 - 9x/11
     string memory render = string(abi.encodePacked(
       '<g id="eye1">',
-          '<ellipse stroke-width="3" ry="29.5" rx="29.5" id="svg_1" cy="154.5" cx="181.5" stroke="#000" fill="#fff"/>',
-          '<ellipse ry="3.5" rx="2.5" id="svg_3" cy="154.5" cx="173.5" stroke-width="3" stroke="#000" fill="#000000"/>',
-        '</g>',
-        '<g id="head">',
-          '<ellipse fill="#',
-          color[id].toColor(),
-          '" stroke-width="3" cx="204.5" cy="211.80065" id="svg_5" rx="',
-          chubbiness[id].toString(),
-          '" ry="51.80065" stroke="#000"/>',
-        '</g>',
-        '<g id="eye2">',
-          '<ellipse stroke-width="3" ry="29.5" rx="29.5" id="svg_2" cy="168.5" cx="209.5" stroke="#000" fill="#fff"/>',
-          '<ellipse ry="3.5" rx="3" id="svg_4" cy="169.5" cx="208" stroke-width="3" fill="#000000" stroke="#000"/>',
-        '</g>'
-      ));
+        '<ellipse stroke-width="3" ry="29.5" rx="29.5" id="svg_1" cy="154.5" cx="181.5" stroke="#000" fill="#fff"/>',
+        '<ellipse ry="3.5" rx="2.5" id="svg_3" cy="154.5" cx="173.5" stroke-width="3" stroke="#000" fill="#000000"/>',
+      '</g>',
+      '<g id="head">',
+        '<ellipse fill="#',
+        color[id].toColor(),
+        '" stroke-width="3" cx="204.5" cy="211.80065" id="svg_5" rx="',
+        chubbiness[id].toString(),
+        '" ry="51.80065" stroke="#000"/>',
+      '</g>',
+      '<g id="eye2">',
+        '<ellipse stroke-width="3" ry="29.5" rx="29.5" id="svg_2" cy="168.5" cx="209.5" stroke="#000" fill="#fff"/>',
+        '<ellipse ry="3.5" rx="3" id="svg_4" cy="169.5" cx="208" stroke-width="3" fill="#000000" stroke="#000"/>',
+      '</g>'
+      '<g class="mouth" transform="translate(',uint256((810-9*chubbiness[id])/11).toString(),',0)">',
+        '<path d="M 130 240 Q 165 250 ',mouthLength[id].toString(),' 235" stroke="black" stroke-width="3" fill="transparent"/>',
+      '</g>'
+    ));
 
     return render;
   }
