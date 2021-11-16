05616778b23fe0b59a7b4847ecaabd68658a4ae9
azf20 <azfuller20@gmail.com>
2021-11-16 00:48:12 +0000

    buidlguidlsubgraph first pass

--- packages/subgraph/src/mapping.ts
+++ packages/subgraph/src/mapping.ts
@@ -1,34 +1,90 @@
-import { BigInt, Address } from "@graphprotocol/graph-ts"
+import { BigInt, Address, dataSource } from "@graphprotocol/graph-ts";
 import {
-  YourContract,
-  SetPurpose
-} from "../generated/YourContract/YourContract"
-import { Purpose, Sender } from "../generated/schema"
+  Deposit as DepositEvent,
+  Withdraw as WithdrawalEvent,
+  SimpleStream,
+} from "../generated/Stream/SimpleStream";
+import {
+  Stream,
+  Builder,
+  Funder,
+  Deposit,
+  Withdrawal,
+} from "../generated/schema";
 
-export function handleSetPurpose(event: SetPurpose): void {
+export function handleDeposit(event: DepositEvent): void {
+  let streamAddress = dataSource.address();
 
-  let senderString = event.params.sender.toHexString()
+  let stream = Stream.load(streamAddress.toHexString());
 
-  let sender = Sender.load(senderString)
+  if (stream == null) {
+    stream = new Stream(streamAddress.toHexString());
 
-  if (sender == null) {
-    sender = new Sender(senderString)
-    sender.address = event.params.sender
-    sender.createdAt = event.block.timestamp
-    sender.purposeCount = BigInt.fromI32(1)
+    let contract = SimpleStream.bind(streamAddress);
+    let builderAddress = contract.toAddress();
+
+    stream.builder = builderAddress.toHexString();
+    stream.createdAt = event.block.timestamp;
+    stream.totalDeposited = event.params.amount;
+    stream.totalWithdrawn = BigInt.fromI32(0);
+
+    let builder = Builder.load(builderAddress.toHexString());
+
+    if (builder == null) {
+      builder = new Builder(builderAddress.toHexString());
+      builder.totalWithdrawn = BigInt.fromI32(0);
+      builder.save();
+    }
+  } else {
+    stream.totalDeposited = stream.totalDeposited.plus(event.params.amount);
   }
-  else {
-    sender.purposeCount = sender.purposeCount.plus(BigInt.fromI32(1))
+
+  let funder = Funder.load(event.params.from.toHexString());
+  if (funder == null) {
+    funder = new Funder(event.params.from.toHexString());
+    funder.totalDeposited = event.params.amount;
+    funder.createdAt = event.block.timestamp;
+  } else {
+    funder.totalDeposited = funder.totalDeposited.plus(event.params.amount);
   }
 
-  let purpose = new Purpose(event.transaction.hash.toHex() + "-" + event.logIndex.toString())
+  let deposit = new Deposit(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  );
 
-  purpose.purpose = event.params.purpose
-  purpose.sender = senderString
-  purpose.createdAt = event.block.timestamp
-  purpose.transactionHash = event.transaction.hash.toHex()
+  deposit.stream = streamAddress.toHexString();
+  deposit.amount = event.params.amount;
+  deposit.funder = event.params.from.toHexString();
+  deposit.createdAt = event.block.timestamp;
+  deposit.reason = event.params.reason;
+  deposit.transactionHash = event.transaction.hash.toHex();
 
-  purpose.save()
-  sender.save()
+  stream.save();
+  funder.save();
+  deposit.save();
+}
 
+export function handleWithdrawal(event: WithdrawalEvent): void {
+  let streamAddress = dataSource.address();
+
+  let builder = Builder.load(event.params.to.toHexString());
+  if (builder !== null) {
+    builder.totalWithdrawn = builder.totalWithdrawn.plus(event.params.amount);
+  }
+
+  let stream = Stream.load(streamAddress.toHexString());
+  if (stream !== null) {
+    stream.totalWithdrawn = stream.totalWithdrawn.plus(event.params.amount);
+  }
+
+  let withdrawal = new Withdrawal(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  );
+
+  withdrawal.stream = streamAddress.toHexString();
+  withdrawal.amount = event.params.amount;
+  withdrawal.builder = event.params.to.toHexString();
+  withdrawal.createdAt = event.block.timestamp;
+  withdrawal.reason = event.params.reason;
+  withdrawal.transactionHash = event.transaction.hash.toHex();
 }
