2dc20f6f0a9dac81ead4d576eb660596b832c0d0
Bliss Da Sailor <meremezeukah@gmail.com>
2021-11-18 14:23:25 -0600

    add Home view

--- packages/react-app/src/views/index.js
+++ packages/react-app/src/views/index.js
@@ -1,3 +1,4 @@
+export { default as Home } from "./Home";
 export { default as ExampleUI } from "./ExampleUI";
 export { default as Hints } from "./Hints";
 export { default as Subgraph } from "./Subgraph";
