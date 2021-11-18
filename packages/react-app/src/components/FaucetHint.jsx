429dc20e7b8270cc2c1fa475337667e25bf53a1f
Bliss Da Sailor <meremezeukah@gmail.com>
2021-11-18 14:22:56 -0600

    clean up: faucetHint & network display components

---
+++ packages/react-app/src/components/FaucetHint.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { useBalance, useGasPrice } from "eth-hooks";
+import { ethers } from "ethers";
+import { Button } from "antd";
+
+import { Transactor } from "../helpers";
+
+function FaucetHint({ localProvider, targetNetwork, address }) {
+  const [faucetClicked, setFaucetClicked] = useState(false);
+
+  // fetch local balance
+  const yourLocalBalance = useBalance(localProvider, address);
+
+  // get gas Price from network
+  const gasPrice = useGasPrice(targetNetwork, "fast");
+
+  // Faucet Tx can be used to send funds from the faucet
+  const faucetTx = Transactor(localProvider, gasPrice);
+
+  let faucetHint = "";
+
+  if (
+    !faucetClicked &&
+    localProvider &&
+    localProvider._network &&
+    localProvider._network.chainId === 31337 &&
+    yourLocalBalance &&
+    ethers.utils.formatEther(yourLocalBalance) <= 0
+  ) {
+    faucetHint = (
+      <div style={{ padding: 16 }}>
+        <Button
+          type="primary"
+          onClick={() => {
+            faucetTx({
+              to: address,
+              value: ethers.utils.parseEther("0.01"),
+            });
+            setFaucetClicked(true);
+          }}
+        >
+          💰 Grab funds from the faucet ⛽️
+        </Button>
+      </div>
+    );
+  }
+
+  return faucetHint;
+}
+
+export default FaucetHint;
