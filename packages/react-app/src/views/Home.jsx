5988086f9632abbe2a11996a6a2973cf6a9dbd36
Bliss Da Sailor <meremezeukah@gmail.com>
2021-11-23 05:42:39 -0600

    better doc for Home component

--- packages/react-app/src/views/Home.jsx
+++ packages/react-app/src/views/Home.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useContractReader } from "eth-hooks";
+import { ethers } from "ethers";
 
-function Home(params) {
+/**
+ * web3 props can be passed from '../App.jsx' into your local view component for use
+ * @param {*} yourLocalBalance balance on current network
+ * @param {*} readContracts contracts from current chain already pre-loaded using ethers contract module. More here https://docs.ethers.io/v5/api/contract/contract/
+ * @returns react component
+ */
+function Home({ yourLocalBalance, readContracts }) {
+  // you can also use hooks locally in your component of choice
+  // in this case, let's keep track of 'purpose' variable from our contract
+  const purpose = useContractReader(readContracts, "YourContract", "purpose");
+
   return (
     <div>
       <div style={{ margin: 32 }}>
@@ -15,8 +27,37 @@ function Home(params) {
         </span>
       </div>
       <div style={{ margin: 32 }}>
+        <span style={{ marginRight: 8 }}>🤓</span>
+        The "purpose" variable from our contract is{" "}
+        <span
+          className="highlight"
+          style={{ marginLeft: 4, /* backgroundColor: "#f9f9f9", */ padding: 4, borderRadius: 4, fontWeight: "bolder" }}
+        >
+          {purpose}
+        </span>
+      </div>
+      <div style={{ margin: 32 }}>
+        <span style={{ marginRight: 8 }}>🤖</span>
+        An example prop of your balance{" "}
+        <span style={{ fontWeight: "bold", color: "green" }}>({ethers.utils.formatEther(yourLocalBalance)})</span> was
+        passed into the
+        <span
+          className="highlight"
+          style={{ marginLeft: 4, /* backgroundColor: "#f9f9f9", */ padding: 4, borderRadius: 4, fontWeight: "bolder" }}
+        >
+          Home.jsx
+        </span>{" "}
+        component from
+        <span
+          className="highlight"
+          style={{ marginLeft: 4, /* backgroundColor: "#f9f9f9", */ padding: 4, borderRadius: 4, fontWeight: "bolder" }}
+        >
+          App.jsx
+        </span>
+      </div>
+      <div style={{ margin: 32 }}>
         <span style={{ marginRight: 8 }}>💭</span>
-        OR Check out <Link to="/hints">Hints Tab</Link>
+        Check out the <Link to="/hints">"Hints"</Link> tab for more tips.
       </div>
     </div>
   );
