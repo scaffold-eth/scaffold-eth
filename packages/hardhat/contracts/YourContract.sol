cdbca031d4db69038eab3e1105c3583221db223f
Marius Manole <mariusmanole@192.168.1.153>
2021-11-26 17:04:33 +0000

    equity v1

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -1,23 +1,57 @@
 pragma solidity >=0.8.0 <0.9.0;
-//SPDX-License-Identifier: MIT
 
-import "hardhat/console.sol";
-// import "@openzeppelin/contracts/access/Ownable.sol"; 
-// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract YourContract {
+contract YourContract is Ownable {
+    uint256 mappingId;
+    // mapping of Deed ID to equity holders to equity shares
+    mapping(uint256 => mapping(address => uint256)) equityMap;
+    address deedOwner;
+    
+    // address of rent payment contract
+    address rentPaymentContract;
+    
+    constructor() {
+      mappingId = 0;
+      // TODO Replace with contract facilitating rent payment
+      rentPaymentContract = 0x7c13f6F69D64119FfDEDcB04fFF559362942103c;
 
-  // event SetPurpose(address sender, string purpose);
+      // TODO add functions to set deed NFT token address
+      // TODO add function to set rent payment contract address
+    }
 
-  string public purpose = "Building Unstoppable Apps!!!";
+    function showEquity(address _addr, uint256 _deedId) public view returns (uint256) {
+      return equityMap[_deedId][_addr];
+    }
 
-  constructor() {
-    // what should we do on deploy?
-  }
+    function mintEquity(uint256 _deedId, uint256 _mortgageTermYears) public {
+      // require that the user holds the erc721 deed token with token id = _deedId
+      // check out OwnerOf or isOwnerOf from erc721 contract 
+      deedOwner = msg.sender;
+      equityMap[_deedId][msg.sender] = _mortgageTermYears * 20;
+    }
 
-  function setPurpose(string memory newPurpose) public {
-      purpose = newPurpose;
-      console.log(msg.sender,"set purpose to",purpose);
-      // emit SetPurpose(msg.sender, purpose);
-  }
+    function rewardRenter(uint256 _deedId, address _addr) public {
+      if (msg.sender == rentPaymentContract) {
+        require(equityMap[_deedId][deedOwner] >= 1);
+        equityMap[_deedId][deedOwner] -= 1;
+        equityMap[_deedId][_addr] += 1;
+      }
+    }
+  
+    function allocateEquity(uint256 _deedId, address _addr, uint256 _equityAmount) public {
+      // require that the user holds the erc721 deed token with token id = _deedId
+      require(equityMap[_deedId][msg.sender] >= _equityAmount);
+      equityMap[_deedId][msg.sender] -= _equityAmount;
+      equityMap[_deedId][_addr] += _equityAmount;
+
+    }
+
+    function buyoutEquityHolders(uint256 _deedId) public {
+      require(msg.sender == deedOwner);
+      // can't buy out people during the vesting period -> will need to check
+      // the block number? 5 10 or 15 years from now
+      // send payment to others in PseudoStableCoin
+      //delete equityMap[_deedId][##### EACH HOLDER #####]; // TODO
+    }
 }
--- packages/hardhat/deploy/00_deploy_your_contract.js
+++ packages/hardhat/deploy/00_deploy_your_contract.js
@@ -12,17 +12,17 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     log: true,
   });
 
-  /*
+
     // Getting a previously deployed contract
     const YourContract = await ethers.getContract("YourContract", deployer);
-    await YourContract.setPurpose("Hello");
   
-    To take ownership of yourContract using the ownable library uncomment next line and add the 
-    address you want to be the owner. 
-    // yourContract.transferOwnership(YOUR_ADDRESS_HERE);
+    //To take ownership of yourContract using the ownable library uncomment next line and add the 
+    //address you want to be the owner. 
+    await YourContract.deployed();
+    YourContract.transferOwnership(0x7c13f6F69D64119FfDEDcB04fFF559362942103c);
 
     //const yourContract = await ethers.getContractAt('YourContract', "0xaAC799eC2d00C013f1F11c37E654e59B0429DF6A") //<-- if you want to instantiate a version of a contract at a specific address!
-  */
+  
 
   /*
   //If you want to send value to an address from the deployer
