8492afc3e77834720d0ec7e4a3807714ad8af021
Bliss Da Sailor <meremezeukah@gmail.com>
2021-11-28 22:34:55 -0600

    ipfs

---
+++ packages/react-app/helpers/ipfs.js
@@ -0,0 +1,20 @@
+const { BufferList } = require("bl");
+// https://www.npmjs.com/package/ipfs-http-client
+const ipfsAPI = require("ipfs-http-client");
+
+export const ipfs = ipfsAPI({ host: "ipfs.infura.io", port: "5001", protocol: "https" });
+
+// helper function to "Get" from IPFS
+// you usually go content.toString() after this...
+export async function getFromIPFS(hashToGet) {
+  for await (const file of ipfs.get(hashToGet)) {
+    console.log(file.path);
+    if (!file.content) continue;
+    const content = new BufferList();
+    for await (const chunk of file.content) {
+      content.append(chunk);
+    }
+    console.log(content);
+    return content;
+  }
+}
