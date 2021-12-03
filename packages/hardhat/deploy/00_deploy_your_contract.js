7041460740f3019d3e6f9c811d6d8f22d314b483
Dinu Barbu <dinu@MacBook-Pro.local>
2021-12-04 00:07:00 +0200

    wait a bit before verifying a contract post-deployment

--- packages/hardhat/deploy/00_deploy_your_contract.js
+++ packages/hardhat/deploy/00_deploy_your_contract.js
@@ -4,6 +4,14 @@ const { ethers } = require("hardhat");
 
 const localChainId = "31337";
 
+const sleep = (ms) =>
+  new Promise((r) =>
+    setTimeout(() => {
+      // console.log(`waited for ${(ms / 1000).toFixed(3)} seconds`);
+      r();
+    }, ms)
+  );
+
 module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
@@ -54,6 +62,8 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
   // Verify your contracts with Etherscan
   // You don't want to verify on localhost
   if (chainId !== localChainId) {
+    // wait for etherscan to be ready to verify
+    await sleep(15000);
     await run("verify:verify", {
       address: YourContract.address,
       contract: "contracts/YourContract.sol:YourContract",
