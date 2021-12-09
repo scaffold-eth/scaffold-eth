200ddb0a78e20371f5e118fd81accbe1cf5e2dac
Bliss Da Sailor <meremezeukah@gmail.com>
2021-12-09 11:48:42 -0600

    contract audit and clean up

--- packages/hardhat/contracts/Upgrade3dETHBot.sol
+++ packages/hardhat/contracts/Upgrade3dETHBot.sol
@@ -5,6 +5,7 @@ pragma solidity >=0.8.0 <0.9.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+
 //learn more: https://docs.openzeppelin.com/contracts/3.x/erc721
 
 /*
@@ -20,51 +21,43 @@ Contract by @Blind_nabler and @ghostffcode
 Built with scaffold-eth!
 */
 
-contract UpgradedEthBot is ERC721  {
+contract UpgradedEthBot is ERC721 {
+    IERC721 public ogNFT;
 
-  address public constant ogNFT = 0x1e4749d61990CA7F1aAf229DE91075ddC6cAf493;
-  mapping(uint256 => bool) hasMinted;
-  string constant URI = 'https://bonez.mypinata.cloud/ipfs/QmYXZgrTBfNHszRiV8s8cp2RmmAFetN4Tyf49MfcU2nVSp';
+    constructor(IERC721 _oldContract) ERC721("3dEthBot", "3dth") {
+        ogNFT = _oldContract;
+    }
 
-  constructor() public ERC721("3dEthBot", "3dth") {
-  }
+    function _baseURI() internal view virtual override returns (string memory) {
+        return
+            "https://bonez.mypinata.cloud/ipfs/QmYXZgrTBfNHszRiV8s8cp2RmmAFetN4Tyf49MfcU2nVSp/";
+    }
 
-  function claim(uint256 _tokenId) public returns (uint256) {
-      require(IERC721(ogNFT).ownerOf(_tokenId) == msg.sender, 'msg.sender not owner of this NFT');
-      require(IERC721(ogNFT).ownerOf(_tokenId) != address(0), 'invalid tokenId');
-      require(hasMinted[_tokenId] == false, 'nft already minted for this id!');
-      require(_tokenId <= 200, 'Only EthBots 1-200 are eligible for minting!');
-      require(IERC721(ogNFT).getApproved(_tokenId) == address(this), 'Must approve before minting');
-      IERC721(ogNFT).transferFrom(msg.sender, address(this), _tokenId);
-      hasMinted[_tokenId] = true;
-      _mint(msg.sender, _tokenId);
-  }
+    function claim(uint256 _tokenId) public returns (uint256) {
+        require(
+            ogNFT.ownerOf(_tokenId) == msg.sender,
+            "msg.sender not owner of this NFT"
+        );
+        require(
+            _tokenId <= 200,
+            "Only EthBots 1-200 are eligible for minting!"
+        );
+        require(
+            ogNFT.isApprovedForAll(msg.sender, address(this)),
+            "Must approve before minting"
+        );
+        ogNFT.transferFrom(msg.sender, address(this), _tokenId);
+        _mint(msg.sender, _tokenId);
 
-  function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
-          if (_i == 0) {
-              return "0";
-          }
-          uint j = _i;
-          uint len;
-          while (j != 0) {
-              len++;
-              j /= 10;
-          }
-          bytes memory bstr = new bytes(len);
-          uint k = len;
-          while (_i != 0) {
-              k = k-1;
-              uint8 temp = (48 + uint8(_i - _i / 10 * 10));
-              bytes1 b1 = bytes1(temp);
-              bstr[k] = b1;
-              _i /= 10;
-          }
-          return string(bstr);
-      }
+        return _tokenId;
+    }
 
-
-  function tokenURI(uint256 _tokenId) public view override returns (string memory) {
-        return string(abi.encodePacked(URI,'/',uint2str(_tokenId),'.json'));
-}
-
+    function tokenURI(uint256 _tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        return string(abi.encodePacked(super.tokenURI(_tokenId), ".json"));
+    }
 }
