41d26fcfbb9d4e6c4946176af64d7fd432ffec3a
DeveloperMarwan <developermarwan@gmail.com>
2021-12-25 16:07:36 -0500

    Update Header.jsx

--- packages/react-app/src/components/Header.jsx
+++ packages/react-app/src/components/Header.jsx
@@ -5,11 +5,19 @@ import React from "react";
 
 export default function Header() {
   return (
-    <a href="https://github.com/austintgriffith/scaffold-eth" target="_blank" rel="noopener noreferrer">
+    <a
+      href="https://github.com/scaffold-eth/scaffold-eth-examples/tree/abi-uploader"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
       <PageHeader
-        title="🏗 scaffold-eth"
-        subTitle="forkable Ethereum dev stack focused on fast product iteration"
+        title="ABI Ninja"
+        subTitle="Interact with Ethereum contracts using their address and ABI"
         style={{ cursor: "pointer" }}
+        className="site-page-header"
+        avatar={{
+          src: "https://user-images.githubusercontent.com/17074344/143511127-9e24b576-d1e2-42e0-b3c4-23e4a594a561.png",
+        }}
       />
     </a>
   );
