66cafefa0f4104157f3a9b493be7c70fa23b3ba3
Jason Romero <codenamejason@outlook.com>
2022-01-05 14:28:34 -0500

    remove verify add wait confirms. add verify script

--- packages/hardhat/deploy/00_deploy_your_contract.js
+++ packages/hardhat/deploy/00_deploy_your_contract.js
@@ -4,13 +4,13 @@ const { ethers } = require("hardhat");
 
 const localChainId = "31337";
 
-const sleep = (ms) =>
-  new Promise((r) =>
-    setTimeout(() => {
-      // console.log(`waited for ${(ms / 1000).toFixed(3)} seconds`);
-      r();
-    }, ms)
-  );
+// const sleep = (ms) =>
+//   new Promise((r) =>
+//     setTimeout(() => {
+//       console.log(`waited for ${(ms / 1000).toFixed(3)} seconds`);
+//       r();
+//     }, ms)
+//   );
 
 module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
   const { deploy } = deployments;
@@ -22,6 +22,7 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
     from: deployer,
     // args: [ "Hello", ethers.utils.parseEther("1.5") ],
     log: true,
+    waitConfirmations: 5,
   });
 
   // Getting a previously deployed contract
@@ -59,16 +60,20 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
   });
   */
 
-  // Verify your contracts with Etherscan
+  // Verify from the command line by running `yarn verify`
+
+  // You can also Verify your contracts with Etherscan here...
   // You don't want to verify on localhost
-  if (chainId !== localChainId) {
-    // wait for etherscan to be ready to verify
-    await sleep(15000);
-    await run("verify:verify", {
-      address: YourContract.address,
-      contract: "contracts/YourContract.sol:YourContract",
-      contractArguments: [],
-    });
-  }
+  // try {
+  //   if (chainId !== localChainId) {
+  //     await run("verify:verify", {
+  //       address: YourContract.address,
+  //       contract: "contracts/YourContract.sol:YourContract",
+  //       contractArguments: [],
+  //     });
+  //   }
+  // } catch (error) {
+  //   console.error(error);
+  // }
 };
 module.exports.tags = ["YourContract"];
