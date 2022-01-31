5f8bd307d1107aa381b833387409026f420387ba
viraj124 <virajm72@gmail.com>
2022-01-31 23:18:11 +0530

    refactor: update price fetching logic for retroPGF token

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -298,7 +298,7 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: "0.8.4",
+        version: "0.8.7",
         settings: {
           optimizer: {
             enabled: true,
