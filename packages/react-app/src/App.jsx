4e2ccca9dc058a21254d69dd39b082ccdaf669ea
Jaxcoder <codenamejason@outlook.com>
2022-02-02 21:36:03 -0500

    add the css ref back

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -11,7 +11,7 @@ import {
 import { useExchangeEthPrice } from "eth-hooks/dapps/dex";
 import React, { useCallback, useEffect, useState } from "react";
 import { Link, Route, Switch, useLocation } from "react-router-dom";
-// import "./App.css";
+import "./App.css";
 import {
   Account,
   Contract,
