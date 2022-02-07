5d551cefbae1f50f9c2ce009984cebbce5b8066e
Steve Pham <33653442+umphams@users.noreply.github.com>
2022-02-07 00:02:54 -0500

    Move files and uncomment section in deployment script for others to help troubleshoot issue #7

--- packages/hardhat/deploy/00_deploy_your_contract.js
+++ packages/hardhat/deploy/00_deploy_your_contract.js
@@ -42,16 +42,16 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
     "" + 10 * 10 ** 18
   );
 
-  // // uncomment to init DEX on deploy:
-  // console.log(
-  //   "Approving DEX (" + dex.address + ") to take Balloons from main account..."
-  // );
-  // // If you are going to the testnet make sure your deployer account has enough ETH
-  // await balloons.approve(dex.address, ethers.utils.parseEther("100"));
-  // console.log("INIT exchange...");
-  // await dex.init(ethers.utils.parseEther("5"), {
-  //   value: ethers.utils.parseEther("5"),
-  //   gasLimit: 200000,
-  // });
+  // uncomment to init DEX on deploy:
+  console.log(
+    "Approving DEX (" + dex.address + ") to take Balloons from main account..."
+  );
+  // If you are going to the testnet make sure your deployer account has enough ETH
+  await balloons.approve(dex.address, ethers.utils.parseEther("100"));
+  console.log("INIT exchange...");
+  await dex.init(ethers.utils.parseEther("5"), {
+    value: ethers.utils.parseEther("5"),
+    gasLimit: 200000,
+  });
 };
 module.exports.tags = ["Balloons", "DEX"];
