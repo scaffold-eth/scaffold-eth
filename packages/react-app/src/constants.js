dacf16d4ba94294bac099967074848cdf5553ccb
metaverde <jenniferleesilves@gmail.com>
2022-02-11 17:51:32 -0800

    Got the nft-store-bonding-curve into pragma 8.

--- packages/react-app/src/constants.js
+++ packages/react-app/src/constants.js
@@ -1,5 +1,5 @@
 // MY INFURA_ID, SWAP IN YOURS FROM https://infura.io/dashboard/ethereum
-export const INFURA_ID = "460f40a260564ac4a4f4b3fffb032dad";
+export const INFURA_ID = "2511f70801c0408dac8de8a403efbe3a";
 
 //MY ETHERSCAN_ID, SWAP IN YOURS FROM https://etherscan.io/myapikey
 export const ETHERSCAN_KEY = "PSW8C433Q667DVEX5BCRMGNAH9FSGFZ7Q8";
