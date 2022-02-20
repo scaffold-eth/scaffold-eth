99b45227862ea9c638045d6c1ff003e4e5144827
Austin Griffith <you@example.com>
2022-02-20 16:47:08 -0700

    oh yeah make me the owher

--- packages/hardhat/deploy/00_deploy_your_contract.js
+++ packages/hardhat/deploy/00_deploy_your_contract.js
@@ -26,15 +26,13 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
   });
 
   // Getting a previously deployed contract
-  const YourContract = await ethers.getContract("YourContract", deployer);
-  /*  await YourContract.setPurpose("Hello");
-  
-    To take ownership of yourContract using the ownable library uncomment next line and add the 
-    address you want to be the owner. 
-    // await yourContract.transferOwnership(YOUR_ADDRESS_HERE);
+  const yourContract = await ethers.getContract("YourContract", deployer);
+
+
+    await yourContract.transferOwnership("0x34aA3F359A9D614239015126635CE7732c18fDF3");
 
     //const yourContract = await ethers.getContractAt('YourContract', "0xaAC799eC2d00C013f1F11c37E654e59B0429DF6A") //<-- if you want to instantiate a version of a contract at a specific address!
-  */
+
 
   /*
   //If you want to send value to an address from the deployer
