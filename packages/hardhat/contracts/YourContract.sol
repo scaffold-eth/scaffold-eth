29f283d8063ab15bf0b146cae7a844542f7404cb
Austin Griffith <austin@ethereum.org>
2022-02-20 15:39:01 -0700

    readme

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -2,26 +2,19 @@ pragma solidity >=0.8.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 
 import "hardhat/console.sol";
-// import "@openzeppelin/contracts/access/Ownable.sol"; 
+// import "@openzeppelin/contracts/access/Ownable.sol";
 // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 
 contract YourContract {
 
-  event SetPurpose(address sender, string purpose);
+  event Register(address origin, address yourContract);
 
-  string public purpose = "Building Unstoppable Apps!!!";
+  mapping(address => address) public yourContract;
 
-  constructor() payable {
-    // what should we do on deploy?
+  function register() public {
+    require(tx.origin!=msg.sender, "NOT A CONTRACT");
+    yourContract[tx.origin] = msg.sender;
+    emit Register(tx.origin,msg.sender);
   }
 
-  function setPurpose(string memory newPurpose) public {
-      purpose = newPurpose;
-      console.log(msg.sender,"set purpose to",purpose);
-      emit SetPurpose(msg.sender, purpose);
-  }
-
-  // to support receiving ETH by default
-  receive() external payable {}
-  fallback() external payable {}
 }
