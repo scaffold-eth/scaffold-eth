f37f0a7b6fd70888dc79af266b1f832960fa7e41
Austin Griffith <you@example.com>
2022-02-20 16:41:27 -0700

    moves?

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -2,19 +2,38 @@ pragma solidity >=0.8.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 
 import "hardhat/console.sol";
-// import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 
-contract YourContract {
+contract YourContract is Ownable{
 
   event Register(address origin, address yourContract);
+  event Move(address origin, string move);
 
   mapping(address => address) public yourContract;
 
+  bool public gameOn;
+
   function register() public {
+    require(!gameOn, "TOO LATE");
     require(tx.origin!=msg.sender, "NOT A CONTRACT");
     yourContract[tx.origin] = msg.sender;
     emit Register(tx.origin,msg.sender);
+  }
+
+  function start() public {
+    gameOn=true;
+  }
+
+  mapping(address => string) public moves;
+  
+
+  function move(string calldata yourMove) public {
+    require(gameOn, "NOT YET");
+    require(tx.origin!=msg.sender, "NOT A CONTRACT");
+    require(msg.sender==yourContract[tx.origin], "STOP LARPING");
+    moves[tx.origin]=yourMove;
+    emit Move(tx.origin, yourMove);
   }
 
 }
