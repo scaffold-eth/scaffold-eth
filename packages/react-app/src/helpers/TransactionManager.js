417fcd4f46a201c99cc9dcfae489c78c114e9898
tamas.molnar <tamas.molnar@liferay.com>
2022-03-19 21:53:50 +0100

    Create TransactionManager It can be used to store transactionResponses in localStorage and speed them up if necessary. Speed up will use the params from the pending transactionResponses and modify the gasPrice, resulting in a new tx hash. Fires localStorageChanged event.

---
+++ packages/react-app/src/helpers/TransactionManager.js
@@ -0,0 +1,178 @@
+const { BigNumber, ethers } = require("ethers");
+
+const STORAGE_KEY = "transactionResponses";
+const LOCAL_STORAGE_CHANGED_EVENT_NAME = "localStorageChanged";
+
+export class TransactionManager {
+	constructor(provider, signer, loggingEnabled) {
+		this.provider = provider;
+		this.signer = signer;
+		this.loggingEnabled = loggingEnabled ? loggingEnabled : false
+	}
+
+	log = (...args) => {
+		if (!this.loggingEnabled) {
+			return;
+		}
+
+  	console.log(...args);
+	}
+
+	getStorageKey() {
+		return STORAGE_KEY;
+	}
+
+	getLocalStorageChangedEventName() {
+		return LOCAL_STORAGE_CHANGED_EVENT_NAME;
+	}
+
+	getTransactionResponses() {
+		let transactionResponsesString = localStorage.getItem(STORAGE_KEY);
+
+		if (transactionResponsesString === null) {
+			return {};
+		}
+
+		return JSON.parse(transactionResponsesString);
+	}
+	setTransactionResponses(transactionResponses) {
+		localStorage.setItem(STORAGE_KEY, JSON.stringify(transactionResponses));
+
+		// StorageEvent doesn't work in the same window
+		window.dispatchEvent(new CustomEvent(LOCAL_STORAGE_CHANGED_EVENT_NAME));
+	}
+
+	getTransactionResponse(nonce) {
+		let transactionResponses = this.getTransactionResponses();
+
+		return transactionResponses[nonce];
+	}
+	setTransactionResponse(transactionResponse) {
+		let transactionResponses = this.getTransactionResponses();
+
+		transactionResponses[transactionResponse.nonce] = transactionResponse;
+
+		this.setTransactionResponses(transactionResponses);
+	}
+	removeTransactionResponse(transactionResponse) {
+		let transactionResponses = this.getTransactionResponses();
+
+		delete transactionResponses[transactionResponse.nonce];
+
+		this.setTransactionResponses(transactionResponses);
+	}
+
+	getTransactionResponsesArray() {
+		let transactionResponses = this.getTransactionResponses();
+
+		if (Object.keys(transactionResponses).length === 0) {
+			return [];
+		}
+
+		let transactionResponsesArray = [];
+
+		let keysArray = Object.keys(transactionResponses);
+
+		keysArray.forEach(key => {
+			transactionResponsesArray.push(transactionResponses[key]);
+		})
+
+		return transactionResponsesArray;
+	}
+
+	async getConfirmations(transactionResponse) {
+		let newTransactionResponse = await this.provider.getTransaction(transactionResponse.hash);
+
+		if (!newTransactionResponse) {
+			this.log("getConfirmations newTransactionResponse is undefined", transactionResponse);
+
+			// I'm not sure what is this case, but it happened
+			// Maybe the transaction was just confirmed when SpeedUpTx button was hit, resulting in the previous response to be confirmed, 
+			// and the new sped up hash to be invalid
+			// Also, sometimes the provider is faulty and returns null
+			let nonce = await this.provider.getTransactionCount(transactionResponse.from);
+
+			if (transactionResponse.nonce <= (nonce - 1)) {
+				console.log("getConfirmations nonce is already used", transactionResponse);
+				// Transaction with the same nonce was already confirmed
+				this.removeTransactionResponse(transactionResponse);
+
+				return -1;
+			}
+
+			return 0;
+		}
+
+		return newTransactionResponse.confirmations;
+	}	
+
+	async isTransactionPending(transactionResponse) {
+		let confirmations = await this.getConfirmations(transactionResponse);
+
+		return !(confirmations > 0);
+	}
+	
+	speedUpTransaction(nonce, speedUpPercentage) {
+		if (!speedUpPercentage) {
+			speedUpPercentage = 10;
+		}
+
+		let transactionResponse = this.getTransactionResponse(nonce);
+
+		if (!transactionResponse) {
+			return;
+		}
+
+		let transactionParams = this.getTransactionParams(transactionResponse);
+
+		// Legacy txs
+		if (transactionParams.gasPrice) {
+			transactionParams.gasPrice = this.getUpdatedGasPrice(transactionParams.gasPrice, speedUpPercentage);
+		}
+		// EIP1559
+		else {
+			transactionParams.maxPriorityFeePerGas = this.getUpdatedGasPrice(transactionParams.maxPriorityFeePerGas, speedUpPercentage);
+			
+			// This shouldn't be necessary, but without it polygon fails way too many times with "replacement transaction underpriced"
+			transactionParams.maxFeePerGas = this.getUpdatedGasPrice(transactionParams.maxFeePerGas, speedUpPercentage);
+		}
+
+		this.log("transactionParams", transactionParams);
+
+		return this.signer.sendTransaction(transactionParams);
+	}
+
+	getTransactionParams(transactionResponse) {
+		if (!transactionResponse) {
+			return {};
+		}
+
+		let transactionParams = {};
+
+		["type", "chainId", "nonce", "maxPriorityFeePerGas", "maxFeePerGas", "gasPrice", "gasLimit", "to", "value", "data"].forEach(param => {
+			this.addTransactionParamIfExists(transactionParams, param, transactionResponse[param]);
+		})
+		
+		return transactionParams;
+	}
+	getUpdatedGasPrice(gasPrice, speedUpPercentage) {
+		let gasPriceBigNumber = BigNumber.from(gasPrice);
+
+		gasPriceBigNumber = gasPriceBigNumber.mul(speedUpPercentage + 100).div(100);
+
+		return gasPriceBigNumber.toHexString();
+	}
+	addTransactionParamIfExists(transactionParams, param, value) {
+		if ((value == 0) || (value && value != null)) {
+			if (["maxPriorityFeePerGas", "maxFeePerGas", "gasPrice", "gasLimit", "value"].indexOf(param) > -1) {
+				value = BigNumber.from(value).toHexString();
+			}
+
+			transactionParams[param] = value;
+		}
+	}
+}
+
+
+
+
