18dc63401bb4e5ea19599cda75fc4578979c3903
Austin Griffith <you@example.com>
2022-04-12 20:09:39 -0600

    hardcoded stream deploy test

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -23,6 +23,8 @@ import {
   NetworkDisplay,
   FaucetHint,
   NetworkSwitch,
+  AddressInput,
+  Address
 } from "./components";
 import { NETWORKS, ALCHEMY_KEY } from "./constants";
 import externalContracts from "./contracts/external_contracts";
@@ -32,6 +34,7 @@ import { Transactor, Web3ModalSetup } from "./helpers";
 import { Home, ExampleUI, Hints, Subgraph } from "./views";
 import { useStaticJsonRPC } from "./hooks";
 
+import { ContractFactory } from 'ethers';
 const { ethers } = require("ethers");
 /*
     Welcome to 🏗 scaffold-eth !
@@ -53,7 +56,7 @@ const { ethers } = require("ethers");
 */
 
 /// 📡 What chain are your contracts deployed to?
-const initialNetwork = NETWORKS.localhost; // <------- select your target frontend network (localhost, rinkeby, xdai, mainnet)
+const initialNetwork = NETWORKS.mainnet; // <------- select your target frontend network (localhost, rinkeby, xdai, mainnet)
 
 // 😬 Sorry for all the console logging
 const DEBUG = true;
@@ -167,7 +170,7 @@ function App(props) {
   ]);
 
   // keep track of a variable from the contract in the local React state:
-  const purpose = useContractReader(readContracts, "YourContract", "purpose");
+  const purpose = false//useContractReader(readContracts, "YourContract", "purpose");
 
   /*
   const addressFromENS = useResolveName(mainnetProvider, "austingriffith.eth");
@@ -244,6 +247,10 @@ function App(props) {
 
   const faucetAvailable = localProvider && localProvider.connection && targetNetwork.name.indexOf("local") !== -1;
 
+  const [ toAddress, setToAddress ] = useState()
+  const [ result, setResult ] = useState()
+
+
   return (
     <div className="App">
       {/* ✏️ Edit the header and change the title to your project name */}
@@ -256,100 +263,51 @@ function App(props) {
         logoutOfWeb3Modal={logoutOfWeb3Modal}
         USE_NETWORK_SELECTOR={USE_NETWORK_SELECTOR}
       />
-      <Menu style={{ textAlign: "center", marginTop: 40 }} selectedKeys={[location.pathname]} mode="horizontal">
-        <Menu.Item key="/">
-          <Link to="/">App Home</Link>
-        </Menu.Item>
-        <Menu.Item key="/debug">
-          <Link to="/debug">Debug Contracts</Link>
-        </Menu.Item>
-        <Menu.Item key="/hints">
-          <Link to="/hints">Hints</Link>
-        </Menu.Item>
-        <Menu.Item key="/exampleui">
-          <Link to="/exampleui">ExampleUI</Link>
-        </Menu.Item>
-        <Menu.Item key="/mainnetdai">
-          <Link to="/mainnetdai">Mainnet DAI</Link>
-        </Menu.Item>
-        <Menu.Item key="/subgraph">
-          <Link to="/subgraph">Subgraph</Link>
-        </Menu.Item>
-      </Menu>
 
       <Switch>
         <Route exact path="/">
-          {/* pass in any web3 props to this Home component. For example, yourLocalBalance */}
-          <Home yourLocalBalance={yourLocalBalance} readContracts={readContracts} />
-        </Route>
-        <Route exact path="/debug">
-          {/*
-                🎛 this scaffolding is full of commonly used components
-                this <Contract/> component will automatically parse your ABI
-                and give you a form to interact with it locally
-            */}
 
-          <Contract
-            name="YourContract"
-            price={price}
-            signer={userSigner}
-            provider={localProvider}
-            address={address}
-            blockExplorer={blockExplorer}
-            contractConfig={contractConfig}
+        <div style={{width:500, padding:32, margin: "auto"}}>
+          <AddressInput
+            autoFocus
+            ensProvider={mainnetProvider}
+            placeholder="Enter address"
+            value={toAddress}
+            onChange={setToAddress}
           />
-        </Route>
-        <Route path="/hints">
-          <Hints
-            address={address}
-            yourLocalBalance={yourLocalBalance}
-            mainnetProvider={mainnetProvider}
-            price={price}
-          />
-        </Route>
-        <Route path="/exampleui">
-          <ExampleUI
-            address={address}
-            userSigner={userSigner}
-            mainnetProvider={mainnetProvider}
-            localProvider={localProvider}
-            yourLocalBalance={yourLocalBalance}
-            price={price}
-            tx={tx}
-            writeContracts={writeContracts}
-            readContracts={readContracts}
-            purpose={purpose}
-          />
-        </Route>
-        <Route path="/mainnetdai">
-          <Contract
-            name="DAI"
-            customContract={mainnetContracts && mainnetContracts.contracts && mainnetContracts.contracts.DAI}
-            signer={userSigner}
-            provider={mainnetProvider}
-            address={address}
-            blockExplorer="https://etherscan.io/"
-            contractConfig={contractConfig}
-            chainId={1}
-          />
-          {/*
-            <Contract
-              name="UNI"
-              customContract={mainnetContracts && mainnetContracts.contracts && mainnetContracts.contracts.UNI}
-              signer={userSigner}
-              provider={mainnetProvider}
-              address={address}
-              blockExplorer="https://etherscan.io/"
-            />
-            */}
-        </Route>
-        <Route path="/subgraph">
-          <Subgraph
-            subgraphUri={props.subgraphUri}
-            tx={tx}
-            writeContracts={writeContracts}
-            mainnetProvider={mainnetProvider}
-          />
+        </div>
+
+
+          <Button onClick={async ()=>{
+
+            const contractAbi = [{"inputs":[{"internalType":"address payable","name":"_toAddress","type":"address"},{"internalType":"uint256","name":"_cap","type":"uint256"},{"internalType":"uint256","name":"_frequency","type":"uint256"},{"internalType":"bool","name":"_startsFull","type":"bool"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"},{"indexed":false,"internalType":"string","name":"reason","type":"string"}],"name":"Deposit","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"},{"indexed":false,"internalType":"string","name":"reason","type":"string"}],"name":"Withdraw","type":"event"},{"inputs":[],"name":"cap","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"frequency","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"last","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"streamBalance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"reason","type":"string"}],"name":"streamDeposit","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"},{"internalType":"string","name":"reason","type":"string"}],"name":"streamWithdraw","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"toAddress","outputs":[{"internalType":"address payable","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"stateMutability":"payable","type":"receive"}]
+
+            const contractByteCode = "0x608060405234801561001057600080fd5b506040516106c73803806106c783398101604081905261002f9161007d565b600080546001600160a01b0319166001600160a01b03861617905560018390556002829055801561006f5760025461006790426100d7565b600355610074565b426003555b505050506100fa565b60008060008060808587031215610092578384fd5b84516001600160a01b03811681146100a8578485fd5b809450506020850151925060408501519150606085015180151581146100cc578182fd5b939692955090935050565b6000828210156100f557634e487b7160e01b81526011600452602481fd5b500390565b6105be806101096000396000f3fe6080604052600436106100745760003560e01c8063a8397ddc1161004e578063a8397ddc1461010f578063c3ae1e591461012f578063c5a15ec814610144578063ead50da31461015757600080fd5b806319c87f1f14610098578063355274ea146100d557806347799da8146100f957600080fd5b36610093576100916040518060200160405280600081525061016d565b005b600080fd5b3480156100a457600080fd5b506000546100b8906001600160a01b031681565b6040516001600160a01b0390911681526020015b60405180910390f35b3480156100e157600080fd5b506100eb60015481565b6040519081526020016100cc565b34801561010557600080fd5b506100eb60035481565b34801561011b57600080fd5b5061009161012a36600461044e565b6101b3565b34801561013b57600080fd5b506100eb610340565b610091610152366004610413565b61016d565b34801561016357600080fd5b506100eb60025481565b336001600160a01b03167f643e927b32d5bfd08eccd2fcbd97057ad413850f857a2359639114e8e8dd3d7b34836040516101a8929190610493565b60405180910390a250565b6000546001600160a01b031633146102125760405162461bcd60e51b815260206004820152601a60248201527f746869732073747265616d206973206e6f7420666f7220796f7500000000000060448201526064015b60405180910390fd5b600061021c610340565b90508281101561026e5760405162461bcd60e51b815260206004820152601860248201527f6e6f7420656e6f75676820696e207468652073747265616d00000000000000006044820152606401610209565b60006002544261027e9190610545565b90508060035410156102905760038190555b8184600354426102a09190610545565b6102aa9190610526565b6102b49190610506565b6003546102c191906104ee565b60035560405133907f485f1bb6524c663555797e00171a10f341656e59b02d6b557a0a38ba7d5d9751906102f89087908790610493565b60405180910390a2600080546040516001600160a01b039091169186156108fc02918791818181858888f19350505050158015610339573d6000803e3d6000fd5b5050505050565b6000600254600354426103539190610545565b1115610360575060015490565b6002546003546103709042610545565b60015461037d9190610526565b6103879190610506565b905090565b600082601f83011261039c578081fd5b813567ffffffffffffffff808211156103b7576103b7610572565b604051601f8301601f19908116603f011681019082821181831017156103df576103df610572565b816040528381528660208588010111156103f7578485fd5b8360208701602083013792830160200193909352509392505050565b600060208284031215610424578081fd5b813567ffffffffffffffff81111561043a578182fd5b6104468482850161038c565b949350505050565b60008060408385031215610460578081fd5b82359150602083013567ffffffffffffffff81111561047d578182fd5b6104898582860161038c565b9150509250929050565b828152600060206040818401528351806040850152825b818110156104c6578581018301518582016060015282016104aa565b818111156104d75783606083870101525b50601f01601f191692909201606001949350505050565b600082198211156105015761050161055c565b500190565b60008261052157634e487b7160e01b81526012600452602481fd5b500490565b60008160001904831182151516156105405761054061055c565b500290565b6000828210156105575761055761055c565b500390565b634e487b7160e01b600052601160045260246000fd5b634e487b7160e01b600052604160045260246000fdfea2646970667358221220998fd3296a17e6fc1e79b14215371f2ae91a0a88a0ca053c18e4bbb8872216b664736f6c63430008040033"
+
+            const factory = new ContractFactory(contractAbi, contractByteCode, userSigner);
+
+            // If your contract requires constructor args, you can specify them here
+            const contract = await factory.deploy(
+              /* to address */ toAddress,
+              /* cap */ ethers.utils.parseEther("0.5"),//ether
+              /* frequency */2592000, //2592000 // 30days,//seconds //1296000,//15 days
+              /* starts full: */ true,
+            );
+
+            setResult(contract.address)
+            console.log(contract.deployTransaction);
+          }}>
+            DEPLOY
+          </Button>
+
+
+          <div style={{width:500, padding:64, margin: "auto"}}>
+
+            {result && <Address address={result} ensProvider={mainnetProvider} blockExplorer={blockExplorer} />}
+
+          </div>
+
+
         </Route>
       </Switch>
 
