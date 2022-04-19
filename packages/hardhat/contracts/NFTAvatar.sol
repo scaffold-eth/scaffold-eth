26f5b6a2d4230a0291f02bbe6a9100e4c01a3c17
andrejrakic <andrej.rakic@smartcontract.com>
2022-04-19 02:43:50 +0200

    Simplify NFT

--- packages/hardhat/contracts/NFTAvatar.sol
+++ packages/hardhat/contracts/NFTAvatar.sol
@@ -3,31 +3,16 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
-import "@openzeppelin/contracts/utils/Base64.sol";
 
 contract NFTAvatar is ERC721URIStorage {
   using Counters for Counters.Counter;
   Counters.Counter private tokenIds;
-
+  string constant tokenUri = "https://ipfs.io/ipfs/QmUpmeipNceYanSvrbcFDPA2Vw9FoaC2Cm8YB5XFHKpo9v";
 
   struct Character {
       uint256 attackStrength;
       uint256 defenceStrength;
   }
-
-  string[] private emojis = [
-    unicode"😁",
-    unicode"😂",
-    unicode"😍",
-    unicode"😭",
-    unicode"😴",
-    unicode"😎",
-    unicode"🤑",
-    unicode"🥳",
-    unicode"😱",
-    unicode"🙄"
-  ];
 
   address public gameContract;
   mapping(uint => Character) public characters;
@@ -42,16 +27,10 @@ contract NFTAvatar is ERC721URIStorage {
 
         uint256 tokenId = tokenIds.current();
 
-        uint256 defenceStrength = randomNumber % 1000;
-        uint256 attackStrength = defenceStrength + 500;
+        uint256 defenceStrength = randomNumber % 500;
+        uint256 attackStrength = defenceStrength + 250;
 
         characters[tokenId] = Character(attackStrength, defenceStrength);
-
-        uint256 emojiIndex = (randomNumber % emojis.length) + 1;
-        string memory emoji = emojis[emojiIndex];
-        string memory color = pickRandomColor(randomNumber);
-        string memory svg = createOnChainSvg(emoji, color);
-        string memory tokenUri = createTokenUri(emoji, svg);
 
         _safeMint(player, tokenId);
         _setTokenURI(tokenId, tokenUri);
@@ -68,54 +47,4 @@ contract NFTAvatar is ERC721URIStorage {
         tokenId = yourTokens[owner];
         (attackStrength, defenceStrength) = getCharacterByTokenId(tokenId);
     }
-
-  function pickRandomColor(uint256 randomNumber)
-    internal
-    pure
-    returns (string memory)
-  {
-    uint256 r = uint256(keccak256(abi.encode(randomNumber, 1))) % 256;
-    uint256 g = uint256(keccak256(abi.encode(randomNumber, 2))) % 256;
-    uint256 b = uint256(keccak256(abi.encode(randomNumber, 3))) % 256;
-
-    return
-      string(
-        abi.encodePacked(
-          "rgb(",
-          Strings.toString(r),
-          ", ",
-          Strings.toString(g),
-          ", ",
-          Strings.toString(b),
-          ");"
-        )
-      );
-  }
-
-  function createOnChainSvg(string memory emoji, string memory color) internal pure returns(string memory svg) {
-    string memory baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { font-size: 100px; }</style><rect width='100%' height='100%' style='fill:";
-    string memory afterColorSvg = "' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
-
-    svg = string(abi.encodePacked(baseSvg, color, afterColorSvg, emoji, "</text></svg>"));
-  }
-
-  function createTokenUri(string memory emoji, string memory svg) internal pure returns(string memory tokenUri) {
-    string memory json = Base64.encode(
-      bytes(
-        string(
-          abi.encodePacked(
-            '{"name": "',
-            emoji,
-            '", "description": "Random Emoji NFT Collection Powered by Chainlink VRF", "image": "data:image/svg+xml;base64,',
-            Base64.encode(bytes(svg)),
-            '"}'
-          )
-        )
-      )
-    );
-
-    tokenUri = string(
-      abi.encodePacked("data:application/json;base64,", json)
-    );
-  }
 }
--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -264,7 +264,7 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: "0.8.4",
+        version: "0.8.6",
         settings: {
           optimizer: {
             enabled: true,
