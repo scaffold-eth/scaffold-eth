9e1de32e8d0533feb11d20ae6ef1e503cab9f3eb
nullbitx8 <nullbitx8@protonmail.com>
2022-04-21 21:20:25 +0100

    create contracts directory if it does not already exist; fixes this script from blowing up on first run

--- packages/react-app/scripts/create_contracts.js
+++ packages/react-app/scripts/create_contracts.js
@@ -1,5 +1,16 @@
 const fs = require("fs");
 
+// create contracts dir if it does not exist
+if (!fs.existsSync("./src/contracts")) {
+  try {
+    fs.mkdirSync("./src/contracts");
+    console.log("src/contracts directory created.");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// create hardhat_contracts.json file if it does not exist
 if (!fs.existsSync("./src/contracts/hardhat_contracts.json")) {
   try {
     fs.writeFileSync("./src/contracts/hardhat_contracts.json", JSON.stringify({}));
