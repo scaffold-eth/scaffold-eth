2b649f2debf9b21ea38621ea02c46f020a4c0f74
Jake Sucoff <jsucoff@gmail.com>
2022-04-28 16:02:37 -0400

    show balance, staking threshold, event listeners

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -5,10 +5,30 @@ import "./ExampleExternalContract.sol";
 
 contract Staker {
 
+  // this contract holds staked funds
   ExampleExternalContract public exampleExternalContract;
+
+  // shows balances of address participating
+  mapping ( address => uint256 ) public balances;
+
+  // staking threshold
+  uint256 public constant threshold = 1 ether;
+
+
+  // Contract's Events
+  event Stake(address indexed sender, uint256 amount);
 
   constructor(address exampleExternalContractAddress) public {
       exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
+  }
+
+  // stake method that update the user's balance
+  function stake() public payable {
+    // update the user's balance
+    balances[msg.sender] += msg.value;
+
+    // emit the event to notify the blockchain that we have correctly staked funds from the user
+    emit Stake(msg.sender, msg.value);
   }
 
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
@@ -17,7 +37,6 @@ contract Staker {
 
   // After some `deadline` allow anyone to call an `execute()` function
   //  It should either call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
-
 
   // if the `threshold` was not met, allow everyone to call a `withdraw()` function
 
