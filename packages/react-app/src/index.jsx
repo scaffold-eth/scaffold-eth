e27d2159a2cf5c2a3822057591a1287efd7cfebf
Joseph Izang <josephizang@gmail.com>
2022-05-04 15:17:15 +0100

    remove dark theme from index.jsx

--- packages/react-app/src/index.jsx
+++ packages/react-app/src/index.jsx
@@ -1,32 +1,30 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
-import React from "react";
-import { ThemeSwitcherProvider } from "react-css-theme-switcher";
-import { BrowserRouter } from "react-router-dom";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./index.css";
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import React from 'react'
+import { ThemeSwitcherProvider } from 'react-css-theme-switcher'
+import { BrowserRouter } from 'react-router-dom'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './index.css'
 
 const themes = {
   dark: `${process.env.PUBLIC_URL}/dark-theme.css`,
   light: `${process.env.PUBLIC_URL}/dark-theme.css`,
-};
+}
 
-const prevTheme = window.localStorage.getItem("theme");
+const prevTheme = window.localStorage.getItem('theme')
 
-const subgraphUri = "http://localhost:8000/subgraphs/name/scaffold-eth/your-contract";
+const subgraphUri = 'http://localhost:8000/subgraphs/name/scaffold-eth/your-contract'
 
 const client = new ApolloClient({
   uri: subgraphUri,
   cache: new InMemoryCache(),
-});
+})
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <ThemeSwitcherProvider themeMap={themes} defaultTheme={prevTheme || "light"}>
-      <BrowserRouter>
-        <App subgraphUri={subgraphUri} />
-      </BrowserRouter>
-    </ThemeSwitcherProvider>
+    <BrowserRouter>
+      <App subgraphUri={subgraphUri} />
+    </BrowserRouter>
   </ApolloProvider>,
-  document.getElementById("root"),
-);
+  document.getElementById('root'),
+)
