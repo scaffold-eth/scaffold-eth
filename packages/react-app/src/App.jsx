b97367104531a184061d66a8b94dd2288ea4da9d
Joseph Izang <josephizang@gmail.com>
2022-05-04 15:40:59 +0100

    clean up and remove all unused imports and artifacts from index.jsx and App.jsx

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -1,45 +1,10 @@
-import { Button, Col, Menu, Row } from 'antd'
 import 'antd/dist/antd.css'
-import {
-  useBalance,
-  useContractLoader,
-  useContractReader,
-  useGasPrice,
-  useOnBlock,
-  useUserProviderAndSigner,
-} from 'eth-hooks'
-import { useExchangeEthPrice } from 'eth-hooks/dapps/dex'
-import React, { useCallback, useEffect, useState } from 'react'
-import { Link, Route, Switch, useLocation } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
 import './App.css'
-import {
-  Account,
-  Contract,
-  Faucet,
-  GasGauge,
-  Header,
-  Ramp,
-  ThemeSwitch,
-  NetworkDisplay,
-  FaucetHint,
-  NetworkSwitch,
-} from './components'
-import { NETWORKS, ALCHEMY_KEY } from './constants'
-import externalContracts from './contracts/external_contracts'
-// contracts
-// import deployedContracts from "./contracts/hardhat_contracts.json";
-import { Transactor, Web3ModalSetup } from './helpers'
-import { Home, ExampleUI, Hints, Subgraph, BrowseBadges } from './views'
-import { useStaticJsonRPC } from './hooks'
-import ethers from 'ethers'
-// const { ethers } = require('ethers')
+import { BrowseBadges } from './views'
+const { ethers } = require('ethers')
 
 function App(props) {
-  const contractConfig = { deployedContracts: {}, externalContracts: externalContracts || {} }
-
-  // const provider = useStaticJsonRPC(['https://mainnet.optimism.io'])
-  // const mainnet = useStaticJsonRPC(['https://mainnet.infura.io/v3/1b3241e53c8d422aab3c7c0e4101de9c'])
-  // If you want to make 🔐 write transactions to your contracts, use the userSigner:
   const [loaded, setLoaded] = useState(false)
   const [localProvider, setLocalProvider] = useState(null)
   const [mainnet, setMainnet] = useState(null)
--- packages/react-app/src/index.jsx
+++ packages/react-app/src/index.jsx
@@ -1,17 +1,9 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 import React from 'react'
-import { ThemeSwitcherProvider } from 'react-css-theme-switcher'
 import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
-
-const themes = {
-  dark: `${process.env.PUBLIC_URL}/dark-theme.css`,
-  light: `${process.env.PUBLIC_URL}/dark-theme.css`,
-}
-
-const prevTheme = window.localStorage.getItem('theme')
 
 const subgraphUri = 'http://localhost:8000/subgraphs/name/scaffold-eth/your-contract'
 
