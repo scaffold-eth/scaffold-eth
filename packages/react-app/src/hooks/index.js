9562401835e3af3ec25a4b73f32d33b744189a03
Zak G <zakgriffith@gmail.com>
2022-05-13 10:45:20 -0600

    Clear Files

--- packages/react-app/src/hooks/index.js
+++
@@ -1,4 +0,0 @@
-export { default as useDebounce } from "./useDebounce";
-export { default as useLocalStorage } from "./useLocalStorage";
-export { default as useStaticJsonRPC } from "./useStaticJsonRPC";
-export * from "./useContractConfig";
