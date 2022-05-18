42619882d9634395ff67be0f03753598511fd748
Zak G <zakgriffith@gmail.com>
2022-05-18 10:30:10 -0600

    Rename Folders / Files

--- packages/eth-dev/src/components/levels/SetupLocalNetwork/Dialog/index.js
+++
@@ -1,5 +0,0 @@
-import Start from './dialogParts/Start'
-import BeginnerDev from './dialogParts/BeginnerDev'
-import SetupNetwork from './dialogParts/SetupNetwork'
-
-export default [...Start, ...BeginnerDev, ...SetupNetwork]
