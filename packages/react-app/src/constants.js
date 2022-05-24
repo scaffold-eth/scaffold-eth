1f2ed92e335e7a2dfccecab2defb1655dddb5a92
Mike Ryan <mike@fadedink.co.uk>
2022-05-24 20:11:34 +0200

    Added Codespace config

--- packages/react-app/src/constants.js
+++ packages/react-app/src/constants.js
@@ -9,13 +9,17 @@ export const BLOCKNATIVE_DAPPID = "0b58206a-f3c0-4701-a62f-73c7243e8c77";
 
 export const ALCHEMY_KEY = "oKxs-03sij-U_N0iOlrSsZFr29-IqbuF";
 
+const localRpcUrl = process.env.REACT_APP_CODESPACES
+  ? `https://${window.location.hostname.replace("3000", "8545")}`
+  : "http://" + (global.window ? window.location.hostname : "localhost") + ":8545";
+
 export const NETWORKS = {
   localhost: {
     name: "localhost",
     color: "#666666",
     chainId: 31337,
     blockExplorer: "",
-    rpcUrl: "http://" + (global.window ? window.location.hostname : "localhost") + ":8545",
+    rpcUrl: localRpcUrl,
   },
   mainnet: {
     name: "mainnet",
@@ -200,14 +204,6 @@ export const NETWORKS = {
     chainId: 1281,
     blockExplorer: "https://moonbeam-explorer.netlify.app/",
     rpcUrl: "http://127.0.0.1:9933",
-  },
-  godwoken: {
-    name: "godwoken",
-    color: "#000000",
-    chainId: 71401,
-    blockExplorer: "https://gw-explorer.nervosdao.community/",
-    rpcUrl: "https://godwoken-testnet-v1.ckbapp.dev",
-    faucet: "https://testnet.bridge.godwoken.io",
   },
 };
 
