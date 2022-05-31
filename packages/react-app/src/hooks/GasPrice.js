e4693dec280fc914186e937e6c032521764e423f
Joseph Izang <josephizang@gmail.com>
2022-05-31 02:06:05 +0100

    Revert "prettier cleanup"
    This reverts commit 10b2c2323108476d0f7c70d2418a32892fa787c9.

--- packages/react-app/src/hooks/GasPrice.js
+++ packages/react-app/src/hooks/GasPrice.js
@@ -1,25 +1,25 @@
-import axios from 'axios'
-import { usePoller } from 'eth-hooks'
-import { useState } from 'react'
+import axios from "axios";
+import { usePoller } from "eth-hooks";
+import { useState } from "react";
 
 export default function useGasPrice(targetNetwork, speed) {
-  const [gasPrice, setGasPrice] = useState()
+  const [gasPrice, setGasPrice] = useState();
   const loadGasPrice = async () => {
-    if (targetNetwork.hasOwnProperty('gasPrice')) {
-      setGasPrice(targetNetwork.gasPrice)
+    if (targetNetwork.hasOwnProperty("gasPrice")) {
+      setGasPrice(targetNetwork.gasPrice);
     } else {
-      axios
-        .get('https://ethgasstation.info/json/ethgasAPI.json')
-        .then(response => {
-          const newGasPrice = response.data[speed || 'fast'] * 100000000
-          if (newGasPrice !== gasPrice) {
-            setGasPrice(newGasPrice)
-          }
-        })
-        .catch(error => console.log(error))
+        axios
+          .get("https://ethgasstation.info/json/ethgasAPI.json")
+          .then(response => {
+            const newGasPrice = response.data[speed || "fast"] * 100000000;
+            if (newGasPrice !== gasPrice) {
+              setGasPrice(newGasPrice);
+            }
+          })
+          .catch(error => console.log(error));
     }
-  }
+  };
 
-  usePoller(loadGasPrice, 39999)
-  return gasPrice
+  usePoller(loadGasPrice, 39999);
+  return gasPrice;
 }
