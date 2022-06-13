164cfb5ee138f81f7158778c27ab5472f542dc32
Zak G <zakgriffith@gmail.com>
2022-06-13 09:22:52 -0600

    App.jsx Updates
    Updated riggedRoll function to allow for a require instead of forcing an if statement.  Fixed it so RR doesn't start the dice roll animation unless it is a winner.

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -474,31 +474,21 @@ function App(props) {
     });
   };
 
-  {
-    /*
+  /*
   const riggedRoll = async () => {
-
-    setDiceRolled(true);
-    setDiceRollImage("ROLL");
-
     tx(
       writeContracts.RiggedRoll.riggedRoll({ gasLimit: 500000 }),
       update => {
         console.log("TX UPDATE",update)
+        if (update?.status === "sent" || update?.status === 1) {
+          setDiceRolled(true);
+          setDiceRollImage("ROLL");
+        }
 
         if (update?.status === "failed") {
           setDiceRolled(false);
           setDiceRollImage(null);
         }
-
-        if(update?.status==1 || update?.status=="confirmed")
-        {
-          setTimeout(()=>{
-            setDiceRolled(false);
-            setDiceRollImage(null);
-          },1500)
-        }
-
       },
     );
   };
@@ -513,7 +503,6 @@ function App(props) {
     }
   });
 */
-  }
 
   const filter = readContracts.DiceGame?.filters.Roll(address, null);
 
