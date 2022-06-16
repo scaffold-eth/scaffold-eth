01f30d813ad7a16fadce7824db54ecdf7c40b03e
Jaxcoder <codenamejason@outlook.com>
2022-06-16 14:54:30 -0400

    building but nothing is showing...

--- packages/react-app/webpack.config.js
+++ packages/react-app/webpack.config.js
@@ -22,6 +22,18 @@ module.exports = ({ mode } = { mode: "development" }) => {
     },
     resolve: {
       extensions: [".js", ".jsx"],
+      fallback: {
+        fs: false,
+        tls: false,
+        net: false,
+        path: false,
+        zlib: false,
+        http: false,
+        https: false,
+        stream: true,
+        crypto: false,
+        "crypto-browserify": require.resolve("crypto-browserify"), //if you want to use this module also don't forget npm i crypto-browserify
+      },
     },
     module: {
       rules: [
