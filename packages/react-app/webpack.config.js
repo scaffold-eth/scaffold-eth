467e2e9651e82dfcddc4c529efd2e03fd2e35538
Jaxcoder <codenamejason@outlook.com>
2022-06-16 16:04:27 -0400

    woot, almost there

--- packages/react-app/webpack.config.js
+++ packages/react-app/webpack.config.js
@@ -30,7 +30,7 @@ module.exports = ({ mode } = { mode: "development" }) => {
         zlib: false,
         http: false,
         https: false,
-        stream: true,
+        stream: false,
         crypto: false,
         "crypto-browserify": require.resolve("crypto-browserify"), //if you want to use this module also don't forget npm i crypto-browserify
       },
