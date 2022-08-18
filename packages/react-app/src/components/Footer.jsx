241dce1bee388feeab3da4427588a6b07b2c7e05
Joseph Izang <josephizang@gmail.com>
2022-08-19 00:35:43 +0100

    add icons to footer

--- packages/react-app/src/components/Footer.jsx
+++ packages/react-app/src/components/Footer.jsx
@@ -1,6 +1,9 @@
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
+import EmailIcon from '@mui/icons-material/Email'
 import Logo from './Logo'
+import GitHubIcon from '@mui/icons-material/GitHub'
+import TwitterIcon from '@mui/icons-material/Twitter'
 
 const footerCategories = {
   PRODUCTS: ['Online IDE', 'Desktop App', 'Ethereum Remix', 'RemixD'],
@@ -30,9 +33,20 @@ function Footer() {
             ))}
           </Box>
         ))}
+        <Box display={'flex'}>
+          <EmailIcon sx={{ color: '#4F4F4F' }} />
+          <GitHubIcon sx={{ color: '#4F4F4F' }} />
+          <TwitterIcon sx={{ color: '#4f4f4f' }} />
+        </Box>
       </Box>
-      <Box borderTop={'1px solid #ccc'} display={'block'} sx={{ marginTop: 5 }}>
-        <Typography variant={'subtitle2'}>2022 Remix. All rights reserved.</Typography>
+      <Box
+        borderTop={'1px solid #EAEAEA'}
+        display={'block'}
+        sx={{ marginTop: 5, height: 66 }}
+        p={5}
+        alignItems={'center'}
+      >
+        <Typography variant={'subtitle2'}>&copy; 2022 Remix. All rights reserved.</Typography>
       </Box>
     </Box>
   )
