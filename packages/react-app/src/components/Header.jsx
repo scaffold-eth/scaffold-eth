27567f8b0de938888019fc1f516a05e913b4ace3
Jaxcoder <codenamejason@outlook.com>
2022-09-04 16:23:40 -0400

    update github link

--- packages/react-app/src/components/Header.jsx
+++ packages/react-app/src/components/Header.jsx
@@ -24,7 +24,7 @@ export default function Header({ link, title, subTitle, ...props }) {
 }
 
 Header.defaultProps = {
-  link: "https://github.com/austintgriffith/scaffold-eth",
-  title: "🏗 scaffold-eth",
-  subTitle: "forkable Ethereum dev stack focused on fast product iteration",
+  link: "https://github.com/scaffold-eth/scaffold-eth",
+  title: "🏗 Scaffold-Eth",
+  subTitle: "Forkable Ethereum dev stack focused on fast product iteration",
 };
