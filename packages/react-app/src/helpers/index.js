14f6f5187eea8dbed5162d60f2e1cf0690ab2541
Jaxcoder <codenamejason@outlook.com>
2022-09-28 13:50:00 -0400

    nuke files

--- packages/react-app/src/helpers/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Transactor } from "./Transactor";
-export { default as Web3ModalSetup } from "./Web3ModalSetup";
-export * as ipfs from "./ipfs";
--- packages/react-app/src/views/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Home } from "./Home";
-export { default as ExampleUI } from "./ExampleUI";
-export { default as Hints } from "./Hints";
-export { default as Subgraph } from "./Subgraph";
