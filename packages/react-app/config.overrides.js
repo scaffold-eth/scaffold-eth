b6a54b64066fd8dd3504d311d768a0a60d2f76c2
tfius <tadej.fius@gmail.com>
2022-09-29 18:58:33 +0200

    babel

---
+++ packages/react-app/config.overrides.js
@@ -0,0 +1,35 @@
+// config-overrides.js
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+
+module.exports = function override(config, env) {
+  console.log("override");
+  let loaders = config.resolve;
+  loaders.fallback = {
+    crypto: require.resolve("crypto-browserify"),
+    buffer: require.resolve("buffer"),
+    stream: require.resolve("stream-browserify"),
+    assert: require.resolve("assert"),
+  };
+  // disable chunks so the index.html won't change during development
+  config.optimization.splitChunks = {
+    cacheGroups: {
+      default: false,
+    },
+  };
+  config.optimization.runtimeChunk = false;
+
+  config.module.rules = [
+    {
+      test: /node_modules\/monaco-editor/,
+      use: {
+        loader: "babel-loader",
+        // if you include your babel config here,
+        // you don’t need the `babel.config.json` file
+        options: { presets: ["@babel/preset-env"] },
+      },
+    },
+    ...config.module.rules,
+  ];
+
+  return config;
+};
