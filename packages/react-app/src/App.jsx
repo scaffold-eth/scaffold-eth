b81ae55e6c1e4e73af5d7e930463f20820cf73a6
Austin Griffith <austin@ethereum.org>
2022-12-08 08:57:42 -0700

    😅 shame shame me trying to put master scaffold on mainnet🧌

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -54,7 +54,7 @@ const { ethers } = require("ethers");
 */
 
 /// 📡 What chain are your contracts deployed to?
-const initialNetwork = NETWORKS.mainnet; // <------- select your target frontend network (localhost, goerli, xdai, mainnet)
+const initialNetwork = NETWORKS.localhost; // <------- select your target frontend network (localhost, goerli, xdai, mainnet)
 
 // 😬 Sorry for all the console logging
 const DEBUG = true;
