b20aa6bf9d89ddf4420c8fea9af5e7f73585472b
Damian <damianmarti@gmail.com>
2022-12-16 13:19:09 -0300

    Uncomment NetworkDisplay compoment to be able to show 'Wrong Network' message

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -302,15 +302,14 @@ function App(props) {
       {/* {yourLocalBalance.lte(ethers.BigNumber.from("0")) && (
         <FaucetHint localProvider={localProvider} targetNetwork={targetNetwork} address={address} />
       )} */}
-      {/* TODO remove after design update */}
-      {/* <NetworkDisplay
+      <NetworkDisplay
         NETWORKCHECK={NETWORKCHECK}
         localChainId={localChainId}
         selectedChainId={selectedChainId}
         targetNetwork={targetNetwork}
         logoutOfWeb3Modal={logoutOfWeb3Modal}
         USE_NETWORK_SELECTOR={USE_NETWORK_SELECTOR}
-      /> */}
+      />
       <NavBar
         useBurner={USE_BURNER_WALLET}
         address={address}
