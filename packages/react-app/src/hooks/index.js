fd1618f8ee32cbbf730abe50b2fbb5f090fdc16f
Damian <damianmarti@gmail.com>
2022-12-27 21:03:02 -0300

    Use local gas price hook using etherscan api

--- packages/react-app/src/hooks/index.js
+++ packages/react-app/src/hooks/index.js
@@ -1,4 +1,5 @@
 export { default as useDebounce } from "./useDebounce";
 export { default as useLocalStorage } from "./useLocalStorage";
 export { default as useStaticJsonRPC } from "./useStaticJsonRPC";
+export { default as useGasPrice } from "./GasPrice";
 export * from "./useContractConfig";
