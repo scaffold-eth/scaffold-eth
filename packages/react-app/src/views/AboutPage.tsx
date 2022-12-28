e4ea2315a0df8615e2415cb5a87c9a2eaab43b2d
Joseph Izang <josephizang@gmail.com>
2022-12-28 12:59:50 +0100

    add review comments recommended by @vermouth22

--- packages/react-app/src/views/AboutPage.tsx
+++ packages/react-app/src/views/AboutPage.tsx
@@ -8,15 +8,15 @@ const inforCardPayload: { imageSrc: string; rewardType: RewardNames; text: JSX.E
   {
     imageSrc: '/Contributor.png',
     rewardType: 'Contributor',
-    text: <p>Contribute a pull request to the Remix codebase. When it's merged, you earn the Contributor</p>,
+    text: <p>Contribute a pull request to the Remix codebase. When it's merged, you earn the Contributor.</p>,
   },
   {
     imageSrc: '/Remixer.png',
     rewardType: 'Remixer',
     text: (
       <p>
-        Our base reward. When you earn any of the other rewards, you gain the ability to mint a Remixer to any wallet of
-        your choice.
+        The Remixer is our base reward. When you earn any of the other rewards, you gain the ability to mint a Remixer
+        to a wallet of your choice.
       </p>
     ),
   },
@@ -28,12 +28,12 @@ const inforCardPayload: { imageSrc: string; rewardType: RewardNames; text: JSX.E
         Beta test any release of Remix IDE and you earn the Beta Tester for that version.
         <br />
         <Button
-          variant="outlined"
-          sx={{ marginTop: 3 }}
+          variant="contained"
+          sx={{ background: '#81a6f7', ':hover': { background: '#1976d2', color: '#fff' }, marginTop: 3 }}
           href=" https://docs.google.com/forms/d/e/1FAIpQLSd0WsJnKbeJo-BGrnf7WijxAdmE4PnC_Z4M0IApbBfHLHZdsQ/viewform"
-          color={'inherit'}
+          target={'_blank'}
         >
-          Here is how
+          Here's How
         </Button>
       </p>
     ),
@@ -43,8 +43,14 @@ const inforCardPayload: { imageSrc: string; rewardType: RewardNames; text: JSX.E
     rewardType: 'Specials',
     text: (
       <p>
-        These are special issue rewards for participation in one of the several select events during the year.
-        <Button variant="outlined" sx={{ marginTop: 3 }} color={'inherit'} href="https://remix-project.org/#events">
+        These are special issue rewards for participation in one of several annual Ethereum events.
+        <br />
+        <Button
+          variant="contained"
+          sx={{ background: '#81a6f7', ':hover': { background: '#1976d2', color: '#fff' }, marginTop: 3 }}
+          href="https://remix-project.org/#events"
+          target={'_blank'}
+        >
           Event schedule
         </Button>
       </p>
@@ -53,7 +59,7 @@ const inforCardPayload: { imageSrc: string; rewardType: RewardNames; text: JSX.E
   {
     imageSrc: '/UX Champion.png',
     rewardType: 'UX Champion',
-    text: <p>If you help us learn more about the user experience from your perspective, you earn the UX Chamion.</p>,
+    text: <p>When you help us improve the Remix user experience, you earn the UX Champion.</p>,
   },
 ]
 
@@ -61,11 +67,16 @@ export default function About() {
   return (
     <>
       <Box sx={{ paddingTop: '76px' }}>
-        <Typography variant="h3" fontWeight={700} fontFamily={'Noah'} color={'#333333'} marginBottom={5} align="left">
+        <Typography
+          variant="h3"
+          fontWeight={700}
+          fontFamily={'Noah'}
+          color={'#333333'}
+          marginBottom={5}
+          align="left"
+          paddingLeft={7}
+        >
           About Rewards
-        </Typography>
-        <Typography variant="h5" fontWeight={400} marginBottom={5} color={'#333333'} align="left">
-          Have you ever wondered how you can get a reward? Well you find out how here.
         </Typography>
         <Box
           sx={{
