8bff638810aa900aa1279b8cfe4f6bb3ffa615db
Xiangan He <xiangan9@gmail.com>
2023-01-10 10:35:38 -0500

    demo-day docstrings additions, minor modifications

--- packages/hardhat/contracts/YourContractV2.sol
+++ packages/hardhat/contracts/YourContractV2.sol
@@ -6,15 +6,20 @@ import '@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
 // https://docs.openzeppelin.com/contracts/3.x/api/proxy#UpgradeableProxy
 
-contract YourContract is UUPSUpgradeable, OwnableUpgradeable {
+contract YourContractV2 is UUPSUpgradeable, OwnableUpgradeable {
 
   event SetPurpose(address sender, string purpose);
 
+  // States are preserved as a part of an upgrade. 
+  // Function implmeentations change, but this should be whatever it is you set previously.
   string public purpose;
 
   function initialize() public initializer {
+    // This doesn't actually do anything. 
+    // Changing the initialization implementation of a contract that has already been initialized doesn't really make sense.
+    // As a result, purpose should still be "Building Unstoppable Apps", or whatever it was changed to from V1.
     __Ownable_init();
-    purpose = "Building Unstoppable Apps";
+    purpose = "Building Stoppable Apps";
   }
 
   function setPurpose(string memory newPurpose) public payable {
@@ -28,7 +33,7 @@ contract YourContract is UUPSUpgradeable, OwnableUpgradeable {
   fallback() external payable {}
 
   function version() public pure returns(string memory) {
-    return "v1.0.0";
+    return "v2.0.0";
   }
 
   function _authorizeUpgrade(
--- packages/hardhat/scripts/upgrade.js
+++ packages/hardhat/scripts/upgrade.js
@@ -1,14 +1,13 @@
 const { ethers, upgrades } = require("hardhat");
 
 // Plug in the address of the contract you want to upgrade
-const YourContractAddress = "";
+const YourContractAddress = "0x0DCd1Bf9A1b36cE34237eEaFef220932846BCD82";
 
 async function main() {
-  const YourContract = await ethers.getContractFactory("YourContract");
-  const yourcontract = await upgrades.upgradeProxy(
-    YourContractAddress,
-    YourContract
+  const YourContractV2Factory = await ethers.getContractFactory(
+    "YourContractV2"
   );
+  await upgrades.upgradeProxy(YourContractAddress, YourContractV2Factory);
   console.log("Contract upgraded");
 }
 
