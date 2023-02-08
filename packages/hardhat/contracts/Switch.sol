b0e8afc2f13ef7e0872524f1cb14b9273dc7527e
Edmilson Rolas <edrolas.er@gmail.com>
2023-02-08 15:22:11 +0200

    first contract

---
+++ packages/hardhat/contracts/Switch.sol
@@ -0,0 +1,19 @@
+/* Here we specify the solidity versions
+* Any version greater than or equal to 0.6.2
+* or less than 0.7.0 will compile this contract */
+pragma solidity ^0.6.2;
+contract YourContract {
+// the switch is on if true
+bool public isOn;
+constructor() public {
+// we'll default to being on
+isOn = true;
+}
+// a publicly accessible function to "flip" the switch
+function toggle() public returns(bool) {
+// flip isOn from true->false or false->true
+isOn = !isOn;
+// return the new value
+return isOn;
+}
+}
