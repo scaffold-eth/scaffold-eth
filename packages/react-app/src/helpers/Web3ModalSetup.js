5596fd950e0acbe745875a7d00b0d42b88221a0d
Damian <damianmarti@gmail.com>
2023-02-24 18:25:01 -0300

    Comment wallets

--- packages/react-app/src/helpers/Web3ModalSetup.js
+++ packages/react-app/src/helpers/Web3ModalSetup.js
@@ -42,23 +42,23 @@ const web3ModalSetup = () =>
           },
         },
       },
-      portis: {
-        display: {
-          logo: "https://user-images.githubusercontent.com/9419140/128913641-d025bc0c-e059-42de-a57b-422f196867ce.png",
-          name: "Portis",
-          description: "Connect to Portis App",
-        },
-        package: Portis,
-        options: {
-          id: "6255fb2b-58c8-433b-a2c9-62098c05ddc9",
-        },
-      },
-      fortmatic: {
-        package: Fortmatic, // required
-        options: {
-          key: "pk_live_5A7C91B2FC585A17", // required
-        },
-      },
+      // portis: {
+      //   display: {
+      //     logo: "https://user-images.githubusercontent.com/9419140/128913641-d025bc0c-e059-42de-a57b-422f196867ce.png",
+      //     name: "Portis",
+      //     description: "Connect to Portis App",
+      //   },
+      //   package: Portis,
+      //   options: {
+      //     id: "6255fb2b-58c8-433b-a2c9-62098c05ddc9",
+      //   },
+      // },
+      // fortmatic: {
+      //   package: Fortmatic, // required
+      //   options: {
+      //     key: "pk_live_5A7C91B2FC585A17", // required
+      //   },
+      // },
       // torus: {
       //   package: Torus,
       //   options: {
@@ -72,21 +72,21 @@ const web3ModalSetup = () =>
       //     },
       //   },
       // },
-      "custom-walletlink": {
-        display: {
-          logo: "https://play-lh.googleusercontent.com/PjoJoG27miSglVBXoXrxBSLveV6e3EeBPpNY55aiUUBM9Q1RCETKCOqdOkX2ZydqVf0",
-          name: "Coinbase",
-          description: "Connect to Coinbase Wallet (not Coinbase App)",
-        },
-        package: walletLinkProvider,
-        connector: async (provider, _options) => {
-          await provider.enable();
-          return provider;
-        },
-      },
-      authereum: {
-        package: Authereum, // required
-      },
+      // "custom-walletlink": {
+      //   display: {
+      //     logo: "https://play-lh.googleusercontent.com/PjoJoG27miSglVBXoXrxBSLveV6e3EeBPpNY55aiUUBM9Q1RCETKCOqdOkX2ZydqVf0",
+      //     name: "Coinbase",
+      //     description: "Connect to Coinbase Wallet (not Coinbase App)",
+      //   },
+      //   package: walletLinkProvider,
+      //   connector: async (provider, _options) => {
+      //     await provider.enable();
+      //     return provider;
+      //   },
+      // },
+      // authereum: {
+      //   package: Authereum, // required
+      // },
     },
   });
 
