2071f23e7956d39a2ea1c05a201e52d7340109cc
Damian <damianmarti@gmail.com>
2023-02-27 14:03:07 -0300

    Compare lower case vendor addresses

--- packages/react-app/src/views/PayToVendor.jsx
+++ packages/react-app/src/views/PayToVendor.jsx
@@ -40,7 +40,7 @@ function PayToVendor({
   useEffect(() => {
     console.log("vendorAddress: ", vendorAddress);
     if (vendors) {
-      const vendorIndex = vendors.findIndex(element => element.value === vendorAddress);
+      const vendorIndex = vendors.findIndex(element => element.value.toLowerCase() === vendorAddress.toLowerCase());
       if (vendorIndex >= 0) {
         setVendorLabel(vendors[vendorIndex].label);
       }
